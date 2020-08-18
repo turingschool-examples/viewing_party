975de8f3a0f00cc898cd095f66fb9281b156b789
Nick Edwin <nickedwin85@gmail.com>
2020-08-18 15:40:56 -0600

    Login button attemped fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
 
   get '/auth/google_oauth2', to: 'sessions#create'
-  
-  # get '/auth/:provider/callback', to: 'sessions#googleAuth'
-  # get '/auth/failure', to: redirect('/')
+  get '/auth/:provider/callback', to: 'sessions#googleAuth'
+  get '/auth/failure', to: redirect('/')
 
 end
