7464291250ccd79e982e28e14af0dfda21b6cf38
“Neeru <neeram85@gmail.com>
2020-08-18 15:51:35 -0600

    Create ViewPartyAttendees model

---
+++ app/models/view_party_attendees.rb
@@ -0,0 +1,2 @@
+class ViewPartyAttendees < ApplicationRecord
+end 
---
+++ spec/models/view_party_attendees_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ViewPartyAttendees, type: :model do
+  describe 'Relationships' do
+    it {should belong_to :view_party}
+    it {should belong_to :user}
+  end
+end
