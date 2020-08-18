4d84d11db75ca60108f1fd83a57628fad3688c78
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-08-18 14:21:46 -0600

    Add welcome spec, controller, and view

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe 'As a user' do
+  describe "When I visit the root path" do
+    it "I should see a welcome page with a brief description of the application." do
+      visit root_path
+      expect(page).to have_content("Viewing Party is an application that allows users to create and watch movies with other users integrated with Google Calender")
+    end
+  end
+end
