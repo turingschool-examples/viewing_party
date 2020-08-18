5e71d4d2af472b2a6a19c247403fbb44a3d82350
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-18 13:37:14 -0600

    Add movie service api call

---
+++ app/services/movie_service.rb
@@ -0,0 +1,18 @@
+class MovieService
+  def movies
+    to_json('discover/movie')
+  end
+
+  private
+
+  def to_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new('https://api.themoviedb.org/3/') do |f|
+      f.params[:api_key] = ENV["MOVIE_API"]
+    end
+  end
+end
