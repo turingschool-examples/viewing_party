01fd5ab733804809b4cdf1e24a02a6575787f5cd
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-18 14:36:38 -0600

    Add test for a button to add party.

---
+++ spec/features/movie/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'movie show page' do
+
+  it 'has a create button' do
+    movie =     {
+      "id": 164558,
+      "original_title": "One Direction: This Is Us",
+      "original_language": "en",
+      "title": "One Direction: This Is Us",
+      "popularity": 1.166982,
+      "vote_count": 55,
+      "video": false,
+      "vote_average": 8.45
+    }
+
+    # binding.pry
+    visit "/movies/#{movie[:id]}"
+
+    expect(page).to  have_button('Add Viewing Party for Movie')
+  end
+end
