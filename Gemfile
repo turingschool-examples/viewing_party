0b018a3afb997ef9751b606de51603ef408da800
Nick Edwin <nickedwin85@gmail.com>
2020-08-18 14:34:02 -0600

    Attempting to implement google login auth
    Co-authored-by: Eric Hale <59599782+EHale64@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -15,11 +15,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'bootsnap'
 gem 'jbuilder', '~> 2.5'
-gem 'therubyracer'
+gem 'omniauth-google-oauth2'
 
 group :development, :test do
   gem 'pry'
-  gem 'travis'
 end
 
 group :development do
---
+++ app/assets/config/initializers/omniauth.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2,
+  ENV['62868422183-mcitplkv4usrjks0t02h67q69oeh7hlu.apps.googleusercontent.com'],
+  ENV['8e3l4ASA5L_sS5-QV6q0oAdV'],
+  {
+    scope: 'userinfo.email, userinfo.profile, userinfo.calender',
+    prompt: 'select_account',
+    image_aspect_ratio: 'square',
+    image_size: 50
+  }
+end
