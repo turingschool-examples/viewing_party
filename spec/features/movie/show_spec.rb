aa79c59e9c8238e9e14acfb02ff020a9873243ab
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-18 21:50:50 -0600

    get request from the API.

--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -1,20 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'movie show page' do
-
-  it 'has a create button' do
-    movie =     {
-      "id": 164558,
-      "original_title": "One Direction: This Is Us",
-      "original_language": "en",
-      "title": "One Direction: This Is Us",
-      "popularity": 1.166982,
-      "vote_count": 55,
-      "video": false,
-      "vote_average": 8.45
-    }
-
-    visit "/movies/#{movie[:id]}"
+  # before :each do
+  #   allow(Faraday).to receive(:get).and_return({"results": [{"id": 164558,"original_title": "One Direction: This Is Us","original_language": "en","title": "One Direction: This Is Us"}]})
+  # end
+  it 'can get to new party page' do
+    res = MovieService.new.movies
+    id = res[:results][0][:id]
+    visit "/movies/#{id}"
 
     expect(page).to  have_button('Add Viewing Party for Movie')
 
