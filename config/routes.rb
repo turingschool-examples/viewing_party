b5364e741423d802c627dd082aeabad986687c04
Josh Tukman <josh.tukman@gmail.com>
2020-08-19 15:34:09 -0600

    Namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: "welcome#index"
+  get '/', to: 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#omniauth'
-  get '/dashboard', to: 'users#show'
+
+  namespace :dashboard do
+    get '/', to: 'users#show'
+    get '/discover', to: 'discover#index'
+  end 
 end
