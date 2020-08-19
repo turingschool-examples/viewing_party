f85bc5a6c417ca36651ea001d809e3b857988800
Josh Tukman <josh.tukman@gmail.com>
2020-08-19 15:31:53 -0600

    change name of parameter to user_info from auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
 
-  def self.from_omniauth(auth)
-    user = where(email: auth.info.email).first_or_create do |user|
-      user.username = auth.info.name
-      user.email = auth.info.email
+  def self.from_omniauth(user_info)
+    user = where(email: user_info.info.email).first_or_create do |user|
+      user.username = user_info.info.name
+      user.email = user_info.info.email
     end
   end
 end
