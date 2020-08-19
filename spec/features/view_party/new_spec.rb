2336100be4fcf111e0390f8a6181a87e8db9cf85
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-19 09:04:40 -0600

    x

--- spec/features/view_party/new_spec.rb
+++ spec/features/view_party/new_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Create Party' do
 
   it 'has a form' do
-    visit new_viewing_party_path
+    visit new_view_party_path
 
     find_field :duration
     find_field :date
