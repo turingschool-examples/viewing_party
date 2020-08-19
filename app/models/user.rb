8de591ba3d81251ed83400cf25cb1acf4349a355
Michael Evans <michaeljevans113@gmail.com>
2020-08-19 16:55:49 -0600

    Create .from_omniauth method to handle incoming user data
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email, :access_token
   validates :uid, uniqueness: true, presence: true
+
+  def self.from_omniauth(params)
+    user = find_or_create_by(uid: params[:uid])
+    user.name = params[:info][:name]
+    user.email = params[:info][:email]
+    user.access_token = params[:credentials][:token]
+    user.save!
+    user
+  end
 end
