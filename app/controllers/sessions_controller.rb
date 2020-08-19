d7b84c5847e2576c7b11cd1712fd908ca572ac40
Michael Evans <michaeljevans113@gmail.com>
2020-08-19 16:56:48 -0600

    Add to create action; creates (or finds) user and creates new session
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
   def create
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to '/dashboard'
   end
 end
