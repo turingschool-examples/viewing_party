9a40e24c12f1a3a5ef166aba13893301a9a26088
“Neeru <neeram85@gmail.com>
2020-08-18 18:31:27 -0600

    Create Friendships table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_001910) do
+ActiveRecord::Schema.define(version: 2020_08_19_002926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "name"
@@ -50,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_001910) do
     t.index ["view_party_id"], name: "index_view_party_attendees_on_view_party_id"
   end
 
+  add_foreign_key "friendships", "users"
   add_foreign_key "movies", "view_parties"
   add_foreign_key "view_parties", "users"
   add_foreign_key "view_party_attendees", "users"
