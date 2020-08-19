97927315d8e3e0069c1553e4aace5159d83f9547
Michael Evans <michaeljevans113@gmail.com>
2020-08-19 17:40:16 -0600

    Add route for dashboard after successful login
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   get '/auth/google_oauth2/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/')
+
+  get '/dashboard', to: 'dashboard#show'
 end
