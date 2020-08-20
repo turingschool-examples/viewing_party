61b967be3849176159305c1475f5b7393f462ef2
Josh Tukman <josh.tukman@gmail.com>
2020-08-19 19:11:40 -0600

    Movie api service

--- app/services/movies_service.rb
+++ app/services/movies_service.rb
@@ -1,7 +1,14 @@
 class MoviesService 
 
   def top40
-    get_json
+    acc = []
+    get_json('/3/movie/top_rated?page=1')[:results].each do |movie|
+      acc << movie
+    end 
+    get_json('/3/movie/top_rated?page=2')[:results].each do |movie|
+      acc << movie
+    end 
+    @movies = acc.map{|info| MovieIndexObject.new(info)}
   end 
 
   private
@@ -12,8 +19,9 @@ class MoviesService
   end
 
   def conn
-    Faraday.new("") do |faraday|
-      faraday.params[key] = 
-    end 
+    Faraday.new("https://api.themoviedb.org") do |faraday|
+      faraday.params[:api_key] = ENV['MOVIES_API_KEY']
+    end
   end
 end 
+#https://api.themoviedb.org/3/movie/top_rated?api_key=768c17292f63a7075f76e2c15d79d38f
