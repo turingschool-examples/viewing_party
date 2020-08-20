200231cb352b3c053b21a22488420d448350eee6
Michael Evans <michaeljevans113@gmail.com>
2020-08-19 18:29:46 -0600

    Add test for .from_omniauth
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,35 @@ RSpec.describe User do
   describe 'relationships' do
 
   end
+
+  describe 'class methods' do
+    it '.from_omniauth' do
+      params = {
+        uid: '123',
+        info: {name: 'Joe', email: 'joe@me.com'},
+        credentials: {token: 'access'}
+      }
+
+      user = User.from_omniauth(params)
+      user_id = user.id
+      expect(user).to eq(User.last)
+      expect(user.uid).to eq('123')
+      expect(user.name).to eq('Joe')
+      expect(user.email).to eq('joe@me.com')
+      expect(user.access_token).to eq('access')
+
+      params_2 = {
+        uid: '456',
+        info: {name: 'Bob', email: 'bob@me.com'},
+        credentials: {token: 'more_access'}
+      }
+
+      user_2 = User.from_omniauth(params_2)
+
+      expect(User.from_omniauth(params)).to eq(user)
+      expect(User.from_omniauth(params).id).to eq(user_id)
+
+      expect(User.last).to eq(user_2)
+    end
+  end
 end
