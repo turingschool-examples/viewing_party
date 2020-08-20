049134a7aef92c6e4a16b2aca879a4a3d33910e9
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-19 20:17:24 -0600

    Rubocop fix
    Co-Authored-By: Kwibe Merci <46441816+jKwibe@users.noreply.github.com>
    Co-Authored-By: Neeru Ram <49666486+neeruram1@users.noreply.github.com>

--- app/models/view_party.rb
+++ app/models/view_party.rb
@@ -1,6 +1,7 @@
 class ViewParty < ApplicationRecord
-  validates_presence_of :duration,
-                        :date
+  validates :duration, presence: true
+  validates :date, presence: true
+
   belongs_to :host, class_name: "User"
   has_many :view_party_attendees
   has_many :users, through: :view_party_attendees
