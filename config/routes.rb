445298786d2f0bd64dfb5d782f55f6930c71e94c
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-19 20:01:34 -0600

    Implement omniauth and add the callback route.
    Co-authored-by: Kwibe Merci <46441816+jKwibe@users.noreply.github.com>
    Co-authored-by: Neeru Ram <49666486+neeruram1@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/dashboard', to: 'users#show'
   get '/discover', to: 'discover#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
   resources :movies, only: [:show]
   resources :view_party, only: [:new, :create]
-
 end
