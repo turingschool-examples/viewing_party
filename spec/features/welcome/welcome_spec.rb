c6e2a644d701be080833f7740f37f5a2630d30fd
Michael Evans <michaeljevans113@gmail.com>
2020-08-19 18:04:08 -0600

    Change users to user
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "as a user" do
   it "I can visit welcome and see descrition" do
     visit '/'
+
     expect(page).to have_content('Viewing Party App')
+    expect(page).to have_button('Log In with Google')
   end
 end
