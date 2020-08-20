56ea427cd3ef216abec62bb52db6909c6497f8a4
Nick Edwin <nickedwin85@gmail.com>
2020-08-19 18:18:45 -0600

    Attempting to parse google response
    Co-authored-by: Eric Hale <59599782+EHale64@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,21 @@
 class SessionsController < ApplicationController
 
   def create
+    code = params[:code]
+    client_id = ENV["GOOGLE_CLIENT_ID"]
+    client_secret = ENV["GOOGLE_CLIENT_SECRET"]
+
+    conn = Faraday.new(url: 'https://google.com', headers: {'Accept': 'application/json'})
+
+    response = conn.post('/login/oauth/access_token') do |req|
+      req.params['code'] = code
+      req.params['client_id'] = client_id
+    end
+
+    binding.pry
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    access_token = data[:access_token]
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
 
-  get '/auth/google_oauth2', to: 'sessions#create'
-  get '/auth/:provider/callback', to: 'sessions#googleAuth'
+  get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/')
 
 end
