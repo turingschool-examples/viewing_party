94235c711b9eb6be53fdd860da69c7d985672715
Jane Greene <janegreenecasa@gmail.com>
2020-08-20 17:18:44 -0600

    Added methods to build and destroy inverse relationships betweeen user and friends Co-authored-by: Michael Evans <62968371+michaeljevans@users.noreply.github.com>

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,26 +2,26 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
-  after_create :create_inverse, unless: :has_inverse?
-  after_destroy :destroy_inverses, if: :has_inverse?
+  after_create :create_inverse, unless: :inverse_exist?
+  after_destroy :destroy_inverse, if: :inverse_exist?
 
   def create_inverse
-   self.class.create(inverse_match_options)
+    self.class.create(inverse_match_options)
   end
 
-  def destroy_inverses
-   inverses.destroy_all
+  def destroy_inverse
+    find_inverse.destroy_all
   end
 
-  def has_inverse?
+  def inverse_exist?
     self.class.exists?(inverse_match_options)
   end
 
-   def inverses
-     self.class.where(inverse_match_options)
-   end
+  def find_inverse
+    self.class.where(inverse_match_options)
+  end
 
-   def inverse_match_options
-     { friend_id: user_id, user_id: friend_id }
-   end
+  def inverse_match_options
+    { friend_id: user_id, user_id: friend_id }
+  end
 end
