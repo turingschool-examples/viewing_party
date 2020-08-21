c0363182dd0a1dc32be923d49622e4ff34a22e5d
Michael Evans <michaeljevans113@gmail.com>
2020-08-20 21:22:01 -0600

    Remove unnecessary if guard clause
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,7 @@ class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(email: params[:friend_email])
     if friend
-      Friendship.create!(user_id: current_user.id, friend_id: friend.id) if friend
+      Friendship.create!(user_id: current_user.id, friend_id: friend.id)
       flash[:success] = 'You have a new Friend!'
     else
       flash[:error] = 'Sorry, your friend cannot be found.'
