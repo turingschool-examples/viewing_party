4ac7ecedb6fa3aefe67a01bd384264b3d836e34a
Josh Tukman <josh.tukman@gmail.com>
2020-08-20 18:06:44 -0600

    Add route to movie show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#omniauth'
-
-  get '/movies', to: 'movies#index'
+  
+  resources :movies, only:[:index, :show]
+  #get '/movies', to: 'movies#index'
+  #get '/movies', to: 'movies#show'
 
   namespace :dashboard do
     get '/', to: 'users#show'
