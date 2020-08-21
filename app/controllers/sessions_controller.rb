306731dd301b94420169a5bab4c3c31276a9dd87
Josh Tukman <josh.tukman@gmail.com>
2020-08-21 16:20:22 -0600

    Refactor method to make it shorter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,6 @@
 class SessionsController < ApplicationController
   def omniauth
-    user_info = request.env['omniauth.auth']
-    user = User.from_omniauth(user_info)
-    user.google_token = user_info.credentials.token
-    refresh_token = user_info.credentials.refresh_token
-    user.google_refresh_token = refresh_token if refresh_token.present?
+    user = oauth_user
     if user.save
       session[:user_id] = user.id
       redirect_to '/dashboard'
@@ -12,5 +8,14 @@ class SessionsController < ApplicationController
       flash[:error] = "Credentials don't work"
       redirect_to '/'
     end
+  end
+
+  def oauth_user
+    user_info = request.env['omniauth.auth']
+    user = User.from_omniauth(user_info)
+    user.google_token = user_info.credentials.token
+    refresh_token = user_info.credentials.refresh_token
+    user.google_refresh_token = refresh_token if refresh_token.present?
+    user
   end
 end
