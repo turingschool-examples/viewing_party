46466d0f836d677654c6da44029addae6283d92a
Michael Evans <michaeljevans113@gmail.com>
2020-08-22 12:59:49 -0600

    Simplify create action; class method to create mutual friendship
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,7 @@ class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(email: params[:friend_email])
     if friend
-      Friendship.create!(user_id: current_user.id, friend_id: friend.id)
+      Friendship.create_mutual_friendship(current_user.id, friend.id)
       flash[:success] = 'You have a new friend!'
     else
       flash[:error] = 'Sorry, your friend cannot be found.'
