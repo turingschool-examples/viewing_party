e7cddb63cd751fec3157e61295446230c74d456f
Michael Evans <michaeljevans113@gmail.com>
2020-08-22 15:33:15 -0600

    Create movie poro
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,7 +1,8 @@
 class Movie
-  attr_reader :title, :vote_avg
+  attr_reader :id, :title, :vote_avg
 
   def initialize(movie_info)
+    @id       = movie_info[:id]
     @title    = movie_info[:title]
     @vote_avg = movie_info[:vote_average].to_f
   end
