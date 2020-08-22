01296b80989c4c8572445074d46f70c9a94a393b
Phillip Strom <phillip.strom@gmail.com>
2020-08-22 15:42:14 -0600

    Create find friends feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#omniauth'
-  
+
   post '/movies', to: 'movies#index'
   resources :movies, only:[:index, :show]
   #get '/movies', to: 'movies#index'
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   namespace :dashboard do
     get '/', to: 'users#show'
     get '/discover', to: 'discover#index'
+    post '/friendships', to: 'friendships#create'
   end
+
 end
---
+++ spec/features/friendships/new_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe "User Friendships" do
+  before :each do
+    @user = User.create!(username: "Quentin", email: "tarantino@gmail.com")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    @user2 = User.create!(username: "Francis", email: "coppola@wine.com")
+    @user3 = User.create!(username: "Martin", email: "scorsese@hotmail.com")
+  end
+  it "can be created by searching for email" do
+    visit '/dashboard'
+    expect(page).to have_content("You currently have no friends")
+
+    fill_in :email, with: @user2.email
+    click_on "Add Friend"
+
+    expect(current_path).to eq('/dashboard')
+
+    expect(page).to_not have_content("You currently have no friends")
+    expect(page).to have_content("You are now friends with #{@user2.username}")
+    within(".friend-list") do
+      expect(page).to have_content(@user2.username)
+    end
+  end
+  it "can't add non-existent users" do
+    visit '/dashboard'
+    expect(page).to have_content("You currently have no friends")
+
+    fill_in :email, with: "fake@email.com"
+    click_on "Add Friend"
+
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content("You currently have no friends")
+    expect(page).to have_content("User not found")
+  end
+end
