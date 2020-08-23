f9c6334d856b1b20b49faf229d68235fb95e26dd
Michael Evans <michaeljevans113@gmail.com>
2020-08-23 14:25:22 -0600

    Refactor top 40 portion of index action to utilize movie facade & movie service
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,28 +1,7 @@
 class MoviesController < ApplicationController
   def index
-    if params[:search] == 'top_40'
-      url = 'https://api.themoviedb.org'
-      connection = Faraday.new(url)
-
-      response_1 = connection.get('/3/movie/top_rated') do |f|
-        f.headers['Content-Type'] = 'application/json'
-        f.params['api_key']       = ENV['TMDB_API_KEY']
-      end
-
-      response_2 = connection.get('/3/movie/top_rated') do |f|
-        f.headers['Content-Type'] = 'application/json'
-        f.params['api_key']       = ENV['TMDB_API_KEY']
-        f.params['page']          = '2'
-      end
-
-      parsed_1 = JSON.parse(response_1.body, symbolize_names: true)
-      parsed_2 = JSON.parse(response_2.body, symbolize_names: true)
-
-      top_40 = parsed_1[:results] << parsed_2[:results]
-
-      @movies = top_40.flatten.map do |movie_info|
-        Movie.new(movie_info)
-      end
+    if params[:search]
+      @movies = MovieFacade.get_top_movies
     elsif params[:search_terms]
       keywords = params[:search_terms].gsub(' ', '+')
       url = 'https://api.themoviedb.org'
