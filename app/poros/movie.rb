961c1d88437e0fee8cd0f7077c2dbe412aca9e19
Jane Greene <janegreenecasa@gmail.com>
2020-08-22 18:33:55 -0600

    Edited poro for more details still wip
    Co-authored-by: Michael Evans <62968371+michaeljevans@users.noreply.github.com>

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -2,8 +2,13 @@ class Movie
   attr_reader :id, :title, :vote_avg
 
   def initialize(movie_info)
+    # genre_array = parsed[:genres].map do |genre|
+    #   genre[:name]
+    # end
+    require "pry"; binding.pry
     @id       = movie_info[:id]
-    @title    = movie_info[:title]
+    @runtime = movie_info[:runtime]
+    @title    = movie_info[:title] || movie_info[:original_title]
     @vote_avg = movie_info[:vote_average].to_f
   end
 end
