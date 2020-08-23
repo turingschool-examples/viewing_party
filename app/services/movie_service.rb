895172ccf7d4509fbd69ef53a4184543bf850050
Michael Evans <michaeljevans113@gmail.com>
2020-08-23 16:35:54 -0600

    Create #top_rated() and #movie_search() to reduce length of other methods
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,40 +1,22 @@
 class MovieService
-  def get_top_40
-    page_1 = connection.get('/3/movie/top_rated') do |f|
-      f.headers['Content-Type'] = 'application/json'
-      f.params['api_key']       = ENV['TMDB_API_KEY']
-    end
+  def top_40
+    page1 = top_rated('1')
+    page2 = top_rated('2')
 
-    page_2 = connection.get('/3/movie/top_rated') do |f|
-      f.headers['Content-Type'] = 'application/json'
-      f.params['api_key']       = ENV['TMDB_API_KEY']
-      f.params['page']          = '2'
-    end
+    parsed1 = JSON.parse(page1.body, symbolize_names: true)
+    parsed2 = JSON.parse(page2.body, symbolize_names: true)
 
-    parsed_1 = JSON.parse(page_1.body, symbolize_names: true)
-    parsed_2 = JSON.parse(page_2.body, symbolize_names: true)
-
-    parsed_1[:results] + parsed_2[:results]
+    parsed1[:results] + parsed2[:results]
   end
 
   def from_search(terms)
-    page_1 = connection.get('/3/search/movie') do |f|
-      f.headers['Content-Type'] = 'application/json'
-      f.params['api_key']       = ENV['TMDB_API_KEY']
-      f.params['query']         = terms
-    end
+    page1 = movie_search(terms, '1')
+    page2 = movie_search(terms, '2')
 
-    page_2 = connection.get('/3/search/movie') do |f|
-      f.headers['Content-Type'] = 'application/json'
-      f.params['api_key']       = ENV['TMDB_API_KEY']
-      f.params['query']         = terms
-      f.params['page']          = '2'
-    end
+    parsed1 = JSON.parse(page1.body, symbolize_names: true)
+    parsed2 = JSON.parse(page2.body, symbolize_names: true)
 
-    parsed_1 = JSON.parse(page_1.body, symbolize_names: true)
-    parsed_2 = JSON.parse(page_2.body, symbolize_names: true)
-
-    parsed_1[:results] + parsed_2[:results]
+    parsed1[:results] + parsed2[:results]
   end
 
   def movie_details(movie_id)
@@ -72,5 +54,22 @@ class MovieService
 
   def connection
     Faraday.new('https://api.themoviedb.org')
+  end
+
+  def top_rated(page)
+    connection.get('/3/movie/top_rated') do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.params['api_key']       = ENV['TMDB_API_KEY']
+      f.params['page']          = page
+    end
+  end
+
+  def movie_search(terms, page)
+    connection.get('/3/search/movie') do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.params['api_key']       = ENV['TMDB_API_KEY']
+      f.params['query']         = terms
+      f.params['page']          = page
+    end
   end
 end
