a2d4bc3cd9a28cc2b55faff7e38ae7fcfe935bfb
Michael Evans <michaeljevans113@gmail.com>
2020-08-23 16:08:13 -0600

    Facade is now for one movie; gathers movie info, cast, and reviews
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,13 +1,25 @@
 class MovieFacade
-  def self.get_top_movies
-    top_40 = MovieService.new.top_40
+  attr_reader :info, :cast, :reviews
 
-    top_40.map { |movie_info| Movie.new(movie_info) }
+  def initialize(movie_id)
+    @movie_service ||= MovieService.new
+    @info    = movie_details(movie_id)
+    @cast    = cast_details(movie_id)
+    @reviews = movie_reviews(movie_id)
   end
 
-  def self.movies_from_search(keywords)
-    results = MovieService.new.from_search(keywords)
+  def movie_details(movie_id)
+    movie_info = @movie_service.movie_details(movie_id)
+    Movie.new(movie_info)
+  end
 
-    results.map { |movie_info| Movie.new(movie_info) }
+  def cast_details(movie_id)
+    cast_info = @movie_service.cast_details(movie_id)
+    Cast.new(cast_info)
+  end
+
+  def movie_reviews(movie_id)
+    reviews_info = @movie_service.reviews_details(movie_id)
+    reviews_info.map { |review_info| Review.new(review_info) }
   end
 end
