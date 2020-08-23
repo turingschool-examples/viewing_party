4a020e7074a001f836348d598c70da6f6406b329
Michael Evans <michaeljevans113@gmail.com>
2020-08-23 14:33:05 -0600

    Add #from_search() method; make connection private
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -17,6 +17,28 @@ class MovieService
     parsed_1[:results] + parsed_2[:results]
   end
 
+  def from_search(keywords)
+    page_1 = connection.get('/3/search/movie') do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.params['api_key']       = ENV['TMDB_API_KEY']
+      f.params['query']         = keywords
+    end
+
+    page_2 = connection.get('/3/search/movie') do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.params['api_key']       = ENV['TMDB_API_KEY']
+      f.params['query']         = keywords
+      f.params['page']          = '2'
+    end
+
+    parsed_1 = JSON.parse(page_1.body, symbolize_names: true)
+    parsed_2 = JSON.parse(page_2.body, symbolize_names: true)
+
+    parsed_1[:results] + parsed_2[:results]
+  end
+
+  private
+
   def connection
     Faraday.new('https://api.themoviedb.org')
   end
