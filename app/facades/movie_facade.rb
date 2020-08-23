eb16e4f395db7eabfc7f4d43de2370b2f8e84258
Michael Evans <michaeljevans113@gmail.com>
2020-08-23 14:32:37 -0600

    Add .movies_from_search() method
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -2,8 +2,12 @@ class MovieFacade
   def self.get_top_movies
     top_40 = MovieService.new.top_40
 
-    top_40.map do |movie_info|
-      Movie.new(movie_info)
-    end
+    top_40.map { |movie_info| Movie.new(movie_info) }
+  end
+
+  def self.movies_from_search(keywords)
+    results = MovieService.new.from_search(keywords)
+
+    results.map { |movie_info| Movie.new(movie_info) }
   end
 end
