deddf56fd77e3e3c6034c4a5bbf7bf377c6459b4
Michael Evans <michaeljevans113@gmail.com>
2020-08-23 16:09:29 -0600

    Refactor to include methods for different API endpoints
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,5 +1,5 @@
 class MovieService
-  def top_40
+  def get_top_40
     page_1 = connection.get('/3/movie/top_rated') do |f|
       f.headers['Content-Type'] = 'application/json'
       f.params['api_key']       = ENV['TMDB_API_KEY']
@@ -17,17 +17,17 @@ class MovieService
     parsed_1[:results] + parsed_2[:results]
   end
 
-  def from_search(keywords)
+  def from_search(terms)
     page_1 = connection.get('/3/search/movie') do |f|
       f.headers['Content-Type'] = 'application/json'
       f.params['api_key']       = ENV['TMDB_API_KEY']
-      f.params['query']         = keywords
+      f.params['query']         = terms
     end
 
     page_2 = connection.get('/3/search/movie') do |f|
       f.headers['Content-Type'] = 'application/json'
       f.params['api_key']       = ENV['TMDB_API_KEY']
-      f.params['query']         = keywords
+      f.params['query']         = terms
       f.params['page']          = '2'
     end
 
@@ -35,6 +35,37 @@ class MovieService
     parsed_2 = JSON.parse(page_2.body, symbolize_names: true)
 
     parsed_1[:results] + parsed_2[:results]
+  end
+
+  def movie_details(movie_id)
+    response = connection.get("/3/movie/#{movie_id}") do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.params['api_key']       = ENV['TMDB_API_KEY']
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def cast_details(movie_id)
+    response = connection.get("/3/movie/#{movie_id}/credits") do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.params['api_key']       = ENV['TMDB_API_KEY']
+    end
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    parsed[:cast]
+  end
+
+  def reviews_details(movie_id)
+    response = connection.get("/3/movie/#{movie_id}/reviews") do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.params['api_key']       = ENV['TMDB_API_KEY']
+    end
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    parsed[:results]
   end
 
   private
