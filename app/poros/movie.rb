2e3d812aff71ccb6593a0610fc61abed1312161e
Jane Greene <janegreenecasa@gmail.com>
2020-08-23 12:51:31 -0600

    Updated movie poro for new endpoints and instance variables needed
    Co-authored-by: Michael Evans <62968371+michaeljevans@users.noreply.github.com>

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,14 +1,21 @@
 class Movie
-  attr_reader :id, :title, :vote_avg
+  attr_reader :id,
+              :title,
+              :genres,
+              :runtime,
+              :vote_avg,
+              :overview
 
   def initialize(movie_info)
-    # genre_array = parsed[:genres].map do |genre|
-    #   genre[:name]
-    # end
-    require "pry"; binding.pry
     @id       = movie_info[:id]
-    @runtime = movie_info[:runtime]
-    @title    = movie_info[:title] || movie_info[:original_title]
+    @title    = movie_info[:title]
+    @genres   = get_genres(movie_info)
+    @runtime = movie_info[:runtime] if movie_info[:runtime]
     @vote_avg = movie_info[:vote_average].to_f
+    @overview = movie_info[:overview]
+  end
+
+  def get_genres(movie_info)
+    movie_info[:genres].map { |genre| genre[:name] } if movie_info[:genres]
   end
 end
