6d26b27d01b4a6999599452908dc85e4a64d8889
Michael Evans <michaeljevans113@gmail.com>
2020-08-23 14:24:42 -0600

    Create movie service that connects to TMDB and returns info based on request
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

---
+++ app/services/movie_service.rb
@@ -0,0 +1,23 @@
+class MovieService
+  def top_40
+    page_1 = connection.get('/3/movie/top_rated') do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.params['api_key']       = ENV['TMDB_API_KEY']
+    end
+
+    page_2 = connection.get('/3/movie/top_rated') do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.params['api_key']       = ENV['TMDB_API_KEY']
+      f.params['page']          = '2'
+    end
+
+    parsed_1 = JSON.parse(page_1.body, symbolize_names: true)
+    parsed_2 = JSON.parse(page_2.body, symbolize_names: true)
+
+    parsed_1[:results] + parsed_2[:results]
+  end
+
+  def connection
+    Faraday.new('https://api.themoviedb.org')
+  end
+end
