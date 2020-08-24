4825e4276b4c0ba6ea71a069519537f42114174d
Josh Tukman <josh.tukman@gmail.com>
2020-08-24 15:50:15 -0600

    Add users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+quentin = User.create!(username: "Quentin", email: "tarantino@gmail.com")
+josh = User.create!(username: "Josh", email: "jt@gmail.com")
+ed = User.create!(username: "Ed", email: "ed@gmail.com")
+larry = User.create!(username: "Larry", email: "larry@gmail.com")
+frank = User.create!(username: "Frank", email: "frank@gmail.com")
