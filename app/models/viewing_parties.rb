71743c7dddd244bcbb6ac440691d54e301e1ac33
Jane Greene <janegreenecasa@gmail.com>
2020-08-25 14:56:27 -0600

    Changed runtime to duration on test and model
    Co-authored-by: Michael Evans <62968371+michaeljevans@users.noreply.github.com>

--- app/models/viewing_parties.rb
+++ app/models/viewing_parties.rb
@@ -1,4 +1,6 @@
 class ViewingParties < ApplicationRecord
-  validates :title, :date, :runtime, :poster, presence: true
+  validates :title, :date, :time, :duration, :poster, presence: true
   belongs_to :user
+
+
 end
--- spec/models/viewing_parties_spec.rb
+++ spec/models/viewing_parties_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe ViewingParties do
   describe 'validations' do
     it { should validate_presence_of :title }
-    it { should validate_presence_of :runtime }
+    it { should validate_presence_of :duration }
     it { should validate_presence_of :poster }
     it { should validate_presence_of :date }
+    it { should validate_presence_of :time }
   end
   describe 'relationships' do
     it { should belong_to :user}
