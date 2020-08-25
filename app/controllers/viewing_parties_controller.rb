c85b9f6e0091c7b455ddcbcf637aee7c892b467e
Jane Greene <janegreenecasa@gmail.com>
2020-08-24 19:26:01 -0600

    Added create method for viewing parties controller
    Co-authored-by: Michael Evans <62968371+michaeljevans@users.noreply.github.com>

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,5 +1,13 @@
 class ViewingPartiesController < ApplicationController
   def new
+    @movie = MovieFacade.new(params[:movie_id])
+  end
+
+  def create
     require "pry"; binding.pry
+    #have
+    #current_user, duration, auth token, movie title, datetime(schema date string), friend_ids
+    #need
+    # poster
   end
 end
