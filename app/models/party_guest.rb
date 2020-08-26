ff167a4e9b9a640ca6f3cdd10a35f33f57b41321
Michael Evans <michaeljevans113@gmail.com>
2020-08-26 14:05:48 -0600

    Clarify variable names
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/models/party_guest.rb
+++ app/models/party_guest.rb
@@ -2,10 +2,10 @@ class PartyGuest < ApplicationRecord
   belongs_to :view_party
   belongs_to :user
 
-  def self.create_invites(party_id, friends)
-    friends.delete('')
-    friends.each do |friend|
-      PartyGuest.create!(view_party_id: party_id, user_id: friend.to_i)
+  def self.create_invites(party_id, friends_ids)
+    friends_ids.delete('')
+    friends_ids.each do |friend_id|
+      PartyGuest.create!(view_party_id: party_id, user_id: friend_id.to_i)
     end
   end
 end
