8aee17cf637fd7015105228e7e3c9a68b2f59481
Phillip Strom <phillip.strom@gmail.com>
2020-08-26 15:37:36 -0600

    Add attending? method

--- app/models/view_party.rb
+++ app/models/view_party.rb
@@ -13,11 +13,15 @@ class ViewParty < ApplicationRecord
     DateTime.new(year, month, day, hour, minute, 0, '-06:00')
   end
 
-
   def end_time
     hours = runtime.to_i / 60
     minutes = runtime.to_i % 60
     start_time.advance(hours: hours, minutes: minutes)
+  end
+
+  def attending?(current_user)
+    user_view_party = self.user_view_parties.where(user_id: current_user.id).first
+    user_view_party.attending
   end
 
 end
