fef95c7258725e31e4aa5cb322e8350d584fd490
Phillip Strom <phillip.strom@gmail.com>
2020-08-26 15:37:12 -0600

    Add attending status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_211146) do
+ActiveRecord::Schema.define(version: 2020_08_26_212347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_211146) do
     t.bigint "view_party_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "attending", default: false
     t.index ["user_id"], name: "index_user_view_parties_on_user_id"
     t.index ["view_party_id"], name: "index_user_view_parties_on_view_party_id"
   end
