83384252af98f29b8845a7852f91fb9d68aa38cb
Michael Evans <michaeljevans113@gmail.com>
2020-08-26 14:04:45 -0600

    Add scope for Google Calendar
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
   ENV['GOOGLE_CLIENT_ID'],
   ENV['GOOGLE_CLIENT_SECRET'],
-  { scope: 'userinfo.email, calendar' }
+  {
+    scope: 'userinfo.email, calendar',
+    access_type: 'offline',
+    prompt: 'consent',
+    select_account: true,
+    skip_jwt: true
+  }
 end
