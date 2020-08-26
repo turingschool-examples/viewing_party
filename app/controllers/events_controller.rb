bbb4dbb1e33e6db75b61652313248e22e2fcd104
Michael Evans <michaeljevans113@gmail.com>
2020-08-26 17:09:12 -0600

    Remove redirect duplication
    Co-authored-by: Jane Greene <37886458+janegreene@users.noreply.github.com>

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,11 +6,10 @@ class EventsController < ApplicationController
     party = ViewParty.find(params[:party_id])
     event = GoogleCalendarService.new.add_to_calendar(current_user, party)
     if event.status == 'confirmed'
-      redirect_to dashboard_path
       flash[:success] = 'Event added to your Google Calendar!'
     else
-      redirect_to dashboard_path
       flash[:error] = 'Error - event NOT added to your calendar.'
     end
+    redirect_to dashboard_path
   end
 end
