3495bfec72a90588ce5406a346080e7abe26266f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-05 20:36:19 -0600

    Add see welcome test to welcome show spec for #15

---
+++ spec/features/welcome/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+include ActionView::Helpers::NumberHelper
+
+RSpec.describe 'Welcome Page' do
+  describe 'As a user' do
+    before :each do
+      visit '/'
+    end
+
+    it "I can see a welcome message" do
+      expect(page).to have_content("Welcome to Viewing Party!")
+    end
+
+
+  end
+end
+
+#  Welcome message
+#  Brief description of the application
+#  Button to Log in
+#  Link to Registration
+# Details: Implement basic auth in the application allowing a user to log in with an email and password. The password should be stored in the database using bcrypt.
