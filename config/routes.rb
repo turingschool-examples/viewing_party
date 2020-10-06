fc27d30f15144d8b03231fe813e62330eeb72680
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-06 16:55:30 -0600

    Add registration and login paths
    Co-authored-by: Luke Hunter James-Erickson <theeluke@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/', to: 'welcome#index'
+  get :root, to: 'welcome#index'
 
-  get '/register', to: 'register#index'
+  get :register, to: 'users#new'
 
-  post '/dashboard', to: 'user/dashboard#index'
+  post '/users', to: 'users#create'
+
+  get "/login", to: "sessions#new"
+  post '/login', to: 'sessions#create'
+
+  namespace :user do
+    get '/dashboard', to: 'dashboard#show'
+  end
+
+  resources :users, only: [:create]
 
 end
