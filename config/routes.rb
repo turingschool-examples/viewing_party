49e7af57216648a552c7fbecce46c58e3b8060ea
Corey Carter <crc2231@gmail.com>
2020-10-05 22:25:05 -0600

    Add new, create, and show routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Users
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
+  get '/dashboard', to: 'users#show'
+
 end
