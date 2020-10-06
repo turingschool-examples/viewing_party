95946114fcd28886c7ca514a54b3797fc2a222f7
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-06 17:02:20 -0600

    Added routes for users create and show action
    Co-authored-by: Travis McKinstry <mckinstrytg@gmail.com>
    Co-authored-by: Norma Lopez <norma_l304@yahoo.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
+
+  get "/registration", to: 'users#new'
+  post "/users", to: 'users#create'
+  get "/dashboard", to: 'users#show'
 end
