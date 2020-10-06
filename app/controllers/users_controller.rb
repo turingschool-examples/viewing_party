86a976805d5d0f27a2a5f78aa226b7ba45ddc5f3
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-06 16:59:30 -0600

    Add users controller with create and params
    Co-authored-by: Luke Hunter James-Erickson <theeluke@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.name}!"
+      redirect_to '/user/dashboard'
+    else
+      generate_flash(@user)
+      render :new
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
