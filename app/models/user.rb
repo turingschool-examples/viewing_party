9786fe5df8131dcc4c498b3e1f91b76257042907
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-06 17:04:15 -0600

    Created users table and model
    Co-authored-by: Travis McKinstry <mckinstrytg@gmail.com>
    Co-authored-by: Norma Lopez <norma_l304@yahoo.com>

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
+end
