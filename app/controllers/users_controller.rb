e1d1d4167ea744a130ffedc77fd4c0b2c3759022
Logan Riffell <lkriffell@gmail.com>
2020-10-05 20:56:26 -0600

    Add login action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,38 @@
 class UsersController < ApplicationController
-
-    def new
-      @user = User.new
-    end
-
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        flash[:success] = "Welcome #{@user.email}! Thank you for registering!"
-        session[:current_user] = @user.id
-        redirect_to "/dashboard"
-      else
-        flash[:error] = @user.errors.full_messages.to_sentence
-        @user.email = "" if @user.errors.details.keys.include?(:email)
-        render :new
-      end
-    end
-
-    def show
-    end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
-  
+  def new
+    @user = User.new
   end
-  
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome #{@user.email}! Thank you for registering!"
+      session[:current_user] = @user.id
+      redirect_to '/dashboard'
+    else
+      flash[:error] = @user.errors.full_messages.to_sentence
+      @user.email = '' if @user.errors.details.keys.include?(:email)
+      render :new
+    end
+  end
+
+  def login
+    @user = User.find_by(email: user_params[:email])
+    if @user && @user.authenticate(user_params[:password])
+      flash[:success] = "Now logged in as #{@user.email}!"
+      session[:current_user] = @user.id
+      redirect_to '/dashboard'
+    else
+      flash[:error] = 'That email/password combonation does not exist in our records'
+      redirect_to '/'
+    end
+  end
+
+  def show; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
