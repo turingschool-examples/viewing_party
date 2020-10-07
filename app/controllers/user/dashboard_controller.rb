bba435966fb6fcd519cad2609fa15ad04cfbf633
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-07 16:00:52 -0600

    Add access to parties via active record method and shorted ... current_user.id to cu_id to appease rubocop's line length laws.  line 4 is still too long but I'm unsure how we could get it much shorter
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class User::DashboardController < ApplicationController
   def show
-    @friends = User.where(id: Friendship.where(user_id: current_user.id).pluck(:friend_id)).or(User.where(id: Friendship.where(friend_id: current_user.id).pluck(:user_id)))
-
-
+    cu_id = current_user.id
+    @friends = User.where(id: Friendship.where(user_id: cu_id).pluck(:friend_id)).or(User.where(id: Friendship.where(friend_id: cu_id).pluck(:user_id)))
+    @parties = Party.where(id: PartyUser.where(user_id: cu_id).pluck(:party_id)).or(Party.where(user_id: cu_id))
   end
 end
