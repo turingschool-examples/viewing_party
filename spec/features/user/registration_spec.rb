d7742ce939ce1737805f21a8a4f1ddbdfe326dc1
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-06 19:18:28 -0600

    Edit form to use form_with, adjust tests

--- spec/features/user/registration_spec.rb
+++ spec/features/user/registration_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe 'User Registration' do
 
       expect(current_path).to eq("/register")
     end
-    
+
     it 'I can register as a user' do
       visit register_path
 
-      fill_in 'Name', with: 'Megan'
-      fill_in 'Email', with: 'megan@example.com'
-      fill_in 'Password', with: 'securepassword'
-      fill_in 'Password confirmation', with: 'securepassword'
+      fill_in 'user[name]', with: 'Megan'
+      fill_in 'user[email]', with: 'megan@example.com'
+      fill_in 'user[password]', with: 'securepassword'
+      fill_in 'user[password_confirmation]', with: 'securepassword'
       click_button 'Register'
 
       expect(current_path).to eq('/user/dashboard')
@@ -27,28 +27,42 @@ RSpec.describe 'User Registration' do
       it 'I do not complete the registration form' do
         visit register_path
 
-        fill_in 'Name', with: 'Megan'
+        fill_in 'user[name]', with: 'Megan'
         click_button 'Register'
 
         expect(page).to have_button('Register')
         expect(page).to have_content("email: [\"can't be blank\"]")
-        expect(page).to have_content("password: [\"can't be blank\"]")
+        expect(page).to have_content("password: [\"can't be blank\", \"can't be blank\"]")
+      end
+
+      it "My password and password confirmation does not match" do
+        visit register_path
+
+        fill_in 'user[name]', with: 'Megan'
+        fill_in 'user[email]', with: 'megan@example.com'
+        fill_in 'user[password]', with: 'securepassword'
+        fill_in 'user[password_confirmation]', with: 'wrongpassword'
+        click_button 'Register'
+
+        expect(page).to have_button('Register')
+
       end
 
       it 'I use a non-unique email' do
-        user = User.create(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
+        user = User.create(name: 'Megan', email: 'megan@example.com', password: 'securepassword', password_confirmation: 'securepassword')
 
         visit register_path
 
-        fill_in 'Name', with: user.name
-        fill_in 'Email', with: user.email
-        fill_in 'Password', with: user.password
-        fill_in 'Password confirmation', with: user.password
+        fill_in 'user[name]', with: user.name
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        fill_in 'user[password_confirmation]', with: user.password
         click_button 'Register'
 
         expect(page).to have_button('Register')
         expect(page).to have_content("email: [\"has already been taken\"]")
       end
+
     end
   end
 end
