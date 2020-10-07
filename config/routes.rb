b1203f3bc3ebc802cbf5e1031b38cb953f7bb698
Alex Desjardins <ardesjardins91@gmail.com>
2020-10-07 15:40:48 -0600

    Visitor can register as a user; happy path and sad paths.
    Co-authored-by: Eric Hale <59599782+EHale64@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'session/create'
-  get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   post '/login', to: 'sessions#create', as: :user_login
+  get '/registration', to: 'users#new', as: :user_registration
+  post '/users', to: 'users#create', as: :user_create
 
   namespace :user do
     get '/dashboard', to: 'dashboard#index'
