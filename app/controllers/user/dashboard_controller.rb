5305cbcd2fe4c58926d1b299ff46ac288dad9341
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-07 14:53:57 -0600

    Updated dashboard controller to allow access to friends
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,3 +1,5 @@
 class User::DashboardController < ApplicationController
-  def show; end
+  def show
+    @friends = Friendship.where(user_id: current_user.id).or(Friendship.where(friend_id: current_user.id))
+  end
 end
