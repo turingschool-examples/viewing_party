ba1de779c07aec39b87f843a81b5ce817f078f90
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-06 19:37:02 -0600

    Create new variables for issue #13 in dashboard spec

--- spec/features/user/dashboard_spec.rb
+++ spec/features/user/dashboard_spec.rb
@@ -4,12 +4,22 @@ include ActionView::Helpers::NumberHelper
 RSpec.describe 'Dashboard Page' do
   describe 'As a user' do
     before :each do
-      @user = User.create(name: 'Jackie Chan', email: 'a@a.com', password: 'a')
+      @user_1 = User.create(name: 'Jackie Chan', email: 'a@a.com', password: 'a')
+      @user_2 = User.create(name: 'Cynthia Rothrock', email: 'b@b.com', password: 'a')
+      @user_3 = User.create(name: 'Michelle Yeoh', email: 'c@c.com', password: 'a')
+      @user_4 = User.create(name: 'Bilbo Baggins', email: 'd@d.com', password: 'a')
+
+      @friendship_1 = Friendship.create(friend_1: @user_1.id, friend_2: @user_2.id)
+      @friendship_2 = Friendship.create(friend_1: @user_1.id, friend_2: @user_3.id)
+
+      @viewing_party_1 = ViewingParty.create(movie_title: "Big City", host: @user_1, date: "June 26th, 1997", time: "12:45pm")
+
+      @party_invites_1 = ViewingParty.create(viewing_party_id: @viewing_party_1.id, user_id: @user_2.id, status: 0)
 
       visit root_path
 
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
+      fill_in 'Email', with: @user_1.email
+      fill_in 'Password', with: @user_1.password
 
       click_button "Log In"
     end
@@ -29,6 +39,12 @@ RSpec.describe 'Dashboard Page' do
       expect(page).to have_button('Discover Movies')
     end
 
-  end
+    it "I can see a section showing my friends" do
+      expect(page).to have_content("Friends:")
+      expect(page).to have_content("Cynthia Rothrock")
+      expect(page).to have_content("Michelle Yeoh")
+      expect(page).to_not have_content("Bilbo Baggins")
+    end
 
+  end
 end
