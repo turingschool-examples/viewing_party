185b79887bb5f5ead95f9c289e11e4b8ee19b2c1
Norma Lopez <norma_l304@yahoo.com>
2020-10-07 16:51:25 -0600

    Add test for registration sad path; if registration form incomplete user will not be registered/created

--- spec/feature/users/registration_spec.rb
+++ spec/feature/users/registration_spec.rb
@@ -13,10 +13,21 @@ RSpec.describe 'User registration page' do
     fill_in :email, with: 'test123@gmail.com'
     fill_in :password, with: 'password'
     fill_in :password_confirmation, with: 'password'
-    
+
     click_button("Register New User")
 
     expect(current_path).to eq('/dashboard')
   end
 
+  it "does not allow registration if form is not complete" do
+    visit '/registration'
+
+    fill_in :email, with: 'test123@gmail.com'
+    fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
+
+    click_button("Register New User")
+
+    expect(page).to have_content("Username can't be blank")
+  end
 end
