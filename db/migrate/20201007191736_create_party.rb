130f49606249643cebd3b37d0ae81e7928a1ba9a
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-07 13:23:35 -0600

    Create party migration

---
+++ db/migrate/20201007191736_create_party.rb
@@ -0,0 +1,10 @@
+class CreateParty < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parties do |t|
+      t.string :movie_title
+      t.references :user, foreign_key: true
+      t.string :date
+      t.string :time
+    end
+  end
+end
