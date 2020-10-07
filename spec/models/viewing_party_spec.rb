9d8cdaef32e9869d7b1e5239a7d7a3baf439a7e2
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-06 21:05:45 -0600

    Add viewing party spec

---
+++ spec/models/viewing_party_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ViewingParty do
+  describe 'Validations' do
+    it {should validate_presence_of :movie_title}
+    it {should validate_presence_of :host}
+    it {should validate_presence_of :date}
+    it {should validate_presence_of :time}
+  end
+end
