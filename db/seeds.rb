581ec94100af90c4f2358c30c5bab42837a08ced
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-06 19:27:28 -0600

    Add password confirmation to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create(name: 'Jackie Chan', email: 'a@a.com', password: 'a')
+user = User.create(name: 'Jackie Chan', email: 'a@a.com', password: 'a', password_confirmation: 'a')
