9f382516284a32a486cece4b182b4f0dbbcbb97b
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-07 15:51:52 -0600

    Refactor @friends active record method to one line in dash controller

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class User::DashboardController < ApplicationController
   def show
-    friendships = Friendship.where(user_id: current_user.id).pluck(:friend_id)
-    friendships << Friendship.where(friend_id: current_user.id).pluck(:user_id)
-    @friends = User.where(id: friendships.flatten)
+    @friends = User.where(id: Friendship.where(user_id: current_user.id).pluck(:friend_id)).or(User.where(id: Friendship.where(friend_id: current_user.id).pluck(:user_id)))
+
+
   end
 end
