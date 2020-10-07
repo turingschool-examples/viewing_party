5722cb4f453c6521cc8bbc69b8f7d45a8377eef5
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-06 20:49:09 -0600

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password, presence: { require: true }
   validates :password_confirmation, presence: { require: true }
+
+  has_many :friendships
+  has_many :friends, :through => :friendships, :source => :friend
 end
