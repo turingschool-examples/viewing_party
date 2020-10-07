b99cf53f6db83c29c7decf283fa293412400d255
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-07 15:30:54 -0600

    Update dashboard controller so it has access to all friends
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class User::DashboardController < ApplicationController
   def show
-    @friends = Friendship.where(user_id: current_user.id).or(Friendship.where(friend_id: current_user.id))
+    friendships = Friendship.where(user_id: current_user.id).pluck(:friend_id)
+    friendships << Friendship.where(friend_id: current_user.id).pluck(:user_id)
+    @friends = User.where(id: friendships.flatten)
   end
 end
