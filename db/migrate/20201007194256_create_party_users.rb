3b4a7bf2c801c220a4ef9dd6de229f29e94fd97d
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-07 13:49:22 -0600

    Update party users migration

--- db/migrate/20201007194256_create_party_users.rb
+++ db/migrate/20201007194256_create_party_users.rb
@@ -3,6 +3,7 @@ class CreatePartyUsers < ActiveRecord::Migration[5.2]
     create_table :party_users do |t|
       t.references :party, foreign_key: true
       t.references :user, foreign_key: true
+      t.integer :status, default: 0
 
       t.timestamps
     end
