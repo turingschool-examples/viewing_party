d825e0b6dd6d9a35f53c1d5646646aa576b0ec4d
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-08 16:14:23 -0600

    Update Movies Controller with ENV API KEY
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,45 @@
 class MoviesController < ApplicationController
+  def index; end
 
-  def index
-    keywords = params[:keywords]
-
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.headers["X-API-Key"] = 'f905331b6149d6abe03629c65fd15007'
+  def search
+    @api_key = ENV['MOVIEDB_API_KEY']
+    if params[:keywords].nil? || params[:keywords] == ''
+      top_40
+    else
+      keywords
     end
-    binding.pry
-    response = conn.get("/3/movie/top_rated?api_key=f905331b6149d6abe03629c65fd15007&language=en-US")
-
-    json = JSON.parse(response.body, symbolize_names: true)
-    @members = json[:results]
   end
 
+  def keywords
+    keywords = params[:keywords]
 
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.headers['X-API-Key'] = @api_key
+    end
+
+    page1 = conn.get("3/search/movie?api_key=#{@api_key}&language=en-US&query=#{keywords}&page=1&include_adult=false&page=1")
+    page2 = conn.get("3/search/movie?api_key=#{@api_key}&language=en-US&query=#{keywords}&page=1&include_adult=false&page=2")
+
+    json1 = JSON.parse(page1.body, symbolize_names: true)
+    json2 = JSON.parse(page2.body, symbolize_names: true)
+
+    movies = json1[:results] + json2[:results]
+
+    @movies_info = movies.pluck(:title).zip(movies.pluck(:vote_average))
+  end
+
+  def top_40
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.headers['X-API-Key'] = @api_key.to_s
+    end
+
+    page1 = conn.get("/3/movie/top_rated?api_key=#{@api_key}&language=en-US&page=1")
+    page2 = conn.get("/3/movie/top_rated?api_key=#{@api_key}&language=en-US&page=2")
+
+    json1 = JSON.parse(page1.body, symbolize_names: true)
+    json2 = JSON.parse(page2.body, symbolize_names: true)
+    movies = json1[:results] + json2[:results]
+
+    @movies_info = movies.pluck(:title).zip(movies.pluck(:vote_average))
+  end
 end
