fb7d67db37aff0306afeacd44c72ba6c5713b477
Corey Carter <crc2231@gmail.com>
2020-10-07 18:19:06 -0600

    Add users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    friend_1 = User.create(email: "a@b.com", password: "password")
+    friend_2 = User.create(email: "b@c.com", password: "password")
