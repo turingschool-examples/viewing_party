de0b5dee650ba327a2780e4d3a20ca62d0c62edc
Norma Lopez <norma_l304@yahoo.com>
2020-10-07 20:59:00 -0600

    Add sad path tests for login. Registered user cannot login if email or password are incorrect

--- spec/feature/users/login_spec.rb
+++ spec/feature/users/login_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "As a registered user" do
-  it "I can visit '/', login with my email and password" do
+  before(:each) do
     visit '/'
     @user = User.create!(username: "eDog", email: "elah@email.com", password: "password")
     click_button("Login")
+  end
+
+  it "I can visit '/', login with my email and password" do
 
     expect(current_path).to eq("/login")
 
@@ -13,5 +16,27 @@ RSpec.describe "As a registered user" do
 
     click_button("Login")
     expect(current_path).to eq("/dashboard")
+  end
+
+  it "I cannot login with incorrect email" do
+    expect(current_path).to eq("/login")
+
+    fill_in :email, with: 'eelah@email.com'
+    fill_in :password, with: @user.password
+
+    click_button("Login")
+    expect(page).to have_content("Sorry, your credentials are bad.")
+    expect(current_path).to eq("/login")
+  end
+
+  it "I cannot login with incorrect email" do
+    expect(current_path).to eq("/login")
+
+    fill_in :email, with: @user.email
+    fill_in :password, with: 'ppassword'
+
+    click_button("Login")
+    expect(page).to have_content("Sorry, your credentials are bad.")
+    expect(current_path).to eq("/login")
   end
 end
