ed06458a6e2481eb8123ea5a100ac9b893176719
Travis McKinstry <mckinstrytg@gmail.com>
2020-10-08 15:56:35 -0600

    Add 'within' part in capybara for more specificity in test
    Co-authored-by: Norma Lopez <norma_l304@yahoo.com>
    Co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>

--- spec/feature/movies/index_spec.rb
+++ spec/feature/movies/index_spec.rb
@@ -22,10 +22,22 @@ RSpec.describe 'movie discover page' do
         it "I should also see the 'Find Top Rated Movies' button and the Find Movies form at the top of the page." do
           json1 = File.read('spec/fixtures/top_40_movies_1.json')
           json2 = File.read('spec/fixtures/top_40_movies_2.json')
+          stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=1").to_return(status: 200, body: json1)
+          stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=2").to_return(status: 200, body: json2)
           visit '/movies'
 
           expect(page).to have_button("Top Rated Movies")
-
+          expect(page).to have_field("Search")
+          expect(page).to have_content("1. ")
+          expect(page).to have_content("40. ")
+          expect(page).to have_link("1.")
+          expect(page).to have_link("40.")
+          expect(page).to have_content("Vote average:")
+          within(first(".movie")) do
+            expect(page).to have_css(".vote")
+            vote = find(".vote").text
+            expect(vote).to_not be_empty
+        end
       end
     end
   end
