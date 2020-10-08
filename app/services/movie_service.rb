3d1ae558312ee528bcd9aea17ea9d468974e8275
Logan Riffell <lkriffell@gmail.com>
2020-10-08 14:49:59 -0600

    Update movie_sercive to fully comply with rubocop's demands

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 class MovieService
   def conn
     Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
-      # faraday.headers["Authorization"] = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZmZhODk2N2Q3NGE0ZGZkYWQyZDUyZGM1Yjk4ZGM0OCIsInN1YiI6IjVmN2JhNWM2OWE2NDM1MDAzNmE4YWM4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lXSxQj059-ewywC8SfzpoCdaB8zvWob3IQ923kQl59U'
-      # faraday.headers["Content-Type"] = 'application/json;charset=utf-8'
     end
   end
 
