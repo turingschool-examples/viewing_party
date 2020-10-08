0c1647b69f67a01d0543ec5e4505aa448cd60804
Logan Riffell <lkriffell@gmail.com>
2020-10-07 18:30:19 -0600

    Update movies controller to collect top 40 movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,15 @@
 class MoviesController < ApplicationController
-  def index; end
+  def index
+    movies = MovieService.new
+    if params[:title]
+      #find the movie by the title while only providing the movie_id to the api
+    else
+      @top_20 = movies.top_40_first_half[:results]
+      @second_top_20 = movies.top_40_second_half[:results]
+    end
+  end
 end
+
+#MovieService class is needed to reference it's methods and get data
+#I put the MovieService class into this file as a workaround until
+#I figured out how to reference it within this file
