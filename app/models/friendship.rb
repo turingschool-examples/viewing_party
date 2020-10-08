63b76c573626b7b8bdcf8703d3cce0e9e981d570
Corey Carter <crc2231@gmail.com>
2020-10-07 18:18:01 -0600

    Refactor check_user method

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,9 +3,12 @@ class Friendship < ApplicationRecord
     belongs_to :friend, class_name: "User"
     validates_uniqueness_of :friend, scope: :user
     validate :check_user
+
+
     def check_user
-        if self.friend_id == self.user_id
-            errors.add(:friend, "can't be yourself")
-        end
+        errors.add(:friend, "can't be yourself") if self.friend_id == self.user_id
     end
+
+    
+
 end
