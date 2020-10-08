38f3a4c10a5c7afabe5bcedecdb18e28b28a38c9
Travis McKinstry <mckinstrytg@gmail.com>
2020-10-08 14:09:48 -0600

    Create method to consume API with Faraday as well as create way to view API results on movies index page
    Co-authored-by: Norma Lopez <norma_l304@yahoo.com>
    Co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,12 @@
 class MoviesController < ApplicationController
   before_action :require_user
 
-  def index; end
+  def index
+    api_key = ENV['MOVIE_API_KEY']
+    conn = Faraday.new(url: 'https://api.themoviedb.org')
+    response = conn.get("/3/movie/top_rated?api_key=#{api_key}&language=en-US&page=1")
+    response2 = conn.get("/3/movie/top_rated?api_key=#{api_key}&language=en-US&page=2")
+    @json = JSON.parse(response.body, symbolize_names: true)
+    @json2 = JSON.parse(response2.body, symbolize_names: true)
+  end
 end
