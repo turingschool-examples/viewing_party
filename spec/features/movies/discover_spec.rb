069f392e085cc7260ad176d4480e649bf291514c
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-08 15:06:42 -0600

    Update discover spec with more robust tests
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -27,22 +27,38 @@ RSpec.describe 'Discover Movies' do
 
       describe "When I enter text in the search field" do
         describe "And click the Search By Movie Title button" do
-          it "I am redirected to the movies page" do
-
+          before :each do
             fill_in 'Keywords', with: 'whatever'
             click_button('Search By Movie Title')
+          end
+
+          it "I am redirected to the movies page" do
             expect(current_path).to eq('/movies')
+          end
+
+          it "I can see movie titles and vote average" do
+            expect(page).to have_content("Whatever Works")
+            expect(page).to have_content("Vote Average: 7.1")
+            expect(page).to have_content("Whatever it Takes")
+            expect(page).to have_content("Vote Average: 6.5")
+          end
+
+          it "I can see 40 movie_info classes" do
+            expect(page).to have_css('ul', :count => 40)
           end
         end
       end
 
       describe "When I do not enter any text in the search field" do
         describe "And click the Search By Movie Title button" do
-          xit "I see an error message indicating I must enter text" do
+          it "I am shown the top 40 movies" do
 
             click_button('Search By Movie Title')
-            expect(current_path).to eq('/discover')
-            expect(page).to have_content('Must enter text to search')
+            expect(current_path).to eq('/movies')
+            expect(page).to have_content("The Green Mile")
+            expect(page).to have_content("Vote Average: 8.5")
+            expect(page).to have_content("Pulp Fiction")
+            expect(page).to have_content("Vote Average: 8.5")
           end
         end
       end
