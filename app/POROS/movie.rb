b3d32c267e546d859598119e2e3f4bacb96e29b1
Travis McKinstry <mckinstrytg@gmail.com>
2020-10-08 15:33:19 -0600

    Create plain old ruby object folder and ruby object to be able to make API call and parse then show it to the movies controller
    Co-authored-by: Norma Lopez <norma_l304@yahoo.com>
    Co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>

---
+++ app/POROS/movie.rb
@@ -0,0 +1,15 @@
+class Movie
+  def get_movies(movie_count_limit)
+    page = 1
+    movie_results = []
+     until movie_results.length >= movie_count_limit
+       api_key = ENV['MOVIE_API_KEY']
+       conn = Faraday.new(url: 'https://api.themoviedb.org')
+       response = conn.get("/3/movie/top_rated?api_key=#{api_key}&language=en-US&page=#{page}")
+       @json = JSON.parse(response.body, symbolize_names: true)
+       @json[:results].each{ |f| movie_results << f}
+       page += 1
+     end
+     return movie_results
+  end
+end
