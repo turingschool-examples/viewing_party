659992dd3706ce0ede9376116ca3a468c68ebd7c
Logan Riffell <61526543+lkriffell@users.noreply.github.com>
2020-10-09 09:57:26 -0600

    Delete 20201006233647_create_friendships.rb

--- db/migrate/20201006233647_create_friendships.rb
+++
@@ -1,8 +0,0 @@
-class CreateFriendships < ActiveRecord::Migration[5.2]
-  def change
-    create_table :friendships do |t|
-      t.integer :friender_id
-      t.integer :friended_id
-    end
-  end
-end
