bd013d5ff80629623f7c94b44b3ac0024ac439ae
Logan Riffell <lkriffell@gmail.com>
2020-10-09 17:27:39 -0600

    Add implementation of vcr into movies/index_spec.rb

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -13,50 +13,55 @@ RSpec.describe 'discover', type: :feature do
       click_on 'Log in'
     end
 
-    it 'has button to discover top 40 movies' do
-      movies = MovieService.new
-      @top_40 = movies.top_40
+      it 'has button to discover top 40 movies' do
+        VCR.use_cassette('top_40_movies') do
+        movies = MovieService.new
+        @top_40 = movies.top_40
 
-      visit '/discover'
-      expect(page).to have_button('Top 40 Movies')
+        visit '/discover'
+        expect(page).to have_button('Top 40 Movies')
 
-      click_button 'Top 40 Movies'
+        click_button 'Top 40 Movies'
 
-      expect(current_path).to eq('/movies')
+        expect(current_path).to eq('/movies')
 
-      expect(@top_40.size).to eq(40)
-      expect(page).to have_content("Gabriel's Inferno Part II")
-      expect(page).to have_content("8.9")
+        expect(@top_40.size).to eq(40)
+        expect(page).to have_content("Gabriel's Inferno Part II")
+        expect(page).to have_content("8.9")
+      end
     end
+      it 'has form to search by movie title' do
+        VCR.use_cassette('happy_movie_search') do
+        movies = MovieService.new
+        @found_movies = movies.find('Hello')
 
-    it 'has form to search by movie title' do
-      movies = MovieService.new
-      @found_movies = movies.find('Hello')
+        visit '/discover'
 
-      visit '/discover'
+        fill_in :title, with: 'Hello'
 
-      fill_in :title, with: 'Hello'
+        click_button 'Search By Title'
 
-      click_button 'Search By Title'
+        expect(current_path).to eq('/movies')
 
-      expect(current_path).to eq('/movies')
-
-      expect(@found_movies.size).to eq(40)
+        expect(@found_movies.size).to eq(40)
+      end
     end
-    it 'returns notice if no movies found' do
-      movies = MovieService.new
-      @found_movies = movies.find('sdsfgwe')
+      it 'returns notice if no movies found' do
+        VCR.use_cassette('sad_movie_search') do
+        movies = MovieService.new
+        @found_movies = movies.find('sdsfgwe')
 
-      visit '/discover'
+        visit '/discover'
 
-      fill_in :title, with: 'sdsfgwe'
+        fill_in :title, with: 'sdsfgwe'
 
-      click_button 'Search By Title'
+        click_button 'Search By Title'
 
-      expect(current_path).to eq('/movies')
+        expect(current_path).to eq('/movies')
 
-      expect(page).to have_content('Sorry, no movies were found.')
-      expect(@found_movies.size).to eq(0)
+        expect(page).to have_content('Sorry, no movies were found.')
+        expect(@found_movies.size).to eq(0)
+      end
     end
   end
 end
