cbc3c7f4c256f0103352b913a30429082483c3f7
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-09 15:46:33 -0600

    Merge conflicts

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,36 +1,7 @@
 class MoviesController < ApplicationController
   def index; end
 
-<<<<<<< HEAD
-  # def index
-  #   keywords = params[:keywords]
-  #
-  #   conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-  #     faraday.headers["X-API-Key"] = '11f680bee00017d74632f717b78d12aa'
-  #   end
-  #
-  #   response = conn.get("/3/search/movie?api_key=11f680bee00017d74632f717b78d12aa&query=#{keywords}")
-  #
-  #   json = JSON.parse(response.body, symbolize_names: true)
-  #
-  # end
-  def index
-    keywords = params[:keywords]
-       conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-         faraday.headers["X-API-Key"] = 'f905331b6149d6abe03629c65fd15007'
-       end
-       page_1 = conn.get("3/search/movie?api_key=f905331b6149d6abe03629c65fd15007&language=en-US&query=#{keywords}&page=1&include_adult=false&page=1")
-       page_2 = conn.get("3/search/movie?api_key=f905331b6149d6abe03629c65fd15007&language=en-US&query=#{keywords}&page=1&include_adult=false&page=2")
-       json_1 = JSON.parse(page_1.body, symbolize_names: true)
-       json_2 = JSON.parse(page_2.body, symbolize_names: true)
-       movies = json_1[:results] + json_2[:results]
-       @movie_info = movies.pluck(:title).zip(movies.pluck(:vote_average))
-       binding.pry
-  end
-
-=======
   def search
-    @api_key = ENV['MOVIEDB_API_KEY']
     if params[:keywords].nil? || params[:keywords] == ''
       top_40
     else
@@ -42,11 +13,11 @@ class MoviesController < ApplicationController
     keywords = params[:keywords]
 
     conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
-      faraday.headers['X-API-Key'] = @api_key
+      faraday.headers['X-API-Key'] = ENV['MOVIEDB_API_KEY']
     end
 
-    page1 = conn.get("3/search/movie?api_key=#{@api_key}&language=en-US&query=#{keywords}&page=1&include_adult=false&page=1")
-    page2 = conn.get("3/search/movie?api_key=#{@api_key}&language=en-US&query=#{keywords}&page=1&include_adult=false&page=2")
+    page1 = conn.get("3/search/movie?api_key=#{ENV['MOVIEDB_API_KEY']}&language=en-US&query=#{keywords}&page=1")
+    page2 = conn.get("3/search/movie?api_key=#{ENV['MOVIEDB_API_KEY']}&language=en-US&query=#{keywords}&page=2")
 
     json1 = JSON.parse(page1.body, symbolize_names: true)
     json2 = JSON.parse(page2.body, symbolize_names: true)
@@ -58,17 +29,17 @@ class MoviesController < ApplicationController
 
   def top_40
     conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
-      faraday.headers['X-API-Key'] = @api_key.to_s
+      faraday.headers['X-API-Key'] = ENV['MOVIEDB_API_KEY']
     end
 
-    page1 = conn.get("/3/movie/top_rated?api_key=#{@api_key}&language=en-US&page=1")
-    page2 = conn.get("/3/movie/top_rated?api_key=#{@api_key}&language=en-US&page=2")
+    page1 = conn.get("/3/movie/top_rated?api_key=#{ENV['MOVIEDB_API_KEY']}&language=en-US&page=1")
+    page2 = conn.get("/3/movie/top_rated?api_key=#{ENV['MOVIEDB_API_KEY']}&language=en-US&page=2")
 
     json1 = JSON.parse(page1.body, symbolize_names: true)
     json2 = JSON.parse(page2.body, symbolize_names: true)
+
     movies = json1[:results] + json2[:results]
 
     @movies_info = movies.pluck(:title).zip(movies.pluck(:vote_average))
   end
->>>>>>> d825e0b6dd6d9a35f53c1d5646646aa576b0ec4d
 end
