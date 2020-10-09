2e2abcaf28fe2696067ed26c0ee32035e4ec99aa
Norma Lopez <norma_l304@yahoo.com>
2020-10-09 11:57:59 -0600

    Display results from search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,19 @@ class MoviesController < ApplicationController
   before_action :require_user
 
   def index
-    @results = Movie.new.get_movies(21)
+    if params[:search]
+      api_key = ENV['MOVIE_API_KEY']
+      conn = Faraday.new(url: 'https://api.themoviedb.org')
+      response = conn.get("/3/search/movie?api_key=#{api_key}&language=en-US&query=#{params[:search]}&page=1&include_adult=false")
+      response2 = conn.get("/3/search/movie?api_key=#{api_key}&language=en-US&query=#{params[:search]}&page=2&include_adult=false")
+      json = JSON.parse(response.body, symbolize_names: true)
+      json2 = JSON.parse(response2.body, symbolize_names: true)
+      first20 = json[:results]
+      first20 << json2[:results]
+      @results = first20.flatten
+    else
+      @results = Movie.new.get_movies(40)
+    end
     # api_key = ENV['MOVIE_API_KEY']
     # conn = Faraday.new(url: 'https://api.themoviedb.org')
     # response = conn.get("/3/movie/top_rated?api_key=#{api_key}&language=en-US&page=1")
