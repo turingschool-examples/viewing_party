63f7e8d9ab953b2d1794ea53f2db1071b2fda5a4
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-09 21:48:31 -0600

    Update schema
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_022958) do
+ActiveRecord::Schema.define(version: 2020_10_10_033226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_10_10_022958) do
     t.string "genre", default: [], array: true
     t.string "summary"
     t.integer "total_reviews"
+    t.integer "movie_db_id"
   end
 
   create_table "parties", force: :cascade do |t|
