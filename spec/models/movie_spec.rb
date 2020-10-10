b6fd0f85dc01b1daecc28c8b9d021c36814093a1
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-09 22:04:25 -0600

    Update movie model spec
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe Movie, type: :model do
   describe 'Validations' do
     it {should validate_presence_of :title}
     it {should validate_presence_of :vote_average}
-    it {should validate_presence_of :genre}
-    it {should validate_presence_of :summary}
-    it {should validate_presence_of :total_reviews}
+    it {should validate_presence_of :movie_db_id}
   end
 end
