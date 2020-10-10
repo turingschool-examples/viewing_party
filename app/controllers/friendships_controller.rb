2a61ad4d7ab5ce63adbf043f70a6ed52af81721e
Luke Hunter James-Erickson <64727217+LHJE@users.noreply.github.com>
2020-10-09 20:05:20 -0600

    Dashboard friends second idea (#34)
    * Add a new user to dashboard spec
    
    * Add test for no friends to dashboard test, and differentiate between ...
    tests with friends and without friends
    
    * Update dashboard show to show "no friends" message when no friends...
    are active.  Also added a section
    
    * Add test to dashboard spec testing for Add Friend button
    
    * Add Add Friend button to dashboard show
    
    * Update dashboard spec with add friend test
    
    * Add field for new friend's email to be added to in dashboard show
    
    * Clean up _resultes_view
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    
    * Clean-up routes
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    
    * Add new friendship route to routes
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Revise add frienship route in dashboard show
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Add FriendshipsController w/ commented out potential fix for a long line
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Add Test to dashboard spec for if the new friend's email doesn't exist..
    in the system
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Updated Friendships Controller for if friend's email doesn't exist ...
    in the system
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Add dashboard test for adding a friend one already has
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Update to test adding a friend you already have
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Add test for when user submits their own email in add new friend in ...
    Friendship Controller
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Update friendships controller with if the email belongs with a friend...
    the user already has
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Update friendship controller to pass edge case testing
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Update wording of error in friendship controller
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Revised dashboard spec with correct language
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Revise dashboard before :each
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Revise friendship create and already_friend in friendships_controller
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    * Update User::DashboardController show method
    
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,13 +4,12 @@ class FriendshipsController < ApplicationController
     if new_friend == []
       flash[:notice] = 'Email Address not in our system.'
     else
-      already_friend = User.where(id: Friendship.where(user_id: new_friend[0].id).pluck(:friend_id)).or(User.where(id: Friendship.where(friend_id: new_friend[0].id).pluck(:user_id))).where(id: current_user.id)
+      already_friend = User.where(id: Friendship.where(user_id: new_friend[0].id).pluck(:friend_id)).where(id: current_user.id)
       if current_user.id == new_friend[0].id
         flash[:notice] = "Well that's your email address! We do like that you're trying to be your own friend though :)"
       elsif already_friend[0].nil?
         Friendship.create(user_id: current_user.id, friend_id: new_friend[0].id)
-        # if this is what we have to do to get that other line longer, then we can implement this feature:
-        # Friendship.create(user_id: friend[0].id, friend_id: current_user.id)
+        Friendship.create(user_id: new_friend[0].id, friend_id: current_user.id)
       else
         flash[:notice] = "They're already your friend!"
       end
