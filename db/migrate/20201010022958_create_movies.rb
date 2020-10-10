067a05a415ca96bd1b21fe508328f9f53f11d01b
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-09 20:47:17 -0600

    Add migration for movies model
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>

---
+++ db/migrate/20201010022958_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.float :vote_average
+      t.string :genre, array: true, default: []
+      t.string :summary
+      t.integer :total_reviews
+    end
+  end
+end
