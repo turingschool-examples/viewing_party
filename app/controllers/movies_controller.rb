74179c238cb52a0e9a211674c702a1395716555f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-10 20:00:56 -0600

    Update with Philip's mocking work
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,24 +5,25 @@ class MoviesController < ApplicationController
     conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.headers['X-API-Key'] = ENV['MOVIEDB_API_KEY']
     end
-    
+
     movie = conn.get("/3/movie/#{params[:id]}?api_key=#{ENV['MOVIEDB_API_KEY']}&language=en-US")
     @movie_details = JSON.parse(movie.body, symbolize_names: true)
 
-<<<<<<< HEAD
-    @runtime = "#{@movie_details[:runtime] / 60}hrs, #{((@movie_details[:runtime].to_f / 60 - @movie_details[:runtime] / 60) * 60).to_i} minutes"
+    find_runtime(@movie_details[:runtime])
 
     credits = conn.get("/3/movie/#{params[:id]}/credits?api_key=#{ENV['MOVIEDB_API_KEY']}")
+    #maybe this instead of the next two lines: @actors = JSON.parse(credits.body, symbolize_names: true)[:cast].take(10)
     credits_details = JSON.parse(credits.body, symbolize_names: true)
     @actors = credits_details[:cast].take(10)
 
     reviews = conn.get("/3/movie/#{params[:id]}/reviews?api_key=#{ENV['MOVIEDB_API_KEY']}&language=en-US&page=1")
     @reviews_details = JSON.parse(reviews.body, symbolize_names: true)[:results]
-=======
-    movie = conn.get("/3/movie/#{params[:id]}?api_key=#{ENV['MOVIEDB_API_KEY']}&language=en-US")
-    @movie_details = JSON.parse(movie.body, symbolize_names: true)
+  end
 
->>>>>>> 6372146da6456d33cfdabc8552797114345fe7be
+  def find_runtime(runtime)
+    hours = runtime / 60
+    minutes = ((runtime.to_f / 60 - runtime / 60) * 60).to_i
+    @runtime = "#{hours}hrs, #{minutes} minutes"
   end
 
   def search
@@ -44,18 +45,6 @@ class MoviesController < ApplicationController
     conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.headers['X-API-Key'] = ENV['MOVIEDB_API_KEY']
     end
-<<<<<<< HEAD
-
-    page1 = conn.get("/3/search/movie?api_key=#{ENV['MOVIEDB_API_KEY']}&language=en-US&query=#{keywords}&page=1")
-    page2 = conn.get("/3/search/movie?api_key=#{ENV['MOVIEDB_API_KEY']}&language=en-US&query=#{keywords}&page=2")
-
-    json1 = JSON.parse(page1.body, symbolize_names: true)
-    json2 = JSON.parse(page2.body, symbolize_names: true)
-
-    make_movies(json1, json2)
-
-    @movies_info = Movie.all
-=======
     until results.length >= 2
       res = conn.get("/3/movie/top_rated?api_key=#{ENV['MOVIEDB_API_KEY']}&language=en-US&page=#{page}") do |f|
         f.params['page'] = page
@@ -65,7 +54,6 @@ class MoviesController < ApplicationController
       page += 1
     end
     @movies_info = results.flatten
->>>>>>> 6372146da6456d33cfdabc8552797114345fe7be
   end
 
   def get_movies(movie_count_limit)
