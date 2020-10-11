317e5a82fd09504d62db2cdb53cdd6da19ce4106
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-11 15:26:53 -0600

    Add controller for parties

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,7 +1,9 @@
 class PartiesController < ApplicationController
 
   def new
+    binding.pry
     @movie_title = params[:movie_title].titleize
+    @movie_runtime = params[:movie_runtime]
   end
 
 end
