6372146da6456d33cfdabc8552797114345fe7be
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-10 19:26:52 -0600

    Update schema with Movies table removed
    Co-Authored-By: Philip DeFraties <philipjfry338@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_035055) do
+ActiveRecord::Schema.define(version: 2020_10_11_012350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,12 +20,6 @@ ActiveRecord::Schema.define(version: 2020_10_10_035055) do
     t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.float "vote_average"
-    t.integer "movie_db_id"
   end
 
   create_table "parties", force: :cascade do |t|
