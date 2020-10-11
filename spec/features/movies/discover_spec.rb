b6ef0cec87571d99cc9f669cf417e5a590fa8ddd
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-10 19:22:31 -0600

    Updating tests according to VCR update
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -15,44 +15,6 @@ RSpec.describe 'Discover Movies' do
         expect(page).to have_field('keywords')
         expect(page).to have_button('Search By Movie Title')
       end
-
-      describe "When I click the Discover Top 40 button" do
-        before :each do
-          click_button('Discover Top 40')
-        end
-
-        it "I am redirected to the movies page" do
-
-          expect(current_path).to eq('/movies')
-        end
-
-        it "I click on movie title" do
-
-          expect(page).to have_link("Gabriel's Inferno Part II")
-        end
-
-        it "I click on movie title" do
-          click_link "Gabriel's Inferno Part II"
-
-          expect(current_path).to eq("/movies/724089")
-        end
-      end
-
-      describe "When I enter text in the search field" do
-        describe "And click the Search By Movie Title button" do
-          before :each do
-            fill_in 'Keywords', with: 'the'
-            click_button('Search By Movie Title')
-          end
-
-          it "I can see movie titles and vote average" do
-            expect(page).to have_content("The Boys in the Band")
-            expect(page).to have_content("Vote Average: 6.8")
-            expect(page).to have_content("Before the Fire")
-            expect(page).to have_content("Vote Average: 6.2")
-          end
-        end
-      end
     end
   end
 end
