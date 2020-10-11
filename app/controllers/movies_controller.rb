316c4ae6812bcac3eea85b44a473ab1d3c94992d
Corey Carter <crc2231@gmail.com>
2020-10-11 16:06:39 -0600

    Refactor index, add show action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,14 @@
 class MoviesController < ApplicationController
   def index
-    movies = MovieService.new
     if params[:title]
-      @found_movies = movies.find(params[:title])
-      flash[:alert] = 'Sorry, no movies were found.' if @found_movies == []
+      @movies = MovieFacade.find(params[:title])
+      flash[:alert] = 'Sorry, no movies were found.' if @movies == []
     else
-      @top_40 = movies.top_40
+      @movies = MovieFacade.top_40
     end
+  end
+
+  def show
+    @movie = MovieFacade.movie_details(params[:id]).first
   end
 end
