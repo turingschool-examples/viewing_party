3065dd74c61cba4bf9823da8c650ae3fbfd057b7
Travis McKinstry <mckinstrytg@gmail.com>
2020-10-11 11:41:19 -0600

    Create party user model again. Party user is joins  table for a movie party and users (attendees)
    Co-authored-by: Norma Lopez <norma_l304@yahoo.com>
    Co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>

--- app/models/movie_party.rb
+++ app/models/movie_party.rb
@@ -3,6 +3,11 @@ class MovieParty < ApplicationRecord
   validates :duration, presence: true
   validates :date, presence: true
   validates :start_time, presence: true
-  belongs_to :user
-  has_many :friends, class_name: 'User', dependent: :destroy
+  belongs_to :user # host
+  has_many :party_users #joins table
+  has_many :users, through: :party_users # attendees
+
+  def find_friends
+    User.where(id: (PartyUser.where(movie_party_id: self[:id]).pluck(:user_id)))
+  end
 end
