63c6018969bae29c29e7d8430ee6397054f35ff6
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-11 22:54:08 -0600

    Add create action

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,9 +1,14 @@
 class PartiesController < ApplicationController
 
   def new
-    binding.pry
-    @movie_title = params[:movie_title].titleize
-    @movie_runtime = params[:movie_runtime]
   end
 
+  def create
+    party = current_user.parties.create(movie_title: params[:movie_title], date: params[:party_date], time: params[:start_time])
+
+    params[:invitees].each do |user_id|
+      party.party_users.create(party_id: party.id, user_id: user_id)
+    end
+    redirect_to '/user/dashboard'
+  end
 end
