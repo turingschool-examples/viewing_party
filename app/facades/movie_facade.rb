136cf316bafe904a1dbaefe63877e2af57822f53
Corey Carter <crc2231@gmail.com>
2020-10-11 22:30:20 -0600

    Add movie_details action

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,23 +1,22 @@
 class MovieFacade
-
   def self.movie_details(movie_id)
-    json = MovieService.get_details(movie_id)
-    @movie = Movie.new(json)
+    details_json = MovieService.get_details(movie_id)
+    cast_json = MovieService.get_cast(movie_id)
+    reviews_json = MovieService.get_reviews(movie_id)
+    @movie = Movie.new(details_json, cast_json, reviews_json)
   end
 
   def self.top_40
     json = MovieService.find_top_40
     @movies = json.map do |movie_data|
-      movie = Movie.new(movie_data)
+      Movie.new(movie_data)
     end
   end
 
   def self.find(title)
     json = MovieService.find_title(title)
     @movies = json.map do |movie_data|
-      movie = Movie.new(movie_data)
+      Movie.new(movie_data)
     end
   end
-
-  
 end
