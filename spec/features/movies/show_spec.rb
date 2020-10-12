67ae66be5c56c0f76078ac1683e0a5ca2fd48177
Corey Carter <crc2231@gmail.com>
2020-10-11 22:32:13 -0600

    Add show spec using hard-code, need to implement vcr/webmock

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -15,25 +15,29 @@ RSpec.describe 'movie details page', type: :feature do
 
     it 'can show details' do
 
-      visit "/movies/5"
+      visit "/movies/500"
 
      
       expect(page).to have_button('Create Viewing Party!')
-      expect(page).to have_content("#{movie.title}")
-      expect(page).to have_content("#{movie.vote_average}")
-      expect(page).to have_content("#{movie.runtime}")
-      expect(page).to have_content("#{movie.genre}")
-      expect(page).to have_content("#{movie.summary}")
-      expect(page).to have_content("#{movie.cast}")
-      expect(page).to have_content("#{movie.review_count}")
-      expect(page).to have_content("#{movie.reviews}")
-# Details: This information should come from 3 different endpoints from The Movie DB API
-
-
-
-      expect(current_path).to eq('/movies')
-
-      expect(@found_movies.size).to eq(40)
+      expect(page).to have_content("Reservoir Dogs")
+      expect(page).to have_content("8.2")
+      expect(page).to have_content("1 hour(s) and 39 minutes")
+      expect(page).to have_content("Crime")
+      expect(page).to have_content("Thriller")
+      expect(page).to have_content("A botched robbery indicates a police informant, and the pressure mounts")
+      within '.cast' do
+        # expect(@movie.cast.count).to eq(10)
+        expect(page).to have_content("Harvey Keitel as Mr. White / Larry Dimmick")
+        expect(page).to have_content("Tim Roth as Mr. Orange / Freddy Newandyke")
+        expect(page).to have_content("Michael Madsen as Mr. Blonde / Vic Vega")
+      end
+      within '.reviews' do
+        expect(page).to have_content("2")
+        expect(page).to have_content("talisencrw")
+        expect(page).to have_content("This unique take on the heist-film-gone-wrong")
+        expect(page).to have_content("Wuchak")
+        expect(page).to have_content("The cuss-oriented squabbles of lowlife crooks")
+      end
     end
   end
 end
