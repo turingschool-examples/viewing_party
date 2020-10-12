c138820b242cf714f4d1b6624dbeb8d676cb7d64
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-11 22:53:46 -0600

    Add relationship to user to party model

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,6 +1,9 @@
 class Party < ApplicationRecord
   # alias_method :host, :user_id
   # belongs_to :host, class_name: 'User'
+  belongs_to :user
+  has_many :party_users
+  has_many :users, through: :party_user
 
   validates :movie_title,
             :user_id,
