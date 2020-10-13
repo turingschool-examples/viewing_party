092c4fc167120273bbc05f83153fe9d8769286dc
Corey Carter <crc2231@gmail.com>
2020-10-13 16:47:09 -0600

    Comment out unused method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def generate_flash(resource)
-    resource.errors.messages.each do |validation, message|
-      flash[validation] = "#{validation}: #{message}"
-    end
-  end
+  # def generate_flash(resource)
+  #   resource.errors.messages.each do |validation, message|
+  #     flash[validation] = "#{validation}: #{message}"
+  #   end
+  # end
 
   def current_user
     session[:current_user]
