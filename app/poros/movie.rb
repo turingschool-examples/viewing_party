d39e7cc039be85fc3880fab4c647238106b1ab8d
Corey Carter <crc2231@gmail.com>
2020-10-12 22:10:05 -0600

    Add if statement to handle empty genre

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -15,14 +15,20 @@ class Movie
     @vote_avg = movie_data[:vote_average]
     @runtime = movie_data[:runtime]
     @summary = movie_data[:overview]
-    @genres = movie_data[:genres].map do |genre|
-      Genre.new(genre)
+    if movie_data[:genres]
+      @genres = movie_data[:genres].map do |genre|
+        Genre.new(genre)
+      end
     end
-    @cast = cast_data.map do |actor|
-      Actor.new(actor)
+    if cast_data
+      @cast = cast_data.map do |actor|
+        Actor.new(actor)
+      end
     end
-    @reviews = review_data.map do |review|
-      Review.new(review)
+    if review_data
+      @reviews = review_data.map do |review|
+        Review.new(review)
+      end
     end
   end
 
