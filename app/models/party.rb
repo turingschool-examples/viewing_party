b71bb12ba28a0d17be05c92b22ef5ca9d187cddb
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-12 22:19:50 -0600

    Add user_status method

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,10 +3,18 @@ class Party < ApplicationRecord
   # belongs_to :host, class_name: 'User'
   belongs_to :user
   has_many :party_users
-  has_many :users, through: :party_user
+  has_many :users, through: :party_users
 
   validates :movie_title,
             :user_id,
             :date,
             :time, presence: true
+
+  def user_status(user_id)
+    if self.user_id == user_id
+      "Host"
+    else
+      "Invited"
+    end
+  end
 end
