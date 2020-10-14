2c805be14dade94a845a085669f50ba97e618c00
Norma Lopez <norma_l304@yahoo.com>
2020-10-13 19:02:46 -0600

    Add spec for details of a specific movie

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -46,4 +46,36 @@ RSpec.describe MovieService do
     expect(next_result).to have_key :overview
     expect(next_result[:overview]).to be_an(String)
   end
+
+  it 'fetch details for specific movie' do
+    first_movie = JSON.parse(File.read('spec/fixtures/first_movie_link.json'), symbolize_names: true)
+    first_movie_cast = JSON.parse(File.read('spec/fixtures/first_movie_cast.json'), symbolize_names: true)
+
+    first_movie_reviews = JSON.parse(File.read('spec/fixtures/first_movie_reviews.json'), symbolize_names: true)
+    json5 = File.read('spec/fixtures/first_movie_reviews.json')
+    json1 = File.read('spec/fixtures/top_40_movies_1.json')
+    json2 = File.read('spec/fixtures/top_40_movies_2.json')
+    json3 = File.read('spec/fixtures/first_movie_link.json')
+    json4 = File.read('spec/fixtures/first_movie_cast.json')
+
+
+    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=1").to_return(status: 200, body: json1)
+    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=2").to_return(status: 200, body: json2)
+    stub_request(:get, "https://api.themoviedb.org/3/movie/#{first_movie[:id]}?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US").to_return(status: 200, body: json3)
+    stub_request(:get, "https://api.themoviedb.org/3/movie/#{first_movie[:id]}/credits?api_key=#{ENV['MOVIE_API_KEY']}").to_return(status: 200, body: json4)
+    stub_request(:get, "https://api.themoviedb.org/3/movie/#{first_movie[:id]}/reviews?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=1").to_return(status: 200, body: json5)
+
+    search_results = MovieService.get_specific_movie(first_movie[:id])
+
+    expect(search_results).to be_a(Hash)
+
+    expect(search_results).to have_key :title
+    expect(search_results[:title]).to be_a(String)
+    expect(search_results).to have_key :vote_average
+    expect(search_results[:vote_average]).to be_a(Float)
+    expect(search_results).to have_key :id
+    expect(search_results[:id]).to be_a(Integer)
+    expect(search_results).to have_key :overview
+    expect(search_results[:overview]).to be_an(String)
+  end
 end
