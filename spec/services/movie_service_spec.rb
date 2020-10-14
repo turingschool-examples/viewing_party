76041da74454ed386c6b134ac50e42784aee1c65
Norma Lopez <norma_l304@yahoo.com>
2020-10-13 18:47:32 -0600

    Add movie service spec file with spec for search query

---
+++ spec/services/movie_service_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe MovieService do
+  it 'fetch movies by search query' do
+    search1 = File.read('spec/fixtures/search_movies_1.json')
+    search2 = File.read('spec/fixtures/search_movies_2.json')
+
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIE_API_KEY']}&include_adult=false&language=en-US&page=1&query=Pirates").to_return(status: 200, body: search1)
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIE_API_KEY']}&include_adult=false&language=en-US&page=2&query=Pirates").to_return(status: 200, body: search2)
+
+    search_results = MovieService.search_movies('Pirates')
+
+    expect(search_results).to be_an(Array)
+
+    next_result = search_results.first
+
+    expect(next_result).to have_key :title
+    expect(next_result[:title]).to be_a(String)
+    expect(next_result).to have_key :vote_average
+    expect(next_result[:vote_average]).to be_a(Float)
+    expect(next_result).to have_key :id
+    expect(next_result[:id]).to be_a(Integer)
+    expect(next_result).to have_key :overview
+    expect(next_result[:overview]).to be_an(String)
+  end
+end
