7b4d63cd7b76bd42c8e062d6ee8f6886e07b8e4a
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-13 19:18:47 -0600

    Add tests for model methods

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe Party, type: :model do
     it {should validate_presence_of :date}
     it {should validate_presence_of :time}
   end
+
+    describe "if a party is made" do
+      before :each do
+        @user_1 = User.create(name: 'Jackie Chan', email: 'a@a.com', password: 'a', password_confirmation: 'a')
+        @user_2 = User.create(name: 'Cynthia Rothrock', email: 'b@b.com', password: 'b', password_confirmation: 'b')
+        @user_3 = User.create(name: 'Michelle Yeoh', email: 'c@c.com', password: 'c', password_confirmation: 'c')
+        @user_4 = User.create(name: 'Bilbo Baggins', email: 'd@d.com', password: 'd', password_confirmation: 'd')
+        @party_1 = Party.create(movie_title: "The Exorcist III", user_id: @user_1.id, date: "October 26th, 1997", time: "1:45pm")
+      end
+
+      it "can determine a users status" do
+        expect(@party_1.user_status(@user_1.id)).to eq('Host')
+        expect(@party_1.user_status(@user_2.id)).to eq('Invited')
+        expect(@party_1.user_status(@user_3.id)).to eq('Invited')
+        expect(@party_1.user_status(@user_4.id)).to eq('Invited')
+      end
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe User do
     it {should validate_uniqueness_of :email}
     it {should validate_presence_of :password}
   end
+
+  it "can find all parties a user is invited to or hosting" do
+    @user_1 = User.create(name: 'Jackie Chan', email: 'a@a.com', password: 'a', password_confirmation: 'a')
+    @user_2 = User.create(name: 'Cynthia Rothrock', email: 'b@b.com', password: 'b', password_confirmation: 'b')
+    @user_3 = User.create(name: 'Michelle Yeoh', email: 'c@c.com', password: 'c', password_confirmation: 'c')
+
+    @party_1 = Party.create(movie_title: "The Exorcist III", user_id: @user_1.id, date: "October 26th, 1997", time: "1:45pm")
+    @party_2 = Party.create(movie_title: "Psycho II", user_id: @user_1.id, date: "June 2nd, 1998", time: "2:45pm")
+    @party_4 = Party.create(movie_title: "House II: The Second Story", user_id: @user_2.id, date: "April 22nd, 2000", time: "4:45pm")
+    @party_5 = Party.create(movie_title: "The Gate II", user_id: @user_2.id, date: "January 19nd, 2001", time: "5:45pm")
+
+    @party_user_7 = PartyUser.create(party_id: @party_4.id, user_id: @user_1.id, status: 1)
+
+    expect(@user_1.all_parties).to eq([@party_1, @party_2, @party_4])
+    expect(@user_1.all_parties).to_not include(@party_5)
+  end
 end
