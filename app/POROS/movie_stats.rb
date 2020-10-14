f49dd74005febbee85cf841a09fefe5a048076e1
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-13 20:19:05 -0600

    created movie stats poro spec

--- app/POROS/movie_stats.rb
+++ app/POROS/movie_stats.rb
@@ -2,6 +2,6 @@ class MovieStats
   def self.calculate_time(movie)
     hours = movie.runtime / 60
     minutes = movie.runtime % 60
-    hours > 1 ? "#{hours} hours and #{minutes}" : "#{hours} hour and #{minutes}"
+    hours > 1 ? "#{hours} hours and #{minutes} minutes" : "#{hours} hour and #{minutes} minutes"
   end
 end
