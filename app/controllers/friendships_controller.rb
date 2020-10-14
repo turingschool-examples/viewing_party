b02b8a77dfce3390709ec48883057745beece179
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-14 15:23:23 -0600

    user cannot add themselves as a friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,7 +4,9 @@ class FriendshipsController < ApplicationController
     friend = User.find_by(email: params[:email])
     friendship1 = Friendship.new(user: user, friend: friend)
     friendship2 = Friendship.new(user: friend, friend: user)
-    if friendship1.save && friendship2.save
+    if friendship1.user == user && friendship1.friend == user
+      flash[:notice] = 'Sorry, you cannot add yourself as a friend'
+    elsif friendship1.save && friendship2.save
       flash[:success] = 'Friend has been added'
     else
       flash[:notice] = 'Sorry, friend cannot be found'
