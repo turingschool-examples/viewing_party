dee6437ccabb6c8072d69ad39657568d1e3aba93
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-01 16:32:38 -0700

    Update the welcome spec to test for description

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe "as a user" do
   describe "when I visit '/'" do 
     it 'I see a welcome message' do
       visit '/'
-
+save_and_open_page
       expect(page).to have_content("Welcome to Viewing Party")
-      expect(page).to have_content("Description")
+      expect(page).to have_content("This application allows you and your friends to have a movie watch party during a pandemic, and give you the comfort to watch from home.")
     end
   end
 end
