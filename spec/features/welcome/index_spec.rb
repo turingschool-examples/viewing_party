b7000ca3c7e7c7ea5d2a7db5c82ff6ac8ebc2ca1
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-01 16:59:30 -0700

    Add test for login link

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,9 +4,15 @@ RSpec.describe "as a user" do
   describe "when I visit '/'" do 
     it 'I see a welcome message' do
       visit '/'
-save_and_open_page
+
       expect(page).to have_content("Welcome to Viewing Party")
       expect(page).to have_content("This application allows you and your friends to have a movie watch party during a pandemic, and give you the comfort to watch from home.")
+    end
+    it 'I see a button to login and link to register' do
+      visit '/'
+
+      expect(page).to have_button("Login")
+      expect(page).to have_link("Register")
     end
   end
 end
