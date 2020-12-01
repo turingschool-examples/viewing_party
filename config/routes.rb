5f602b39602e263acedf628a6161559db7a4f33a
Zach Stearns <zach.stearns92@gmail.com>
2020-11-30 20:55:06 -0700

    Add routes for sessions, still need to test and build upon

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
