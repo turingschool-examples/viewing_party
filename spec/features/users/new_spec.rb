4925ac69f07bb8c1c40ee894f490fae722f431ae
Brian Liu <brian.b.liu@gmail.com>
2020-12-02 14:49:10 -0700

    Add sad path tests for registration, revise registration test fillin params

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -8,14 +8,35 @@ describe "As a visitor" do
 
       expect(current_path).to eq('/registration')
 
-      fill_in :username, with: "John Doe"
-      fill_in :email, with: "john@doe.com"
-      fill_in :password, with: "password"
-      fill_in :password_confirmation, with: "password"
+      fill_in "user[username]", with: "John Doe"
+      fill_in "user[email]", with: "john@doe.com"
+      fill_in "user[password]", with: "password"
+      fill_in "user[password_confirmation]", with: "password"
       click_on 'Create User'
 
-      expect(current_path).to eq('/dashboard')
+      expect(current_path).to eq('/user/dashboard')
       expect(page).to have_content('Welcome John Doe!')
+    end
+  end
+
+  describe "When I try to register with an email or username that already exists" do
+    it "I am given a message and returned to the registration form" do
+      user = User.create!(
+        username: "JonathonDoe",
+        email: "jonathon@doe.com",
+        password: "1234"
+      )
+      visit '/'
+      click_link("New to Viewing Party? Register Here.")
+
+      expect(current_path).to eq('/registration')
+
+      fill_in "user[username]", with: user.username
+      fill_in "user[email]", with: user.email
+      fill_in "user[password]", with: user.password
+      fill_in "user[password_confirmation]", with: user.password
+      click_on 'Create User'
+      expect(page).to have_content('Username has already been taken and Email has already been taken')
     end
   end
 end
