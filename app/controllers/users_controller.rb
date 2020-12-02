a6566543fb6dc58896f6204a21c003712fed6614
Brett Turing <bjsherman80@gmail.com>
2020-12-01 18:42:38 -0700

    create show/create actions and private params
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>
    
    Co-Authored-By: Jake Heft <60531761+jakeheft@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,32 @@ class UsersController < ApplicationController
   def new
   end
 
-  
+  def show
+    @user = User.find(session[:user_id])
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome #{user.name}"
+      redirect_to '/dashboard'
+    elsif user.duplicate_email?
+      flash[:notice] = user.errors.full_messages.uniq
+      render :new
+    elsif params[:password] != params[:password_confirmation]
+      flash[:failure] = "Password and Password Confirmation fields did not match."
+      render :new
+    else
+      flash[:failure] = "You are missing required fields."
+      render :new
+    end
+  end
+
+private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
