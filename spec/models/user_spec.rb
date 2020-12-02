ed362ea82c3d74add4ce43e867dad39cfd05e1f7
Brett Turing <bjsherman80@gmail.com>
2020-12-01 18:47:35 -0700

    Create validations test for user
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>
    
    Co-Authored-By: Jake Heft <60531761+jakeheft@users.noreply.github.com>

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe "validations" do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:password)}
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
+  end
+
+end
