44aa9ef1c603d97b94c2d2035691f87964138fd0
Zach Stearns <zach.stearns92@gmail.com>
2020-12-01 18:59:17 -0700

    change to dashboard index route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
-  get '/dashboard', to: 'users#show'
+  get '/dashboard', to: 'dashoard#index'
 end
