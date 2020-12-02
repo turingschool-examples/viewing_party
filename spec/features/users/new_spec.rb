9ef844fe51bfc3017e75a8fae8737a5662218b90
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-01 20:49:54 -0700

    Remove save and open page

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -41,7 +41,7 @@ describe 'User Registration' do
       fill_in 'Password confirmation', with: 'password'
 
       click_button 'Register'
-save_and_open_page
+
       expect(page).to have_content("Email has already been taken")
     end
 
