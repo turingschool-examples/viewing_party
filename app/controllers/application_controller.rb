c62f5d72bb2413e6aad3c62eeed33a044e55ecf2
Jake Heft <jakeheft@gmail.com>
2020-12-02 14:05:39 -0700

    Add method require_user, commented out for now
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman 
    <64512380+BJSherman80@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  # def require_user
+  #   render file: '/public/401' unless current_user
+  # end
+
 end
