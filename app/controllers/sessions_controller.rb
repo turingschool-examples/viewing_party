dc5cd0a552a8d6f20d83e08f9659c90c336e6930
Jake Heft <jakeheft@gmail.com>
2020-12-02 14:05:12 -0700

    Add destroy method to sessions controller, refactor using path helper
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman 
    <64512380+BJSherman80@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,21 @@
 class SessionsController < ApplicationController
 
-def create
-  user = User.find_by(email: params[:email])
-  if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    flash[:success] = "Welcome, #{user.name}!"
-    redirect_to '/dashboard'
-  else
-    flash[:error] = "Sorry, your credentials are bad."
-    redirect_to '/'
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}!"
+      redirect_to '/dashboard'
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      redirect_to root_path
+    end
   end
-end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = "You've been successfully logged out"
+    redirect_to root_path
+  end
 
 end
