85a0897fc80931a81eb76eced128f7cfc2fa0a69
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-01 18:39:04 -0700

    Add password_confirmation to private params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :email, :password)
+    params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
