5e37aee137f83c2b69f2fd0f89517a82a614cccd
Brian Liu <brian.b.liu@gmail.com>
2020-12-02 14:52:27 -0700

    Add methods generate flash message and current user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def generate_flash(resource)
+    flash[:errors] = resource.errors.full_messages.to_sentence
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
