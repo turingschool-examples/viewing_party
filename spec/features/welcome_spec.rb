b3aa67f1e593f507e985002d903f1a2bc2814b14
Curtis Bartell <cbartell980@gmail.com>
2020-12-01 19:38:08 -0700

    Add test for user registration link

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -7,4 +7,10 @@ describe "Welcome Page:" do
     expect(page).to have_content("Welcome to Viewing Party!")
     expect(page).to have_content("Description")
   end
+
+  it 'it has a link to register a new user' do
+    visit '/'
+
+    expect(page).to have_link("New to Viewing Party? Register Here.")
+  end
 end
