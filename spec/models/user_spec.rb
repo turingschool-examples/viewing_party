0b6f30c87671e9805f35f2b9dea54003c2a8058e
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-01 18:38:44 -0700

    Add test and validation for confirmation of password in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,6 @@ describe User, type: :model do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
     it { should validate_presence_of :password }
+    it { should validate_confirmation_of :password }
   end
 end
