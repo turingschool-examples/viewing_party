0f408dc4640b8776be2d68518fc4e0c7360daed9
Will Dunlap <dunlapww@gmail.com>
2020-12-02 15:43:40 -0700

    Add vcr and webmock gems
    Co-authored-by: Kate Tester <Katemorris@gmail.com>
    Co-authored-by: George Soderholm <Georgesoderholm@gmail.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,4 +69,9 @@ RSpec.configure do |config|
     end
   end
   
+  VCR.configure do |config|
+    config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+    config.hook_into :webmock
+  end
+
 end
