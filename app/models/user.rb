1a9a3c57daa244df2c4288e1dc50ceed3be53d57
Kate Tester <katemorris@gmail.com>
2020-12-01 18:50:44 -0700

    Adds model for User
    Co-authored-by: George Soderholm <georgesoderholm@gmail.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates_presence_of :email
+  validates_presence_of :password
+  validates_presence_of :password_confirmation
+  validates_uniqueness_of :email
+  has_secure_password
+
+end
