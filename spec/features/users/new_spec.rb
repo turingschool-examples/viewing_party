0f6831b7188b0ebf6c87058eeab1c2ce4361da92
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-01 18:16:34 -0700

    Udate test for creating new user

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -5,6 +5,8 @@ describe 'User Registration' do
     it 'shows a form to register as a new user, and when I enter valid information I am redirected to the dashboard' do
       visit '/registration'
 
+      fill_in 'First name', with: 'Sam'
+      fill_in 'Last name', with: 'Smith'
       fill_in 'Email', with: 'sam@email.com'
       fill_in 'Password', with: 'password'
       fill_in 'Password confirmation', with: 'password'
@@ -12,10 +14,19 @@ describe 'User Registration' do
       click_button 'Register'
 
       expect(current_path).to eq('/dashboard')
+      expect(page).to have_content('Welcome, Sam!')
     end
 
-    xit 'displays an error if the email is blank' do
+    it 'displays an error if any required field is left blank' do
+      visit '/registration'
 
+      fill_in 'First name', with: 'Sam'
+
+      click_button 'Register'
+
+      expect(page).to have_content("Last name can't be blank")
+      expect(page).to have_content("Email can't be blank")
+      expect(page).to have_content("Password can't be blank")
     end
 
     xit 'displays an error if the email provided already exists in the database' do
