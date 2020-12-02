ecaa36372ffc35a9f156272c66f92b73c4f9c08e
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-01 18:17:34 -0700

    Add action to create new user with flash messaging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.first_name}!"
+      redirect_to '/dashboard'
+    else
+      flash[:error] = @user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password)
   end
 end
