92a8b28361d859fc66409be89ccc4af3fba22bae
Zach Stearns <zach.stearns92@gmail.com>
2020-12-01 20:23:23 -0700

    Add test for unregistered email

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,18 +2,25 @@ require 'rails_helper'
 
 describe "As a visitor" do
   describe "When I visit the root welcome page" do
+    before(:each) do
+      @user = User.create!(first_name: 'Zach',
+        last_name: 'Stearns',
+        email: 'zach@email.com', 
+        password: 'password')
+    end
+
     it "I see a message welcoming me to the site" do
       visit root_path
 
       expect(page).to have_content('Welcome to Viewing Party!')
-    end
-    
-    it "I see fields to login" do
-      visit root_path
 
       within '#description' do
         expect(page).to have_content('Finally, a place where you can enjoy all your favorite movies with all your best friends!')
       end
+    end
+    
+    it "I see fields to login" do
+      visit root_path
 
       expect(page).to  have_field(:email)
       expect(page).to  have_field(:password)
@@ -34,19 +41,25 @@ describe "As a visitor" do
     end
 
     it "When I enter valid login information and submit, I am redirected to the user dashboard" do
-      user = User.create!(first_name: 'Zach',
-        last_name: 'Stearns',
-        email: 'zach@email.com', 
-        password: 'password')
-      
       visit root_path
 
-      fill_in :email,	with: "zach@email.com" 
-      fill_in :password,	with: "password"
+      fill_in :email,	with: "#{@user.email}" 
+      fill_in :password,	with: "#{@user.password}"
       click_button 'Sign In'
 
       expect(current_path).to eq('/dashboard')
       expect(page).to have_content('Login Successful!')
+    end
+
+    it "When I attempt to log in with an email address that is not registered, I get an error" do
+      visit root_path
+
+      fill_in :email,	with: "bob@email.com"
+      fill_in :password, with: "password"
+      click_button 'Sign In'
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('There is no account associated with this email address.')
     end
   end
 end
