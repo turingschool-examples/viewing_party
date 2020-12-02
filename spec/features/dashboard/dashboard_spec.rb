01ecfc73d8af8041c0664a8daadef55f4b402845
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-02 15:53:05 -0500

    Dashboard spec
    Co-Authored-By: Zach Stearns <zach.stearns92@gmail.com>
    Co-Authored-By: Sheryl Stillman <stillsheryl@users.noreply.github.com>

--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -21,4 +21,33 @@ describe 'As a user' do
 
     expect(current_path).to eq('/discover')
   end
+
+  it "Page has a textbox and a button to add friend" do
+    within id="#Friends" do
+      expect(page).to have_css('input[type="text"]')
+      expect(page).to have_button('Add Friend')
+    end
+  end
+
+  it "User currently has no friends" do
+    within id="#Friends" do
+      expect(page).to have_content('You currently have no friends')
+    end
+  end
+
+  it "Section show current parties and thir info for user if any" do
+    current_user = User.last
+    movie = Movie.create!(title: 'Avengers', api_id: 550)
+    party = Party.create!(user_id: current_user.id, movie_id: movie.id, date: '1/12/2020', time: '2:00 PM', duration: 220)
+    Guest.create(party_id: party.id, user_id: current_user.id, attending: true)
+
+    visit '/dashboard'
+    
+    within id="#party-#{party.id}" do
+      expect(page).to have_content('Avengers')
+      expect(page).to have_content('December 1, 2020')
+      expect(page).to have_content('2:00 PM')
+      expect(page).to have_content('Hosting')
+    end
+  end
 end
