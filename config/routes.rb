a2ca31c17d5005cef76bb695ed2af3cf60c654af
Brett Turing <bjsherman80@gmail.com>
2020-12-01 18:43:58 -0700

    create routes for user registration and sessions
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>
    
    Co-Authored-By: Jake Heft <60531761+jakeheft@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  #user
   get '/registration', to: 'users#new'
-  
+  post '/registration', to: 'users#create'
+  get '/dashboard', to: 'users#show'
+  #session
+   post '/login', to: 'sessions#create'
 end
