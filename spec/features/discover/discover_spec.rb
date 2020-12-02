3d83f4fbcc8021131172d1f85a1dbade4e6b398b
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-01 21:42:17 -0500

    updated spec with a login for the user

--- spec/features/discover/discover_spec.rb
+++ spec/features/discover/discover_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 describe 'As a user' do
   before :each do
-    @user_1 = User.create(email: 'test@gmail.com', password: 'test', first_name: 'Alex', last_name: 'Rivero')
+    User.create(email: 'test@gmail.com', password: 'test', first_name: 'Alex', last_name: 'Rivero')
+
+    visit  '/'
+    fill_in :email,	with: "test@gmail.com"
+    fill_in :password,	with: "test"
+    click_button 'Sign In'
   end
 
   it 'when I visit /discover' do
     visit '/discover'
-
-    expect(page).to have_content("Welcome #{@user_1.first_name} #{@user_1.last_name}")
+    user = User.last
+    expect(page).to have_content("Welcome #{user.first_name} #{user.last_name}")
 
     expect(page).to have_button('Find Top Rated Movies')
     expect(page).to have_button('Find Movies')
