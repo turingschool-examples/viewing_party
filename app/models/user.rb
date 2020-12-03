ec68c72285874c40e471fbd352163d68b2147e83
Connor Ferguson <cpfergus1@gmail.com>
2020-12-02 21:18:37 -0700

    updated user model to reflect new friendship relationship
    Co-authored-by: Curtis Bartell <cbartell980@gmail.com>
    Co-authored-by: Brian Liu <brian.b.liu@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :guests
   has_many :viewings, through: :guests
 
+  has_many :friendships
+  has_many :friends, through: :friendships
+
   validates :username, :email, uniqueness: true, presence: true
   validates :password, presence: true, on: :create
 end
