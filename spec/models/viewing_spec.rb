1621e538692e1ae94637a14465350b4684b476dc
Curtis Bartell <cbartell980@gmail.com>
2020-12-03 15:57:37 -0700

    Add tests for #user_hosting?
    Co-authored-by: Brian Liu <brian.b.liu@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- spec/models/viewing_spec.rb
+++ spec/models/viewing_spec.rb
@@ -12,4 +12,17 @@ describe Viewing, type: :model do
     it {should have_many(:guests)}
     it {should have_many(:users).through(:guests)}
   end
+
+  describe 'instance methods' do
+    it '#user_hosting?' do
+      user1 = create(:user)
+      user2 = create(:user)
+      viewing1 = create(:viewing)
+      viewing1.guests.create!(user_id: user1.id, hosting: true)
+      viewing1.guests.create!(user_id: user2.id, hosting: false)
+
+      expect(viewing1.user_hosting?(user1.id)).to be true
+      expect(viewing1.user_hosting?(user2.id)).to be false
+    end
+  end
 end
