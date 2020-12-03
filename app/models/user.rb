f938f462214b2d93476a540a747991084b1cde70
Connor Ferguson <cpfergus1@gmail.com>
2020-12-02 20:03:34 -0700

    updated user relationships to account for new models
    Co-authored-by: Curtis Bartell <cbartell980@gmail.com>
    Co-authored-by: Brian Liu <brian.b.liu@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :guests
+  has_many :viewings, through: :guests
 
   validates :username, :email, uniqueness: true, presence: true
   validates :password, presence: true, on: :create
