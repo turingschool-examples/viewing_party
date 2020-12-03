13b0915b5546a132836526db969d12fb42cdb879
Zach Stearns <zach.stearns92@gmail.com>
2020-12-02 19:45:34 -0700

    Rubo cop and removed api key

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   def index
     query = params[:movie_title]
     conn = Faraday.new('https://api.themoviedb.org') do |f|
-      f.params['api_key'] = '4e19f98a2287c879dc085f2087b387e5'
+      f.params['api_key'] = '<< API KEY >>'
     end
 
     @titles = []
@@ -12,12 +12,12 @@ class MoviesController < ApplicationController
       response = conn.get("/3/search/movie?&page=#{page_number}") do |req|
         req.params[:query] = query
       end
-    
+
       title_data = JSON.parse(response.body, symbolize_names: true)
-    
+
       @titles << title_data[:results]
     end
-    
+
     @titles = @titles.flatten
   end
 end
