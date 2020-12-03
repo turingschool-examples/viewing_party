0bdf9a088fa93e9ab996f4f99f84148044ff572e
George Soderholm <georgesoderholm@gmail.com>
2020-12-03 16:55:59 -0700

    Add film_length method to format minutes
    Co-authored-by: Will Dunlap dunlapww@gmail.com
    Co-authored-by: Kate Tester katemorris@gmail.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :film_length
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def film_length(minutes)
+    hrs = (minutes / 60).to_s
+    mins = (minutes % 60).to_s.rjust(2, '0')
+    hrs + ':' + mins
   end
 end
