e798ad3115ee37b5d969fb2ad5742c6147fd9310
Zach Stearns <zach.stearns92@gmail.com>
2020-12-02 17:23:48 -0700

    Add index api calls for search query and showing 2 pages, Likely to refactor

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,10 +5,19 @@ class MoviesController < ApplicationController
       f.params['api_key'] = '4e19f98a2287c879dc085f2087b387e5'
     end
 
-    response = conn.get('/3/search/movie?') do |req|
-      req.params[:query] = query
+    @titles = []
+    page_number = 0
+    2.times do
+      page_number += 1
+      response = conn.get("/3/search/movie?&page=#{page_number}") do |req|
+        req.params[:query] = query
+      end
+    
+      title_data = JSON.parse(response.body, symbolize_names: true)
+    
+      @titles << title_data[:results]
     end
-
-    @titles = JSON.parse(response.body, symbolize_names: true)
+    
+    @titles = @titles.flatten
   end
 end
