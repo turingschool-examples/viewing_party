2aa65a3d5c9040c8f5ede3e3d63b86690aa16b2e
John Kim <abcdefghijohnk@gmail.com>
2020-12-03 18:25:07 -0700

    Install figaro, abstracted api key, all tests pass
    Co-Authored-By: todd-estes <elestes@gmail.com>
    Co-Authored-By: Carson Jardine <carsonjardine@gmail.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,16 @@ class SearchController < ApplicationController
   def index
     title = params["Search by movie title"]
 
-    conn = Faraday.new("https://api.themoviedb.org")
-    suffix = "/3/search/movie?api_key="
-    
-    response = conn.get("#{suffix}3f3f1b6bf5a188108490867c4c43f1e4&language=en-US&query=#{title}&page=1&include_adult=false")
+    conn = Faraday.new("https://api.themoviedb.org") do |f|
+
+    f.params[:api_key] = ENV["MOVIE_SEARCH_API_KEY"]
+    end
+
+    suffix = "/3/search/movie?"
+
+    response = conn.get("#{suffix}&language=en-US&query=#{title}&page=1&include_adult=false")
 
     @movies = JSON.parse(response.body, symbolize_names: true)
+    require "pry"; binding.pry
   end
 end
