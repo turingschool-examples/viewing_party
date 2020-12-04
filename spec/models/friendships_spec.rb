38ee2a6297e5b6eeaa8028e7a0be234975416c15
Curtis Bartell <cbartell980@gmail.com>
2020-12-03 19:35:45 -0700

    Add tests for uniqueness of friend_id for a given user_id
    Co-authored-by: Brian Liu <brian.b.liu@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- spec/models/friendships_spec.rb
+++ spec/models/friendships_spec.rb
@@ -5,4 +5,8 @@ describe Friendship, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:friend).class_name('User') }
   end
+
+  describe 'validations' do
+    it { should validate_uniqueness_of(:friend_id).scoped_to(:user_id) }
+  end
 end
