8af46acecef9ccf867f600332bb91a86e51fc35f
Brian Liu <brian.b.liu@gmail.com>
2020-12-05 10:53:41 -0700

    Rubocop formatting in movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,8 @@
 class MoviesController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://api.themoviedb.org")
+    conn = Faraday.new(url: 'https://api.themoviedb.org')
 
-    response = conn.get("/3/search/movie") do |rec|
+    response = conn.get('/3/search/movie') do |rec|
       rec.params[:api_key] = ENV['TMDB_API_KEY']
       rec.params[:query] = params[:query]
       rec.params[:language] = 'en-US'
@@ -10,6 +10,6 @@ class MoviesController < ApplicationController
       rec.params[:include_adult] = false
     end
 
-    @movies = (JSON.parse(response.body, symbolize_names: true))[:results]
+    @movies = JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
