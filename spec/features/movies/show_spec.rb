9d58d0f408c17b640961c832c39a53bf66cd7bfd
todd-estes <elestes@gmail.com>
2020-12-05 18:37:26 -0700

    Deleted vcr cassette, added test for movies show method
    Co-Authored-By: John Kim <abcdefghijohnk@gmail.com>
    Co-Authored-By: Carson Jardine <carsonjardine@gmail.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -13,13 +13,21 @@ RSpec.describe 'Movies Details Page' do
       click_button("Log In")
     end
 
-      it 'I see a button to create a viewing party', :vcr do
+      it 'I can click a movie title to be taken to its movie details page', :vcr do
         visit discover_index_path
 
         fill_in "Search by movie title", with: "Gabriel's Inferno Part III"
         click_button("Find Movies")
         click_on("Gabriel's Inferno Part III")
         expect(current_path).to eq("/movies/761053")
-        end
       end
+
+      # it 'can visit a movie details page and see a create movie viewing party and movie attributes' do
+      #   visit("/movies/761053")
+      #
+      #   expect(page).to have_content("Gabriel's Inferno Part III")
+      #   expect(page).to have_content("Vote Average: 9.2")
+      #   expect(page).to have_content("9.2")
+      # end
     end
+   end
