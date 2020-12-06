8608bd3e87a4129c53ff2987550f8e98cadfccb3
Carson Jardine <carsonjardine@gmail.com>
2020-12-06 14:11:16 -0700

    Add relations to user and friendship models
    Co-Authored-By: John Kim <abcdefghijohnk@gmail.com>
    Co-Authored-By: todd-estes <elestes@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
 
+  has_many :friendships
+  has_many :friends, through: :friendships
   has_secure_password
 end
