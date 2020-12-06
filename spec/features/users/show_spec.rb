6d8efd91b04c7b1b3f23af188a755e8fe45f7a19
Carson Jardine <carsonjardine@gmail.com>
2020-12-06 15:37:06 -0700

    Update test with happy and sad path for adding a friend
    Co-Authored-By: John Kim <abcdefghijohnk@gmail.com>
    Co-Authored-By: todd-estes <elestes@gmail.com>

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe 'User Dashboard Page' do
   describe 'As a registered user' do
     before :each do
       @user = User.create!(email: "John@example.com", password: "password")
+      @friend = User.create!(email: "Todd@example.com", password: "password")
+
       visit root_path
 
       fill_in :email, with: @user.email
@@ -17,7 +19,7 @@ RSpec.describe 'User Dashboard Page' do
 
       expect(page).to have_button("Discover Movies")
       expect(page).to have_button("Add Friend")
-      expect(page).to have_field("Friend's Email")
+      expect(page).to have_field(:friend_email)
       expect(page).to have_content("Viewing Parties")
     end
 
@@ -25,6 +27,39 @@ RSpec.describe 'User Dashboard Page' do
       click_button("Discover Movies")
 
       expect(current_path).to eq('/discover')
+    end
+
+    describe 'I enter an email into the friend search field' do
+      it "The email I enter is valid and registered on the site " do
+        expect(current_path).to eq(dashboard_path)
+
+        within '.friends' do
+          expect(page).to have_content("You currently have no friends")
+        end
+
+        fill_in :friend_email, with: @friend.email
+        click_button 'Add Friend'
+
+        expect(current_path).to eq(dashboard_path)
+
+        within '.friends' do
+          expect(page).to have_content(@friend.email)
+        end
+      end
+
+      it "The email I enter is invalid/user does not exist" do
+        expect(current_path).to eq(dashboard_path)
+
+        fill_in :friend_email, with: 'meow'
+        click_button 'Add Friend'
+
+        expect(current_path).to eq(dashboard_path)
+        expect(page).to have_content("I'm sorry your friend cannot be found")
+
+        within '.friends' do
+          expect(page).to have_content("You currently have no friends")
+        end
+      end
     end
   end
 end
