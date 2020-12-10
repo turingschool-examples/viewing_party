17af12114b5fb6aa5a3736ec692b0560348cf36d
Curtis Bartell <cbartell980@gmail.com>
2020-12-09 19:33:22 -0700

    Update invite templates to include viewing details

--- app/mailers/friend_notifier_mailer.rb
+++ app/mailers/friend_notifier_mailer.rb
@@ -6,14 +6,14 @@ class FriendNotifierMailer < ApplicationMailer
         user[:user].email
       end
     end
-    
+
     @viewing = info[:viewing]
     @host = @users.select { |u| u[:hosting] }.first[:user]
 
     mail(
         reply_to: @host.email,
         to: @recipients.join(', '),
-        subject: "#{@host.username} is sending you viewing party details"
+        subject: "#{@host.username} has invited you to watch #{@viewing.movie.title}"
     )
   end
 end
