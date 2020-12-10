9961703bcfe9c01c876a71ad0a750f02a4f887ee
Curtis Bartell <cbartell980@gmail.com>
2020-12-09 21:35:14 -0700

    Add user seeds and viewing party seed
    Co-authored-by: Brian Liu <brian.b.liu@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create!(
+  username: 'professorX',
+  password: 'password',
+  email: 'sir_pat_stew@example.com'
+)
+user2 = User.create!(
+  username: 'bakula_to_the_future',
+  password: 'password',
+  email: 'scott_bakula@example.com'
+)
+user3 = User.create!(
+  username: 'finding_nimoy',
+  password: 'password',
+  email: 'leo_nimoy@example.com'
+)
+user4 = User.create!(
+  username: 'wild_captain_bill',
+  password: 'password',
+  email: 'wshatner@example.com'
+)
+user1.friends << user2
+user1.friends << user3
+user1.friends << user4
+
+star_trek = Movie.create!(
+  title: "Star Trek II: The Wrath of Khan",
+  api_id: 154,
+  duration_of_movie: 113
+)
+
+trek_party = star_trek.viewings.create!(
+  duration_of_party: star_trek.duration_of_movie,
+  start_time: DateTime.now
+)
+
+trek_party.guests.create!(user_id: user1.id, hosting: true)
+trek_party.guests.create!(user_id: user2.id, hosting: false)
+trek_party.guests.create!(user_id: user3.id, hosting: false)
+trek_party.guests.create!(user_id: user4.id, hosting: false)
