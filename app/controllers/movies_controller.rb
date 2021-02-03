8f04e447ef237800b5a75a2171636608172b3912
foymikek <>
2021-02-02 21:44:21 -0700

    Add working consumption of API
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,12 @@
 class MoviesController < ApplicationController
   def index
-    movie = params[:movie_title]
-    movie = "Fight Club"
+    movie = params[:movie]
+    # movie = "Fight Club"
     conn = Faraday.new("https://api.themoviedb.org")
-    require 'pry'; binding.pry
-    response = conn.get("/search/movies/#{movie}/550?api_key=f54bbd3f0b8d80da437d651c3c2f00b1")
-    data = JSON.parse(response.body, symbolize_names: true)
+    response = conn.get("/3/search/movie?api_key=f54bbd3f0b8d80da437d651c3c2f00b1&query=#{movie}")
+    # https://api.themoviedb.org search/movie?api_key=f54bbd3f0b8d80da437d651c3c2f00b1&query=unbroken
+    @movie = JSON.parse(response.body, symbolize_names: true)
+    # require 'pry'; binding.pry
+    
   end
 end
