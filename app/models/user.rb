e6a1e061b6548d14d595087870beefd914136efe
Aiden Mendez <aidenmendez@gmail.com>
2021-02-03 15:38:40 -0700

    Create welcome controller, route, and view. Also remove extra spacing in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
   has_many :senders, foreign_key: :sender_id, class_name: "Friend"
-
   has_many :receivers, through: :senders
 
   has_many :receivers, foreign_key: :receiver_id, class_name: "Friend"
-
   has_many :senders, through: :receivers
 end
