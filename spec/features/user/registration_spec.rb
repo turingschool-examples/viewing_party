6f3e7212b5cd6cc482791808fd5bacfdb031d22f
Grayson Myers <graymyers10@gmail.com>
2021-02-02 20:42:04 -0700

    Add test for user registration page

---
+++ spec/features/user/registration_spec.rb
@@ -0,0 +1,55 @@
+require "rails_helper"
+
+describe "Registration page" do
+  describe "happy path" do
+    it "redirects to dashboard upon successful registration" do
+      email = "me@example.com"
+      password = "Ilikedogs123"
+      visit new_user_path
+      fill_in :email, with: email
+      fill_in :password, with: password
+      fill_in :password_confirmation, with: password
+      click_button "Create Account"
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Welcome, #{email}")
+    end
+  end
+
+  describe "sad path" do
+    describe "it keeps user on same page and displays error message when" do
+      before :each do
+        @email = "me@example.com"
+        @password = "Ilikedogs123"
+        visit "new_user_path"
+      end
+
+      it "there is no email" do
+        fill_in :email, with: ""
+        fill_in :password, with: @password
+        fill_in :password_confirmation, with: @password
+        click_button "Create Account"
+
+        expect(current_path).to eq(new_user_path)
+      end
+
+      it "there is no password" do
+        fill_in :email, with: @email
+        fill_in :password, with: ""
+        fill_in :password_confirmation, with: ""
+        click_button "Create Account"
+
+        expect(current_path).to eq(new_user_path)
+      end
+
+      it "passwords do not match" do
+        fill_in :email, with: @email
+        fill_in :password, with: @password
+        fill_in :password_confirmation, with: "Ilikecats123"
+        click_button "Create Account"
+
+        expect(current_path).to eq(new_user_paths)
+      end
+    end
+  end
+end
