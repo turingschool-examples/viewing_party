54443a87a3b046e658d3ccbb053867794a19c537
Ely Hess <Ely.hess@me.com>
2021-02-03 15:59:33 -0700

    Add discover link tests/view/routes
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
 	post '/friendships', to: "friendships#create"
 
+	get '/discover', to: "movies#index"
+
 	resources :users, only: [:new, :create]
 end
--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -2,21 +2,32 @@ require 'rails_helper'
 
 describe "as a logged in user" do
 	describe "when on the dashboard page" do
-		it 'I can see my friends' do
+
+		before :each do
 			@user = User.create(email: "ely@me.com", password: "password")
+			allow_any_instance_of(Current).to receive(:user).and_return(@user)
+		end
+
+		it 'I can see my friends' do
 			@user2 = User.create(email: "friend@me.com", password: "password")
 			@user3 = User.create(email: "otherfriend@me.com", password: "password")
 
 			@user.add_friend(@user2)
 			@user.add_friend(@user3)
 
-			allow_any_instance_of(Current).to receive(:user).and_return(@user)
-
 			visit dashboard_path
 
 			expect(page).to have_content("Friends")
 			expect(page).to have_content(@user2.email)
 			expect(page).to have_content(@user3.email)
+		end
+
+		it 'I can click the discover movies link' do
+			visit dashboard_path
+
+			expect(page).to have_link("Discover Movies")
+			click_link "Discover Movies"
+			expect(current_path).to eq(discover_path)
 		end
 	end
 end
