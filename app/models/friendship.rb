67364a7b20f8c4d7d8e4fd959f167350422b39cb
Adam Etzion <60630167+aetzion1@users.noreply.github.com>
2021-02-03 08:15:45 -0700

    Update friendship.rb

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,5 +3,5 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
   validates :status, presence: true
 
-  enum status: { Pending: 0, Accepted: 1, Rejected: 2 }
+  enum status: { pending: 0, accepted: 1, rejected: 2 }
 end
