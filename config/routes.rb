e3bd4118553c337e8e88d91d2c89b21e644a248e
Phil McCarthy <philmcc2631@gmail.com>
2021-02-02 18:01:24 -0700

    Add route for movies index, reorder routes format

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root 'welcome#index'
-
-   resources :users, only: [:new]
-
-   get '/login', to: 'users#login_form'
-   post '/login', to: 'users#login'
-   
-   get '/discover', to: 'discover#index'
+  
+  resources :users, only: [:new]
+  resources :movies, only: [:index]
+  
+  root 'welcome#index'
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login'
+  get '/discover', to: 'discover#index'
 end
