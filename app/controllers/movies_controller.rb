9de40e6425039c4653e88de9f8bc62614492f831
Phil McCarthy <philmcc2631@gmail.com>
2021-02-02 18:00:14 -0700

    Move discover top 40 API call to movies_controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,11 @@
+class MoviesController < ApplicationController
+  def index
+    conn = Faraday.new(url: 'https://api.themoviedb.org/3')
+    # movie_id = params[:movie_id]
+    response = conn.get("discover/movie?api_key=#{ENV["TMDB_API_KEY"]}&sort_by=popularity.desc&page=1")
+    json = JSON.parse(response.body, symbolize_names: true)
+    @movies = json[:results].map do |movie_data|
+      Movie.new(movie_data)
+    end
+  end
+end
