7c2ceef369cb54e13737a449f19dc831fada0d30
Robert Heath <robert.b.heathii@gmail.com>
2021-02-02 19:09:48 -0600

    Create user creation interface.
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,6 @@
+class DashboardController < ApplicationController
+
+  def index
+    @user = User.find(session[:user_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 	root "welcome#index"
+
+	get '/dashboard', to: 'dashboard#index', as: :dashboard
+
+	resources :users, only: [:new, :create]
 end
