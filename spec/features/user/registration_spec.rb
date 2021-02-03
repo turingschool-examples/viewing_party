e8700592ddb798e1d6bd3fb6fce9d4a7569f175a
Grayson Myers <graymyers10@gmail.com>
2021-02-02 21:12:50 -0700

    Add shoulda matchers configuration and validation testing for user

--- spec/features/user/registration_spec.rb
+++ spec/features/user/registration_spec.rb
@@ -6,9 +6,9 @@ describe "Registration page" do
       email = "me@example.com"
       password = "Ilikedogs123"
       visit new_user_path
-      fill_in :email, with: email
-      fill_in :password, with: password
-      fill_in :password_confirmation, with: password
+      fill_in :user_email, with: email
+      fill_in :user_password, with: password
+      fill_in :user_password_confirmation, with: password
       click_button "Create Account"
 
       expect(current_path).to eq(dashboard_path)
@@ -25,27 +25,27 @@ describe "Registration page" do
       end
 
       it "there is no email" do
-        fill_in :email, with: ""
-        fill_in :password, with: @password
-        fill_in :password_confirmation, with: @password
+        fill_in :user_email, with: ""
+        fill_in :user_password, with: @password
+        fill_in :user_password_confirmation, with: @password
         click_button "Create Account"
 
         expect(current_path).to eq(new_user_path)
       end
 
       it "there is no password" do
-        fill_in :email, with: @email
-        fill_in :password, with: ""
-        fill_in :password_confirmation, with: ""
+        fill_in :user_email, with: @email
+        fill_in :user_password, with: ""
+        fill_in :user_password_confirmation, with: ""
         click_button "Create Account"
 
         expect(current_path).to eq(new_user_path)
       end
 
       it "passwords do not match" do
-        fill_in :email, with: @email
-        fill_in :password, with: @password
-        fill_in :password_confirmation, with: "Ilikecats123"
+        fill_in :user_email, with: @email
+        fill_in :user_password, with: @password
+        fill_in :user_password_confirmation, with: "Ilikecats123"
         click_button "Create Account"
 
         expect(current_path).to eq(new_user_paths)
