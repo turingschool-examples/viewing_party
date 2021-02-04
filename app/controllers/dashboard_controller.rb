62db7d1b6f8c70dea537d109c340dd22ab89f4c0
RyanDBarnett <conner2468@gmail.com>
2021-02-03 21:36:34 -0700

    Add dashboard welcome username message and not authenticated user sad path

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
   def index
+    if current_user
+      flash[:success] = "Welcome, #{current_user.name}"
+    else
+      flash[:notice] = "You Must Log In To Visit Your Dashboard"
+      redirect_to login_path
+    end
   end
 end
