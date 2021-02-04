4430fa2401d25bfc7c17da73bbf6c9f7681eca4d
Phil McCarthy <philmcc2631@gmail.com>
2021-02-03 17:42:39 -0700

    Rubocop clean up

--- app/services/movie_db_service.rb
+++ app/services/movie_db_service.rb
@@ -1,13 +1,14 @@
 class MovieDbService
   class << self
     def call_top_40_films
-      response = get("&sort_by=popularity.desc&page=1")
+      response = get('&sort_by=popularity.desc&page=1')
       parse_data(response)
     end
 
     private
+    
     def get(query)
-      conn.get("discover/movie?api_key=#{ENV["TMDB_API_KEY"]}#{query}")
+      conn.get("discover/movie?api_key=#{ENV['TMDB_API_KEY']}#{query}")
     end
 
     def conn
