b41a882c8da85ca763f8e85213606aaf91499827
Robert Heath <robert.b.heathii@gmail.com>
2021-02-04 16:15:18 -0600

    Add tests for no friends contingency on dashboard

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -8,18 +8,28 @@ describe "as a logged in user" do
 			allow_any_instance_of(Current).to receive(:user).and_return(@user)
 		end
 
-		it 'I can see my friends' do
-			@user2 = User.create(email: "friend@me.com", password: "password")
-			@user3 = User.create(email: "otherfriend@me.com", password: "password")
+		describe 'in the friends section' do
+			it 'says I have no friends when I have no friends' do
 
-			@user.add_friend(@user2)
-			@user.add_friend(@user3)
+				 visit dashboard_path
 
-			visit dashboard_path
+				 expect(page).to have_content("You ain't got no friends.")
+			end
 
-			expect(page).to have_content("Friends")
-			expect(page).to have_content(@user2.email)
-			expect(page).to have_content(@user3.email)
+			it 'I can see my friends' do
+				@user2 = User.create(email: "friend@me.com", password: "password")
+				@user3 = User.create(email: "otherfriend@me.com", password: "password")
+
+				@user.add_friend(@user2)
+				@user.add_friend(@user3)
+
+				visit dashboard_path
+
+				expect(page).to have_content("Friends")
+				expect(page).to have_content(@user2.email)
+				expect(page).to have_content(@user3.email)
+				expect(page).not_to have_content("You ain't got no friends.")
+			end
 		end
 
 		it 'I can click the discover movies link' do
@@ -32,5 +42,3 @@ describe "as a logged in user" do
 		end
 	end
 end
-
-
