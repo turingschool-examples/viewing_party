6e4a422afdbdb0e0b31307f67cfa493a24bb0d09
Ely Hess <Ely.hess@me.com>
2021-02-04 13:33:57 -0700

    add formatting
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ApplicationRecord
 	belongs_to :user
 	belongs_to :friend, class_name: 'User'
-	validates_uniqueness_of :friend
+	validates_uniqueness_of :friend, { message: "You're already friends with this person" }
 end
