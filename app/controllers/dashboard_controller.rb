4d5b66d0c812c6601caf667da9c574b2a18c7956
RyanDBarnett <conner2468@gmail.com>
2021-02-03 21:51:32 -0700

    Rubocop fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,14 @@
 class DashboardController < ApplicationController
   def index
-    if current_user
-      flash[:success] = "Welcome, #{current_user.name}"
-    else
-      flash[:notice] = "You Must Log In To Visit Your Dashboard"
-      redirect_to login_path
-    end
+    return not_logged_in_notice unless current_user
+
+    flash[:success] = "Welcome, #{current_user.name}"
+  end
+
+  private
+
+  def not_logged_in_notice
+    flash[:notice] = 'You Must Log In To Visit Your Dashboard'
+    redirect_to login_path
   end
 end
