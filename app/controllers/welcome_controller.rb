02470f8a8ae2d97fcfa86e88da743db3a4dc5931
Adam Etzion <adametzion90@gmail.com>
2021-02-03 20:05:35 -0700

    Update welcome controller syntax

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    if current_user
-      redirect_to dashboard_path
-    end
+    redirect_to dashboard_path if current_user
   end
 end
