5fe986c7e5f58efb6cd6d8ee6f7ed193e01f8dfd
Phil McCarthy <philmcc2631@gmail.com>
2021-02-04 15:59:44 -0700

    Update Film poro to include id
    Co-authored-by: Adam Etzion <60630167+aetzion1@users.noreply.github.com>
    Co-authored-by: Ryan Barnett <15859827+RyanDBarnett@users.noreply.github.com>

--- spec/poros/film_spec.rb
+++ spec/poros/film_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Film do
   it 'exists' do
     attrs = {
+      id: 1,
       title: 'The Birds',
       vote_average: 8.4
     }
@@ -11,5 +12,6 @@ RSpec.describe Film do
 
     expect(film.title).to eq('The Birds')
     expect(film.vote_average).to eq(8.4)
+    expect(film.id).to eq(1)
   end
 end
