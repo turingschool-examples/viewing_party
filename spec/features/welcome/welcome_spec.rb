725f295bac4f1c58597fb6cc1989f3bc6f194011
Grayson Myers <graymyers10@gmail.com>
2021-02-04 19:54:31 -0700

    Add password confirmation to account creation

--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe "welcome page" do
   describe "happy path" do
     describe "as a user" do
       before do
-        @existing_user = User.create!({:email => "adam_smith@example.com", :password => "supertopsecret1"})
-        @new_user = {:email => "johnny_doe@example.com", :password => "supersecret123"}
+        @existing_user = User.create!({:email => "adam_smith@example.com", :password => "supertopsecret1", password_confirmation: "supertopsecret1"})
+        @new_user = {:email => "johnny_doe@example.com", :password => "supersecret123", :password_confirmation => "supersecret123"}
         visit root_path
       end
-      
+
       it "has a welcome message and a brief description of the application" do
         within('#welcome-banner-section') do
           expect(page).to have_content("Welcome to Viewing Party")
         end
-  
+
         within('#welcome-blurb-section') do
           expect(page).to have_content("Hello there! Welcome to Viewing Party, a place friends to plan watch parties for all your favorite movies. We believe you're the customer, not the product! All of your data is stored securely and will never be sold. Let's get the show on the road!")
         end
       end
-  
+
       it "has a log in section and a link to the registration page" do
         within("#log-in-section") do
           expect(page).to have_field("email")
@@ -34,7 +34,6 @@ RSpec.describe "welcome page" do
         end
 
         expect(current_path).to eq(root_path)
-save_and_open_page
         # add testing for post-login welcome page
       end
 
