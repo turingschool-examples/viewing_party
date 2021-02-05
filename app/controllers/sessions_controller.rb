4dc35db6c7de0039bc76ba09026854d410109948
Aiden Mendez <aidenmendez@gmail.com>
2021-02-04 21:05:19 -0700

    Delete session new action and session new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,4 @@
 class SessionsController < ApplicationController
-  def new
-
-  end
-
   def create
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
