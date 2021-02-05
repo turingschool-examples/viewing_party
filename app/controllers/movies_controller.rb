eb7235caf90f2d816153a9c224e093bdeab460c0
Phil McCarthy <philmcc2631@gmail.com>
2021-02-05 12:34:32 -0700

    Abstract collection-making logic. Add sad path handling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,10 @@
 class MoviesController < ApplicationController
   def index
+    return redirect_to root_path, notice: 'Only users may see movies!' if !current_user
     if params[:search].present?
       @films = MovieDbFacade.search_films(params[:search])
     else
-      @films = []
-      @films << MovieDbFacade.discover_films(1)
-      @films << MovieDbFacade.discover_films(2)
-    end.flatten!
+      @films = MovieDbFacade.discover_films
+    end
   end
 end
