0c1f114de6c341f78b27c6420d146a9a725aae39
Grayson Myers <graymyers10@gmail.com>
2021-02-04 19:52:59 -0700

    Fix test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe User do
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
+
+    it {should validate_confirmation_of(:password)}
+    it {should validate_presence_of(:password_confirmation)}
   end
 end
