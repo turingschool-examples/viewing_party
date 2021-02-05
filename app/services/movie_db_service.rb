ee4d649387ac179bdc7da011e1ddcabc0ee8f12c
Phil McCarthy <philmcc2631@gmail.com>
2021-02-05 12:33:44 -0700

    Update service to return JSON with 40 objects. Add parse_data helper

--- app/services/movie_db_service.rb
+++ app/services/movie_db_service.rb
@@ -1,11 +1,15 @@
 class MovieDbService
   class << self
-    def call_top_films(page)
-      discover(page)
+    def call_top_films
+      page_one = discover(1)
+      page_two = discover(2)
+      page_one.merge(page_two) { |key, p1, p2| p1 + p2 }
     end
 
     def call_search_films(query)
-      search(query)
+      page_one = search(query, 1)
+      page_two = search(query, 2)
+      page_one.merge(page_two) { |key, p1, p2| p1 + p2 }
     end
 
     private
@@ -18,11 +22,12 @@ class MovieDbService
       end
       parse_data(response)
     end
-    
-    def search(query)
+
+    def search(query, page)
       response = conn.get('search/movie') do |req|
         req.params['api_key'] = ENV['TMDB_API_KEY']
         req.params['query'] = query
+        req.params['page'] = page
       end
       parse_data(response)
     end
