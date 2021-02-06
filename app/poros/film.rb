bc6bbaee9abfe89a5398a60370eba1e9eca0ac2d
Ely Hess <Ely.hess@me.com>
2021-02-06 10:07:24 -0700

    Add film attributes
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>

--- app/poros/film.rb
+++ app/poros/film.rb
@@ -1,20 +1,32 @@
 class Film
 	attr_reader :title,
+	            :id,
 	            :vote_average,
 	            :runtime,
 	            :popularity,
 	            :genres,
 	            :overview,
+	            :cast,
+							:poster,
+	            :reviews
 
-		def initialize(data)
-			@title        = data[:title].capitalize
-			@vote_average = data[:vote_average]
-			@runtime      = data[:runtime]
-			@popularity   = data[:popularity]
-			@genres       = data[:genres]
-			@overview     = data[:overview]
-			@cast         = cast
-			@reviews      = reviews
+	def initialize(data, cast = nil, reviews = nil)
+		@id           = data[:id]
+		@title        = data[:title].capitalize
+		@vote_average = data[:vote_average]
+		@runtime      = data[:runtime]
+		@popularity   = data[:popularity]
+		@genres       = data[:genres]
+		@overview     = data[:overview]
+		@poster       = data[:poster_path]
+		@cast         = cast
+		@reviews      = reviews
+	end
+
+	def render_image
+		unless @poster.nil?
+			"https://image.tmdb.org/t/p/w500" + @poster
 		end
+	end
 
 end
