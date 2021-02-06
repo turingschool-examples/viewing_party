312d02a1a169b282216551214f4a8084bbd64da5
Liam Cusack <LCusack8108@gmail.com>
2021-02-05 17:24:49 -0700

    "Created spec model and feature files"
    Co-authored-by: Aiden Mendez <41342981+aidenmendez@users.noreply.github.com>
    Co-authored-by: Grayson Myers <51936302+GrayMyers@users.noreply.github.com>

---
+++ spec/features/movie/discover_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe "Discover page" do
+  describe "displays" do
+    it "a button to discover top 40 movies" do
+    end
+
+    it "a text field to enter keywords to seach by movie title" do
+    end
+
+    it "a button to search by movie title" do
+    end
+  end
+end
