080e2fe177d0c1417c9c718bb3126ef63eb552e1
Ely Hess <Ely.hess@me.com>
2021-02-06 10:08:25 -0700

    Add get_movie dream drive
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -8,12 +8,20 @@ class MoviesFacade
       create_movies(MoviesService.top_rated_movies)
     end
 
+    def get_movie(movie)
+      movie_data = MoviesService.find_movie(movie)
+      # cast_data = MoviesService.find_cast(movie)
+      # reviews = MoviesService.retrieve_reviews(movie)
+      Film.new(movie_data, cast_data, reviews)
+    end
+
   private
 
     def create_movies(movies_data)
       movies_data.map do |movie_data|
-        Movies.new(movie_data)
+        Film.new(movie_data)
       end
     end
   end
+
 end
