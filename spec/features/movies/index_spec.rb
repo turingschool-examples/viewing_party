f27d9663ce7c053f5e802d0c0aa9b2b034d67670
Adam Etzion <adametzion90@gmail.com>
2021-02-07 12:11:18 -0700

    Add movie index view refactor and spec refactor with VCR gem

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -2,35 +2,33 @@ require 'rails_helper'
 
 RSpec.describe 'movies index', type: :feature do
   describe 'as a user' do
+    before(:each) do
+      @user = create(:user, email: 'test@email.com')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
     describe 'happy path' do
       before(:each) do
         @user = create(:user, email: 'test@email.com')
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       end
-
-      it 'i see 40 movies' do
+  
+      it 'lists 40 most highest rated movies', :vcr do
         visit movies_path
-
-        top_movies = MovieDbFacade.discover_films
-        top_movie = top_movies.first
-        last_movie = top_movies.last
 
         expect(page).to have_content("40 Movies")
 
-        within("#movie-#{top_movie.id}") do
-          expect(page).to have_content(top_movie.title)
-          expect(page).to have_content("Vote Average: #{top_movie.vote_average}")
-        end
+        expect(page).to have_css(".movie", count: 40)
 
-        within("#movie-#{last_movie.id}") do
-          expect(page).to have_content(last_movie.title)
-          expect(page).to have_content("Vote Average: #{last_movie.vote_average}")
+        within(first('.movie')) do
+          expect(page).to have_css(".title")
+          expect(page).to have_css(".vote_avg")    
         end
       end
 
-      it 'i can search by movie title' do
+      it 'i can search by movie title', :vcr do
         visit movies_path
 
+        # MAYBE CHANGE TO CLASS . SO WE CAN FORMAT EVERY SEARCH BAR THE SAME WAY
         within('#movie_search') do
           fill_in :search, with: 'Elf'
           click_button 'Search'
@@ -44,31 +42,27 @@ RSpec.describe 'movies index', type: :feature do
     end
 
     describe 'sad path' do
-      it 'returns top movies if search field is submitted blank' do
-        @user = create(:user, email: 'test@email.com')
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-        
+      it 'returns top movies if search field is submitted blank', :vcr do        
         visit movies_path
 
         fill_in :search, with: ''
 
         expect(current_path).to eq(movies_path)
         expect(page).to have_content('40 Movies')
+        expect(page).to have_css(".movie", count: 40)
       end
 
-      it 'returns 0 movies when no matches exist' do
-        @user = create(:user, email: 'test@email.com')
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-        
+      it 'returns 0 movies when no matches exist', :vcr do        
         visit movies_path
 
         fill_in :search, with: 'sdkfjaksdjf'
 
         expect(current_path).to eq(movies_path)
         expect(page).to have_content('0 Movies')
+        expect(page).to have_css(".movie", count: 40)
       end
 
-      it 'redirects a user that is not signed in to the root path and gives a flash message', :skip_before do
+      it 'redirects a user that is not signed in to the root path and gives a flash message', :skip_before, :vcr do
         visit movies_path
         
         expect(current_path).to eq(root_path)
