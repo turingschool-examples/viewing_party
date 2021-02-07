f54789dcaee3c301190d213111e1c8118aa14683
Adam Etzion <adametzion90@gmail.com>
2021-02-06 22:06:57 -0700

    Add film_spec for overview and credits

--- spec/poros/film_spec.rb
+++ spec/poros/film_spec.rb
@@ -7,9 +7,10 @@ RSpec.describe Film do
       title: 'The Birds',
       vote_average: 8.4,
       runtime: 500,
-      genres: [{id: 1, name: 'Horror'}, {id: 2, name: 'Historical'}]
+      genres: [{id: 1, name: 'Horror'}, {id: 2, name: 'Historical'}],
+      overview: "Damn these birds are mad!",
+      credits:  {cast: [{name: "Turo Pajala", character: "Mary Gifford"}, {name: "Frank Gerry", character: "Bird one"} ]}
     }
-
     film = Film.new(attrs)
 
     expect(film.title).to eq('The Birds')
@@ -17,5 +18,7 @@ RSpec.describe Film do
     expect(film.id).to eq(1)
     expect(film.runtime).to eq(500)
     expect(film.genres).to eq('Horror, Historical')
+    expect(film.overview).to eq('Damn these birds are mad!')
+    expect(film.credits).to eq([["Turo Pajala", "Mary Gifford"], ["Frank Gerry", "Bird one"]])
   end
 end
