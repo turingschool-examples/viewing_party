d8ebf0153e574976df89857db914e5bb908cee27
Aiden Mendez <aidenmendez@gmail.com>
2021-02-09 19:07:55 -0700

    Remove extra nesting from VCR path

--- spec/facades/movies_facade_spec.rb
+++ spec/facades/movies_facade_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Movie facade" do
   describe "(happy path)" do
     it "returns data from API call", :vcr do
-      VCR.use_cassette ("spec/fixtures/vcr_cassettes/movie_search_results") do
+      VCR.use_cassette ("movie_search_results") do
         # exercise our code by calling facade
         movie_facade = MoviesFacade.get_top_movies
   
