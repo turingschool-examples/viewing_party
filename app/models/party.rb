9ff8a97a3387eba7519daac095834c1ffb3545e6
Ely Hess <Ely.hess@me.com>
2021-02-10 13:22:03 -0700

    Update seeds
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,7 +3,10 @@ class Party < ApplicationRecord
 	belongs_to :movie
 	has_many :party_guests, dependent: :destroy
 
+	validates :datetime, presence: true
+
 	def party_host?
 		Current.user.id == user_id
 	end
+
 end
