ed6db7542abce1b82ef2448505969a584997c0e2
foymikek <>
2021-02-10 14:49:13 -0700

    add refactored test
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

--- spec/features/movies/new_spec.rb
+++ spec/features/movies/new_spec.rb
@@ -12,12 +12,12 @@ describe 'movies', :vcr do
     
     it 'when creating a new party' do
       visit movie_path(330457)
-      
-      expect(Movie.all.count).to eq(2)
+
+      expect(Movie.all.count).to eq(1)
       
       click_button('Create Movie Party')
       expect(current_path).to eq(new_party_path)
-      expect(Movie.all.count).to eq(2)
+      expect(Movie.all.count).to eq(1)
     end
   end
 end
