3955089c843883880e2ab391cf73bb120f5d57c5
foymikek <>
2021-02-09 17:42:46 -0700

    Add passing model tests
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
+	validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: ", you must use a valid email address" }
+	validates_presence_of :password
+
 	has_many :friendships
 	has_many :friends, through: :friendships, source: :friend
 	has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
 	has_many :inverse_friends, :through => :inverse_friendships, :source => :user
-
 	has_many :parties
+	has_many :party_guests
 
 	has_secure_password
-
-	validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: ", you must use a valid email address" }
-	validates_presence_of :password
 
 	before_save do
 		email.downcase!
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,18 @@ RSpec.describe User do
   describe 'relationships', type: :model do
     it { should have_many(:friendships) }
     it { should have_many(:friends).through(:friendships) }
+    it { should have_many(:inverse_friends) }
+    it { should have_many(:inverse_friends).through(:inverse_friendships) }
     it { should have_many(:parties) }
+    it { should have_many(:party_guests) }
   end
 
   describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+  end
+
+  describe 'instance methods' do
     it 'saves email as lower case' do
       email1 = 'ROB@ME.COM'
       @user = User.create(email: email1, password: '123abc')
