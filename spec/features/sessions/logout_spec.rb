80f06c252a42dffa8cf4ad9aa2093645cd148f12
Robert Heath <robert.b.heathii@gmail.com>
2021-02-10 15:40:16 -0600

    Add tests for logout

--- spec/features/sessions/logout_spec.rb
+++ spec/features/sessions/logout_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
-describe "As a visitor" do
-	describe "When i vist the " do
-		it 'should ' do
-			
-		end
+describe "a user can logout" do
+	it 'allows a user to log out' do
+		@user = User.create(email: 'test@example.com', password: "password")
+
+		visit login_path
+
+		fill_in :email, with: "test@example.com"
+		fill_in :password, with: "password"
+		click_button "Log In"
+
+		visit dashboard_path
+
+		click_on "Logout"
+		expect(page).to have_content('You have been successfully logged out')
+		expect(current_path).to eq(root_path)
+		expect(page).to have_content("Sign In")
 	end
 end
