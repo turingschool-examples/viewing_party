9b7d2a989498c11fb8199043c18204ee87c7dffd
Ely Hess <Ely.hess@me.com>
2021-02-10 13:22:22 -0700

    Add user helper methods
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
 	has_many :friends, through: :friendships, source: :friend
 	has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
 	has_many :inverse_friends, :through => :inverse_friendships, :source => :user
-
 	has_many :parties
 
 	has_secure_password
@@ -15,5 +14,16 @@ class User < ApplicationRecord
 		email.downcase!
 	end
 
+	def all_friends
+		friends + inverse_friends
+	end
+
+	def no_friends?
+		friends.none? && inverse_friends.none?
+	end
+
+	def party_invites
+		Party.joins(:party_guests).where("party_guests.user_id = ?", Current.user.id)
+	end
 
 end
