e678218e6448aeca064b9249f8eb39586d1d3747
Robert Heath <robert.b.heathii@gmail.com>
2021-02-10 15:01:48 -0600

    Add tests for movie show spec and party creation

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -37,5 +37,37 @@ describe "As a logged in user" do
 
 			expect(page).to have_content("Add friends to create a viewing party")
 		end
+
+    it 'has a form to create a new party', :vcr do
+      @user = User.create(email: "ely@me.com", password: "password")
+      @user2 = User.create(email: "friend@me.com", password: "password")
+			Friendship.create(user_id: @user.id, friend_id: @user2.id)
+
+			allow_any_instance_of(Current).to receive(:user).and_return(@user)
+
+      visit movie_path(109445)
+
+      click_button "Create Movie Party"
+      save_and_open_page
+      expect(page).to have_field('party[name]', with: 'Frozen')
+      expect(page).to have_field('party[duration]', with: 102)
+      expect(page).to have_field('party[datetime]')
+      expect(page).to have_button('Create Party')
+    end
+
+    it 'displays an error if a field is left blank', :vcr do
+      @user = User.create(email: "ely@me.com", password: "password")
+      @user2 = User.create(email: "friend@me.com", password: "password")
+			Friendship.create(user_id: @user.id, friend_id: @user2.id)
+
+			allow_any_instance_of(Current).to receive(:user).and_return(@user)
+
+      visit movie_path(109445)
+
+      click_button "Create Movie Party"
+
+      click_button 'Create Party'
+      expect(page).to have_content("Datetime can't be blank")
+    end
 	end
 end
