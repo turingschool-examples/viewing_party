0225f96825f1dd769a3bd26b59f15b96efaf7cc0
Grayson Myers <graymyers10@gmail.com>
2021-02-10 21:51:51 -0700

    Add functionality for get_parties method

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,19 @@ class User < ApplicationRecord
   has_many :receivers, foreign_key: :receiver_id, class_name: "Friend"
   has_many :senders, through: :receivers
 
+  has_many :user_parties
+  has_many :parties, through: :user_parties
+
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 
   validates :password_confirmation, presence: true
   validates_confirmation_of :password
+
+  def get_parties
+    {
+      hosting: Party.where(user_id: id),
+      invited: parties
+    }
+  end
 end
