562d3042f65971c00b76bb56da5a0bd03dbe0bda
Aiden Mendez <aidenmendez@gmail.com>
2021-02-10 21:51:02 -0700

    Update dashboard feature and user model specs

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -1,25 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe "dashboard" do
-  describe "as a user (happy path)" do
-    before(:each) do
-      visit root_path
-      User.create(email: "somedude@example.com", password: "")
-      click_link("Register")
-      fill_in(:email, with: "somedude@example.com")
-      fill_in(:password, with: "password123")
-      fill_in(:password_confirmation, with: "password123")
-      click_button("Create Account")
+RSpec.describe "Dashboard" do
+  describe "as a logged in user (happy path)" do
+    describe "the Friends panel" do
+      before(:each) do
+        visit root_path
 
-      visit root_path
+        click_link("Register")
+        
+        fill_in(:"user[email]", with: "somedude@example.com")
+        fill_in(:"user[password]", with: "password123")
+        fill_in(:"user[password_confirmation]", with: "password123")
+        click_button("Create Account")
+      end
+  
+      it "has a list of user's friends" do
+        user2 = User.create(email: "kelsey@email.org", password_digest: "dsaffsdsa")
+        user3 = User.create(email: "bobby@email.org", password_digest: "axcver")
+        user4 = User.create(email: "jane@email.org", password_digest: "dsarertgfffsdsa")
+        user5 = User.create(email: "elisa@email.org", password_digest: "eutkjgkx")
 
-      fill_in(:email, with: "somedude@example.com")
-      fill_in(:password, with: "password123")
-      click_button("Log In")
-    end
+        within("#friends-panel") do
+          expect(page).to have_content(user2.email)
+          expect(page).to have_content(user3.email)
+          expect(page).to have_content(user4.email)
+          expect(page).to have_content(user5.email)
+        end
+      end
+  
+      it "section to add friends" do
 
-    it "" do
+      end
 
+      it "tells a user they have no friends if they have zero" do
+  
+      end
     end
 
   end
