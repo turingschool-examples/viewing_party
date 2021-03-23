e44255d81af7777a64b79c8e2d68f5e04eb83cac
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-23 09:12:57 -0400

    Add restful routes to match the 6 view pages in wireframes and user stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
+
+  resource :dashboard, controller: 'user', only: :show do
+    resources :parties, only: [:new, :create]
+  end
+
+  resources :movies, only: [:index, :show]
+  get '/discover', to 'movies#search'
 end
