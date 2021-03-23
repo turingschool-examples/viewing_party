b72104a5d68b002319729f5f5e35e8a6367c69c2
Dominic Padula <domopadula2192@gmail.com>
2021-03-22 18:39:42 -0600

    handroll routes for registration, login, and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+
+   get '/registration', to: 'users#new'
+   get '/login', to: 'users#login'
 end
