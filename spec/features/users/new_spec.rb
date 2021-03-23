08d47d832a232114d349e138549995a71a85525b
kylejschulz <kylschulz@gmail.com>
2021-03-22 22:02:51 -0400

    add welcome page and controller, add new user sign up page and controller. All tests passing Co-authored-by: Andrew Johnston <72912074+avjohnston@users.noreply.github.com> Co-authored-by: Megan Gonzales <62969459+MGonzales26@users.noreply.github.com>

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe "user new page" do
+  describe "As a guest a user can register a new account" do
+    before (:each) do
+      visit new_user_path
+    end
+
+    it "has a form for name, email, password, and password confirmation" do
+      fill_in :full_name, with: "Kyle schulz"
+      fill_in :email, with: "kylschulz@gmail.com"
+      fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "password"
+      click_on "Submit"
+      expect(page).to have_content("Kyle schulz's dashboard")
+    end
+
+    it "validates that the email is valid" do
+      fill_in :full_name, with: "Kyle schulz"
+      fill_in :email, with: "kylschulzgmail.com"
+      fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "password"
+      click_on "Submit"
+
+      expect(page).to have_content("Email is invalid")
+    end
+
+    it "validates that the password is valid" do
+      fill_in :full_name, with: "Kyle schulz"
+      fill_in :email, with: "kylschulz@gmail.com"
+      fill_in :password, with: "passwo"
+      fill_in :password_confirmation, with: "password"
+      click_on "Submit"
+
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
+
+    it "validates that the name is valid" do
+      fill_in :full_name, with: ""
+      fill_in :email, with: "kylschulz@gmail.com"
+      fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "password"
+      click_on "Submit"
+
+      expect(page).to have_content("Full name can't be blank")
+    end
+
+    it "can't create account with already used email" do
+      user = User.create!(full_name: 'hi', email: 'kylschulz@gmail.com', password: 'password', password_confirmation: 'password')
+      fill_in :full_name, with: "kyle schulz"
+      fill_in :email, with: "kylschulz@gmail.com"
+      fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "password"
+      click_on "Submit"
+
+      expect(page).to have_content("Email has already been taken")
+    end
+  end
+end
