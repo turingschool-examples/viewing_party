b423cb3d4fc39ce988e66cc822bbc1495e06a549
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-03-23 15:01:03 -0600

    add current user to application controller and start on the dashboard view page

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -17,5 +17,28 @@ RSpec.describe "Dashboard" do
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content("Welcome #{@user.name}!")
     end
+
+    it "has a button to discover movies" do
+      expect(page).to have_button("Discover Movies")
+
+      click_button "Discover Movies"
+
+      expect(current_path).to eq(discover_path)
+    end
+
+    it "has a friends section" do
+      within "#friends-#{@user.id}" do
+        expect(page).to have_content("Friends")
+        expect(page).to have_content("You have no one on your friends list yet")
+        expect(page).to have_field("Friend's Email:")
+        expect(page).to have_button("Add Friend")
+      end
+
+      it "has a viewing party section" do
+        within "#viewing_party#{@user.id}" do
+          expect(page).to have_content("Viewing Party")
+        end
+      end
+    end
   end
 end
