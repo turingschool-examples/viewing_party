5fcdb18479d063ec7adfe4ebcea506a7a239283e
Gvieve <gnuebel13@gmail.com>
2021-03-23 18:37:38 -0600

    Cleanup rubocop errors and remove how from pr template
    Co-authored-by: Harrison Blake <72946334+harrison-blake@users.noreply.github.com>
    Co-authored-by: Jacob Arellano <58375638+jakejakearell@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create 
+  validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
 end
