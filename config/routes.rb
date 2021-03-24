f721da8b7da90690bf076dcaf13d0887a2765168
Dominic Padula <domopadula2192@gmail.com>
2021-03-24 15:38:24 -0600

    add post route for users new to create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  post '/users/new', to: 'users#create'
    resources :users, only: [:create, :new]
    post '/login', to: 'users#login'
    get '/dashboard', to: 'users#dashboard'
