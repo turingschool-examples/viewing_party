a2a0482b09f6db10c67834f67c012a3bbb7e480d
Joseph Budina <joebud3@gmail.com>
2021-03-23 20:04:35 -0600

    Add sad path test

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -17,6 +17,26 @@ describe "User registration form" do
 
     click_on "Create User"
 
+    expect(current_path).to eq(dashboard_path)
     expect(page).to have_content("Welcome, #{email}!")
+  end
+
+  it 'creates sad user' do
+    visit root_path
+
+    click_link "Register"
+
+    expect(current_path).to eq(new_user_path)
+
+    email = "joeb@email.com"
+    password = "test"
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: 'fdafdsa'
+
+    click_on "Create User"
+
+    expect(page).to have_content("Wrong email/password")
   end
 end
