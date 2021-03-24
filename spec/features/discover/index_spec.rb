382ae5ad3c690bf1e1846cf887f4d4dd1c4b13d2
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-24 11:04:15 -0400

    Reword tests to match button on view page.
    Co-authored-by: Katy La Tour <23460878+klatour324@users.noreply.github.com>

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -3,19 +3,15 @@ require 'rails_helper'
 RSpec.describe "As a user after I click the link from my dashboard to visit the Discover page" do
   describe "takes me to the landing page" do
     it "should display a search form to search by movie title (returning 40 results)" do
-      user_1 = User.create({email:'am@email.com', password: 'password123'})
-      visit dashboard_path(user_1)
-      click_link "Discover Movies"
+      user_1 = User.create!({email:'am@email.com', password: 'password123'})
+      visit discover_path
 
       expect(current_path).to eq(discover_path)
+      # expect(page).to have_content("Welcome #{user_1.email}!")
+      expect(page).to have_button("Find Top Rated Movies")
+
       expect(page).to have_content("Search by movie title")
       expect(page).to have_button("Find Movies")
-
-    end
-    it "should display a button to 'Find Top Rated Movies'" do
-      visit(discover_path)
-
-      expect(page).to have_button("Find Top Rated Movies")
     end
   end
 
@@ -24,7 +20,11 @@ RSpec.describe "As a user after I click the link from my dashboard to visit the 
       visit(discover_path)
       click_button("Find Top Rated Movies")
 
-      expect(page).to have_content()
+      expect(page).to have_content("Gabriel's Inferno Part II")
+      expect(page).to have_content("Vote Average: 8.8")
+      expect(page).to_not have_content("Vote Average: 1.5")
+      expect(page).to have_content("David Attenborough: A Life on Our Planet")
+      expect(page).to have_content("The Great Dictator")
     end
   end
 
