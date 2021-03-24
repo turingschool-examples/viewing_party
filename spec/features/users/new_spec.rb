7c914727d552d49028e180bf116c0f2e0bd97440
Gvieve <gnuebel13@gmail.com>
2021-03-23 19:49:36 -0600

    Add users#new tests, routes, actions and views, add dashboard#index
    Co-authored-by: Harrison Blake <72946334+harrison-blake@users.noreply.github.com>
    Co-authored-by: Jacob Arellano <58375638+jakejakearell@users.noreply.github.com>

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe "When a user visits the registration path" do
+  describe "has a form with email, password and password confirmation" do
+    it "and I can create a user when I enter valid information" do
+      visit registration_path
+
+      fill_in "user[email]", with: "some@stuff.com"
+      fill_in "user[password]", with: "456tuff"
+      fill_in "user[password_confirmation]", with: "456tuff"
+      click_button "Register"
+
+      expect(current_path).to eq(dashboard_index_path)
+      expect(page).to have_content("Welcome to Viewing Party, some@stuff.com")
+    end
+
+    it "and I cannot create user without a valid email" do
+      visit registration_path
+
+      fill_in "user[email]", with: "somestuff"
+      fill_in "user[password]", with: "456tuff"
+      fill_in "user[password_confirmation]", with: "456tuff"
+      click_button "Register"
+
+      expect(current_path).to eq(users_path)
+      expect(page).to have_content("Please enter valid information to create an acccount")
+    end
+
+    it "and I cannot create user without an email or password" do
+      visit registration_path
+
+      click_button "Register"
+
+      expect(current_path).to eq(users_path)
+      expect(page).to have_content("Please enter valid information to create an acccount")
+    end
+
+    it "and I cannot create user without a accurate password confirmation" do
+      visit registration_path
+
+      fill_in "user[email]", with: "some@stuff.com"
+      fill_in "user[password]", with: "456tuff"
+      fill_in "user[password_confirmation]", with: "123tuff"
+      click_button "Register"
+
+      expect(current_path).to eq(users_path)
+      expect(page).to have_content("Please enter valid information to create an acccount")
+    end
+  end
+end
