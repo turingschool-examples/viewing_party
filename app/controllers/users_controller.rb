7e43184b644636a9f76f97ee8e75542a9a4762ca
Joseph Budina <joebud3@gmail.com>
2021-03-25 11:42:20 -0600

    Refactor login and login form to sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
     user[:email] = user[:email].downcase
     new_user = User.new(user)
     if new_user.save
-      new_user = User.create(user)
       flash[:success] = "Welcome, #{new_user.email}!"
       session[:user_id] = new_user.id
       redirect_to dashboard_path
@@ -20,19 +19,6 @@ class UsersController < ApplicationController
       # find way to notify error for p.w. and email seperately
       flash[:error] = 'Wrong email/password'
       redirect_to new_user_path
-    end
-  end
-
-  def login_form; end
-
-  def login
-    user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:password])
-      flash[:success] = "Welcome, #{user.email}"
-      redirect_to dashboard_path
-    else
-      flash[:error] = 'Wrong email/password'
-      render :login_form
     end
   end
 
