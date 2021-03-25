e320e4accf501c09531027710b097fd63b5cf2a6
Joseph Budina <joebud3@gmail.com>
2021-03-25 17:57:36 -0600

    Log a user in for test

--- spec/features/user/dashboard_spec.rb
+++ spec/features/user/dashboard_spec.rb
@@ -3,8 +3,15 @@ require 'rails_helper'
 RSpec.describe "Dashboard: Discover Movies" do
   describe "Discover movies button" do
     it "Redirects user to a discover page when button is clicked" do
+      user = User.create(email: "joeb@email.com", password: "test")
+      visit root_path
 
-      visit dashboard_path
+      click_link "Login"
+
+      fill_in :email, with: user.email.upcase
+      fill_in :password, with: user.password
+
+      click_button 'Login'
 
       within(".topnav") do
         expect(page).to have_link("Discover Movies")
