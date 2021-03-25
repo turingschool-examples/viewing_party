aa6b9c9ddf4028ad156c5ea5d82d38b1f22d1d75
Dominic Padula <domopadula2192@gmail.com>
2021-03-25 17:27:55 -0600

    Work on stupid bug all day Co-authored-by: Doug Welchons <72056427+DougWelchons@users.noreply.github.com> Co-authored-by: Jeremiah Michlitsch <61896482+jmichlitsch@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,14 @@ class UsersController < ApplicationController
 
   def dashboard; end
 
-  def new; end
+  def new
+   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to dashboard_path
+      redirect_to '/dashboard'
       flash[:success] = "Welcome, #{@user.name}!"
     else
       flash.now[:error] = 'All fields required. Passwords must match'
