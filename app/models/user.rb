1c544448ae6b4ebb369f8ef479707679aad009ae
Gvieve <gnuebel13@gmail.com>
2021-03-25 18:57:41 -0600

    Add model, tests, validations  and associations for Friend and User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: true, on: :create, presence: true
   validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+
+  has_many :followers, foreign_key: :follower_id, class_name: 'Friend'
+  has_many :followed, through: :followers
+  has_many :followed, foreign_key: :followed_id, class_name: 'Friend'
+  has_many :followers, through: :followed
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,12 @@ describe User, type: :model do
   describe "Validations" do
     it {should validate_presence_of :email}
     it {should validate_presence_of :password}
-    it {should validate_uniqueness_of :email}
+    it {should validate_uniqueness_of(:email).on(:create)}
+  end
+
+  describe 'relationships' do
+    it {should have_many(:followed)}
+    it {should have_many(:followers)}
   end
 
   describe "email format" do
