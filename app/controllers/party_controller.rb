93b229dffbbc61d7b6c5f9304e63efab761a567d
Dominic Padula <domopadula2192@gmail.com>
2021-03-26 16:53:31 -0600

    add new action for parties to create movie and party

--- app/controllers/party_controller.rb
+++
@@ -1,8 +0,0 @@
-class PartyController < ApplicationController
-  before_action :current_user
-  def new
-    @movie = Movie.create(name: params[:title], duration: params[:runtime])
-    @party = Party.new(movie_id: @movie.id, host_id: @current_user.id, duration: @movie.duration )
-    # binding.pry
-  end
-end
