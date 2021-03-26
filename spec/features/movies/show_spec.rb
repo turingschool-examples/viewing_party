2e8c7ac9758a42edb7952aa4658d21bdcf0d2ac5
Andrew Johnston <av.johnst@gmail.com>
2021-03-25 19:15:19 -0600

    Fix failing path in movie show spec and add before_install info to travis.yml
    Co-authored-by: Kyle Schulz <54863905+kylejschulz@users.noreply.github.com>
    Co-authored-by: Megan Gonzales <62969459+MGonzales26@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Movies details page', type: :feature do
     @host = create(:user, password: 'password')
     login(@host)
     @movie = MovieService.movie_object(100)
-    visit movie_path(@movie)
+    visit movie_path(@movie.id)
   end
 
   it 'shows all of the movies details' do
