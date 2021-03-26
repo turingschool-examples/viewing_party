55025d2810dbd7212bb047b4bad2c3211eff5f9e
Jordan Beck <jordanfbeck@gmail.com>
2021-03-25 20:59:06 -0600

    Migration and migrate for follows table
    Co-authored-by: Jesus Quezada-Guillen <17693874+jequeza@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_215720) do
+ActiveRecord::Schema.define(version: 2021_03_26_013947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "password_digest"
