003119fa227a51231e09579573eb4156ca02a5d0
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-25 21:08:35 -0400

    Add VCR references to tests that rely on API calls

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -15,29 +15,33 @@ RSpec.describe "As a user after I click the link from my dashboard to visit the 
 
   describe "search for top 40 rated movies" do
     it "should return the top 40 movie titles based on Vote Average" do
-      visit(discover_path)
-      click_button("Find Top Rated Movies")
+      VCR.use_cassette('top_forty_movies') do
+        visit(discover_path)
+        click_button("Find Top Rated Movies")
 
-      expect(page).to have_content("Gabriel's Inferno Part II")
-      expect(page).to have_content("Vote Average: 8.8")
-      expect(page).to_not have_content("Vote Average: 1.5")
-      expect(page).to have_content("David Attenborough: A Life on Our Planet")
-      expect(page).to have_content("Mortal Kombat Legends")
+        expect(page).to have_content("Gabriel's Inferno Part II")
+        expect(page).to have_content("Vote Average: 8.8")
+        expect(page).to_not have_content("Vote Average: 1.5")
+        expect(page).to have_content("David Attenborough: A Life on Our Planet")
+        expect(page).to have_content("Mortal Kombat Legends")
+      end
     end
   end
 
   describe "search for a movie by title" do
     it "should return movies that match the user query and thier rating" do
-      visit(discover_path)
-      fill_in :movie_query, with: "phoenix"
-      click_on("Find Movies")
+      VCR.use_cassette('movie_search') do
+        visit(discover_path)
+        fill_in :movie_query, with: "phoenix"
+        click_on("Find Movies")
 
-      expect(page).to have_content("Dark Phoenix")
-      expect(page).to have_content("Vote Average: 6.1")
-      expect(page).to have_content("Harry Potter and the Order of the Phoenix")
-      expect(page).to have_content("Rising Phoenix")
-      expect(page).to have_content("Griffin & Phoenix")
-      expect(page).to have_content("Phoenix Forgotten")
+        expect(page).to have_content("Dark Phoenix")
+        expect(page).to have_content("Vote Average: 6.1")
+        expect(page).to have_content("Harry Potter and the Order of the Phoenix")
+        expect(page).to have_content("Rising Phoenix")
+        expect(page).to have_content("Griffin & Phoenix")
+        expect(page).to have_content("Phoenix Forgotten")
+      end
     end
   end
 end
--- spec/features/discover/movies_details_spec.rb
+++ spec/features/discover/movies_details_spec.rb
@@ -3,36 +3,43 @@ require 'rails_helper'
 RSpec.describe "As an Authorized User" do
   describe "when I visit a movie details page" do
     it "welcomes me with my username" do
-      visit movie_path(550)
+      VCR.use_cassette('movie_info') do
+        visit movie_path(550)
 
-      #expect(page).to have_content("Welcome #{@user.email}")
+        #expect(page).to have_content("Welcome #{@user.email}")
+      end
     end
 
     it "displays the title of the movie, the vote average, the runtime, summary and genres" do
-      visit movie_path(550)
+      VCR.use_cassette('movie_info') do
+        visit movie_path(550)
 
-      expect(page).to have_content("Fight Club")
-      expect(page).to have_content("Vote Average: 8.4")
-      expect(page).to have_content("Runtime: 139 minutes")
-      expect(page).to have_content("Genre(s): Drama")
+        expect(page).to have_content("Fight Club")
+        expect(page).to have_content("Vote Average: 8.4")
+        expect(page).to have_content("Runtime: 139 minutes")
+        expect(page).to have_content("Genre(s): Drama")
+      end
     end
 
     it "displays the cast" do
-      visit movie_path(550)
+      VCR.use_cassette('movie_info') do
+        visit movie_path(550)
 
-      expect(page).to have_content("Edward Norton / The Narrator")
-      expect(page).to have_content("Brad Pitt / Tyler Durden")
-      expect(page).to have_content("Helena Bonham Carter / Marla Singer")
-      expect(page).to have_content("David Andrews / Thomas")
-      expect(page).to_not have_content("Christina Cabot / Group Leader")
-
+        expect(page).to have_content("Edward Norton / The Narrator")
+        expect(page).to have_content("Brad Pitt / Tyler Durden")
+        expect(page).to have_content("Helena Bonham Carter / Marla Singer")
+        expect(page).to have_content("David Andrews / Thomas")
+        expect(page).to_not have_content("Christina Cabot / Group Leader")
+      end
     end
 
     it "displays the reviews" do
-      visit movie_path(550)
+      VCR.use_cassette('movie_info') do
+        visit movie_path(550)
 
-      expect(page).to have_content("Goddard")
-      expect(page).to have_content("r96sk")
+        expect(page).to have_content("Goddard")
+        expect(page).to have_content("r96sk")
+      end
     end
   end
 end
