a6237c52bf0bec941b762b1aab725ebbf1ad0ceb
Jake Volpe <volpe.jacob@gmail.com>
2021-03-27 09:19:55 -0600

    Add comment in movie cont, waiting for more endpoints

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,7 @@ class MoviesController < ApplicationController
       @movies = MovieService.get_top_rated
     elsif params[:search]
        @movies = [Film.new({title: "Turing is fun", vote_average: 69})]
+      #  just a placeholder until the real endpoint can be used for search by title
     else 
       @movies = []
     end
