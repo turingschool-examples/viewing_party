8c38362be4087f298e37e66fb15bd9e381b44480
DougWelchons <doug.welchons@gmail.com>
2021-03-27 16:31:21 -0600

    Add VCR fix

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe "Discover index page" do
       end
 
       it "redirects to the movies page and shows my searech results" do
-        VCR.use_cassette('search_movies') do
+        VCR.use_cassette('search_movies_fight_club') do
           visit movies_path
 
           fill_in :search, with: "fight club"
