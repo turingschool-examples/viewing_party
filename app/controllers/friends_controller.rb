47d3d9ce1c7c67aff102d0a298d45e97cf133022
Gvieve <gnuebel13@gmail.com>
2021-03-27 01:27:54 -0600

    Cleanup rubocop issues

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,15 +2,12 @@ class FriendsController < ApplicationController
   def create
     user = User.find(params[:id])
     friend = User.find_by(email: params[:email])
-
     if friend
       user.followed << friend
-      session[:user_id] = user.id
-      redirect_to dashboard_index_path
     else
       flash[:message] = "#{params[:email].titleize} cannot be found. Search for another friend."
-      session[:user_id] = user.id
-      redirect_to dashboard_index_path
     end
+    session[:user_id] = user.id
+    redirect_to dashboard_index_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,16 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, on: :create, presence: true
   validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
 
-  has_many :followed_users, foreign_key: :follower_id, class_name: 'Friend'
-  has_many :followed, through: :followed_users
-  has_many :following_users, foreign_key: :followed_id, class_name: 'Friend'
-  has_many :followers, through: :following_users
+  has_many  :followed_users,
+            foreign_key: :follower_id,
+            class_name: 'Friend',
+            dependent: :destroy,
+            inverse_of: :following_users
+  has_many  :followed, through: :followed_users, inverse_of: :followers
+  has_many  :following_users,
+            foreign_key: :followed_id,
+            class_name: 'Friend',
+            dependent: :destroy,
+            inverse_of: :followed_users
+  has_many  :followers, through: :following_users, inverse_of: :followed
 end
