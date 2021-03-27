d482e7a5a971231a7f6eb81c8abc9d147e5140c3
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-03-27 11:36:40 -0600

    Finish create method for parties
    Co-authored-by: Dominic Padula <70593322+domo2192@users.noreply.github.com>

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,20 +1,31 @@
 class PartiesController < ApplicationController
   before_action :current_user
   def new
-    @movie = Movie.create(name: params[:title], duration: params[:duration])
+    @movie = Movie.find_or_create_by(name: params[:title], duration: params[:duration])
     @party = Party.new(movie_id: @movie.id, host_id: current_user.id, duration: @movie.duration )
   end
 
   def create
     @party = Party.create(party_params)
-    @party_friend = PartyFriend.create(party_id: @party.id,
-                                      user_id: params[:party][:friend_check][0])
-    binding.pry
+    if @party.save
+      invite_friends
+      flash[:notice] = 'Invites Sent'
+      redirect_to dashboard_path
+    else
+      flash.now[:error] = 'Invites not sent ,missing Fields'
+      render :new, obj: @party
+    end
   end
 
   private
 
   def party_params
     params.require(:party).permit(:duration, :date, :start_time, :movie_id, :host_id)
+  end
+
+  def invite_friends
+    params[:party][:friend_check].each do |friend|
+      PartyFriend.create(party_id: @party.id,user_id: friend)
+    end
   end
 end
