66809b5ea3946c5e9e290510e831b85871390f90
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-27 18:55:04 -0400

    Change has_many party_friends relationship. Belongs_to was incorrect

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,14 +2,15 @@ class PartiesController < ApplicationController
 
   def create
     movie = Movie.find_by(api_id: params[:party][:api_id])
-    party = Party.create({
+    party = Party.new({
                       movie_id:  movie.id,
                           date:  params[:party][:date],
                       duration:  params[:party][:duration],
                    movie_title:  params[:party][:movie_title],
                        user_id:  params[:party][:user_id]
                         })
-
+    binding.pry
+    party.save
     redirect_to dashboard_path(params[:party][:user_id])
   end
 
