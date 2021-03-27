d0bd61e4010d73e8eca2166d9cef78483a458cab
Harrison Blake <harrison.r.blake.gmail.com>
2021-03-27 16:58:42 -0600

    Finish clean up refactor
    Co-authored-by: Jacob Arellano <58375638+jakejakearell@users.noreply.github.com>
    Co-authored-by: Genevieve Nuebel <72330302+Gvieve@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data('Movie_Key') { ENV['MOVIE_API'] }
   # config.configure_rspec_metadata!
