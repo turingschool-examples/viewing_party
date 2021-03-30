b75894f8c68789b836ce8970af7cc12cfa555f86
Jordan Beck <jordanfbeck@gmail.com>
2021-03-29 19:43:20 -0600

    Adds passing test and code for Similar movies
    Co-authored-by: Jake Volpe <62768837+javolpe@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Movies Show Page" do
-  before :each do 
+  before :each do
     @user = User.create(password: "hello", email: "sample@email.com")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
   describe "When I visit the movie's detail page as a logged in user" do
     it "displays the logged in user email" do
-      VCR.use_cassette('movie_show_page_display_fight_club') do 
+      VCR.use_cassette('movie_show_page_display_fight_club') do
         visit movie_path(550)
         within "#userInfo" do
           expect(page).to have_content("Welcome #{@user.email}!")
+        end
+      end
+    end
+    it "displays similar movies" do
+      VCR.use_cassette('movie_show_page_display_fight_club') do
+        visit movie_path(550)
+      end
+      VCR.use_cassette('similar_to_fight_club') do
+        within "#similarMovies" do
+          expect("Blade Runner").to appear_before("Little Big Man")
         end
       end
     end
