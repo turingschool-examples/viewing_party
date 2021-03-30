909694b7124f12f47a738310560984e5f1d31635
Jordan Beck <jordanfbeck@gmail.com>
2021-03-29 20:59:19 -0600

    Add code to add posters to movie show

--- app/poros/film_details.rb
+++ app/poros/film_details.rb
@@ -3,20 +3,22 @@ class FilmDetails
               :average,
               :summary,
               :runtime,
-              :genres
-  
+              :genres,
+              :image
+
   def initialize(data)
     @title = data[:title]
     @average = data[:vote_average]
     @summary = data[:overview]
     @runtime = time_conversion(data[:runtime])
     @genres = find_genres(data[:genres])
+    @image = data[:poster_path]
   end
 
   def time_conversion(minutes)
     hours = minutes / 60
     rest = minutes % 60
-    time = [hours, rest]  
+    time = [hours, rest]
   end
 
   def find_genres(genres)
