5956f5880cb90d09fafc439190245791c769a6d2
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-30 18:21:16 -0500

    Add functionality to create a new party and guest list
    Co-authored-by: Jake Volpe <62768837+javolpe@users.noreply.github.com>
    Co-authored-by: Jordan Beck <68141454+jordanfbeck0528@users.noreply.github.com>

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -5,6 +5,19 @@ class PartiesController < ApplicationController
   end
 
   def create
+    party = Party.create(party_params)
+    guests_id = params.select { |k| k.include?("invited") }.values
+    guest_list = GuestList.make_guest_list(guests_id, party.id)
+    if party.save
+      redirect_to dashboard_path
+    else
+      flash[:error] = "Input the correct information"
+      redirect_to new_party_path
+    end
+  end
 
+  private
+  def party_params
+    params[:party].permit(:duration, :date, :time, :movie_id, :user_id)
   end
 end
--- app/models/guest_list.rb
+++ app/models/guest_list.rb
@@ -1,4 +1,10 @@
 class GuestList < ApplicationRecord
   belongs_to :user
   belongs_to :party
+
+  def self.make_guest_list(id_collection, party_id)
+    id_collection.map do |id|
+      GuestList.create(user_id: id, party_id: party_id)
+    end
+  end
 end
