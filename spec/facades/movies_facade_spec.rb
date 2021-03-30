c64d8605b491069e3d4b86cc5e7591682e2de141
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-30 19:36:40 -0400

    Add sad paths to facade tests for an invalid movie id

--- spec/facades/movies_facade_spec.rb
+++ spec/facades/movies_facade_spec.rb
@@ -24,20 +24,38 @@ RSpec.describe MovieFacade do
         expect(result.count).to eq(40)
       end
     end
-  end
-  it "returns an openstruct of movie_information" do
-    VCR.use_cassette('movie_info') do
-      result = MovieFacade.movie_information(550)
+    it "returns an openstruct of movie_information" do
+      VCR.use_cassette('movie_info') do
+        result = MovieFacade.movie_information(550)
 
-      expect(result.class).to eq(OpenStruct)
-      expect(result.api_id).to eq(550)
-      expect(result.title).to eq("Fight Club")
-      expect(result.vote_average).to eq(8.4)
-      expect(result.runtime).to eq(139)
-      expect(result.genres).to eq(["Drama"])
-      expect(result.summary.class).to eq(String)
-      expect(result.cast.count).to eq(10)
-      expect(result.reviews.class).to eq(Hash)
+        expect(result.class).to eq(OpenStruct)
+        expect(result.api_id).to eq(550)
+        expect(result.title).to eq("Fight Club")
+        expect(result.vote_average).to eq(8.4)
+        expect(result.runtime).to eq(139)
+        expect(result.genres).to eq(["Drama"])
+        expect(result.summary.class).to eq(String)
+        expect(result.cast.count).to eq(10)
+        expect(result.reviews.class).to eq(Hash)
+      end
+    end
+    it "creates a movie in the database if it does not already exist" do
+      result = MovieFacade.create_movie(550)
+
+      expect(result.class).to eq(Movie)
+      result = Movie.where(api_id: 550)
+
+      expect(result.count).to eq(1)
+    end
+  end
+  describe "sad path" do
+    it "can create an create  " do
+      VCR.use_cassette('movie_info_sad_path') do
+        result = MovieFacade.movie_information(1)
+
+        expect(result.class).to eq(Array)
+        expect(result.empty?).to eq(true)
+      end
     end
   end
 end
