c8345647383bf6340cdea2404920a8ba2ac6229c
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-30 14:26:04 -0400

    Add test for movie information as openstruct on movie facade

--- spec/facades/movies_facade_spec.rb
+++ spec/facades/movies_facade_spec.rb
@@ -3,8 +3,8 @@ require 'ostruct'
 
 RSpec.describe MovieFacade do
   describe "Happy path" do
-    VCR.use_cassette('movie_search') do
-      it "calls the Movie search and gets data" do
+    it "calls the Movie search and gets data" do
+      VCR.use_cassette('movie_search') do
         title = 'Phoenix'
         limit = 20
         result = MovieFacade.search(title, limit)
@@ -14,8 +14,8 @@ RSpec.describe MovieFacade do
         expect(result.count).to eq(limit)
       end
     end
-    VCR.use_cassette('top_forty') do
-      it "calls the Movie top_forty and gets data" do
+    it "calls the Movie top_forty and gets data" do
+      VCR.use_cassette('top_forty') do
         limit = (40)
         result = MovieFacade.top_movies(limit)
 
@@ -23,6 +23,16 @@ RSpec.describe MovieFacade do
         expect(result[0].class).to eq(OpenStruct)
         expect(result.count).to eq(40)
       end
+    end
+  end
+  it "returns an openstruct of movie_information" do
+    VCR.use_cassette('movie_info') do
+      result = MovieFacade.movie_information(550)
+      first_result = result.first
+
+      expect(result.class).to eq(Array)
+      expect(first_result.class).to eq(OpenStruct)
+      expect(first_result.id).to eq(550)
     end
   end
 end
