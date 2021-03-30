aae8e00377327618d949f709a93030c6c146a02d
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-29 21:27:44 -0500

    Set up database for Party model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_013947) do
+ActiveRecord::Schema.define(version: 2021_03_30_021030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2021_03_26_013947) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "parties", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "movie_id"
+    t.time "time"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_parties_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "password_digest"
     t.datetime "created_at", null: false
@@ -29,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_03_26_013947) do
     t.string "email"
   end
 
+  add_foreign_key "parties", "users"
 end
