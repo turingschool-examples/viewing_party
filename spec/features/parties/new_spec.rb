bd70d1294f873bd9970615c2446f18aa592da1c5
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-30 18:20:04 -0500

    Add test for creating a new party and inviting
    Co-authored-by: Jake Volpe <62768837+javolpe@users.noreply.github.com>
    Co-authored-by: Jordan Beck <68141454+jordanfbeck0528@users.noreply.github.com>

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -1,63 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe "New Party Page" do
-  describe "As an authenticated user" do
-    describe "When I visit the new viewing party page" do
-      it "I should see the name of the movie title rendered above a form with some fields" do
-        user = User.create(password: "hello", email: "sample@email.com")
-        follower_1 = User.create(password: "hello1", email: "sample12@email.com")
-        follower_2 = User.create(password: "hello2", email: "sample45@email.com")
-        Follow.create!(follower_id: follower_1.id, followee_id: user.id)
-        Follow.create!(follower_id: follower_2.id, followee_id: user.id)
-        visit root_path
-        click_on "Log In!"
-        fill_in :email, with: user.email
-        fill_in :password, with: user.password
-        click_on "Log In"
-        visit new_party_path
-        save_and_open_page
-          within "#party-info" do
-            expect(page).to have_css("#movie-title")
-            expect(page).to have_field("party[duration]")
-            expect(page).to have_field("party[date]")
-            expect(page).to have_field("party[time]")
-            expect(page).to have_content("Include")
-            expect(page).to have_content(follower_1.email)
-            expect(page).to have_content(follower_2.email)
-          end
+  describe "As an authenticated user When I visit the new viewing party page" do
+    it "I should see the name of the movie title rendered above a form with some fields" do
+      user = User.create(password: "hello", email: "sample@email.com")
+      follower_1 = User.create(password: "hello1", email: "sample12@email.com")
+      follower_2 = User.create(password: "hello2", email: "sample45@email.com")
+      Follow.create!(follower_id: follower_1.id, followee_id: user.id)
+      Follow.create!(follower_id: follower_2.id, followee_id: user.id)
+      visit root_path
+      click_on "Log In!"
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      click_on "Log In"
+      VCR.use_cassette('new_party_1') do
+      visit new_party_path({ movie_id: 550 })
+        within "#party-info" do
+          expect(page).to have_css("#movie-title")
+          expect(page).to have_field("party[duration]")
+          expect(page).to have_field("party[date]")
+          expect(page).to have_field("party[time]")
+          expect(page).to have_content("Include")
+          expect(page).to have_content(follower_1.email)
+          expect(page).to have_content(follower_2.email)
+        end
       end
-      it "it does not display friends to Include if the user has no friends" do
-        user = User.create(password: "hello", email: "sample@email.com")
-        follower_1 = User.create(password: "hello1", email: "sample12@email.com")
-        follower_2 = User.create(password: "hello2", email: "sample45@email.com")
-        visit root_path
-        click_on "Log In!"
-        fill_in :email, with: user.email
-        fill_in :password, with: user.password
-        click_on "Log In"
-        visit new_party_path
-          within "#party-info" do
-            expect(page).to_not have_content("Include")
-            expect(page).to_not have_content(follower_1.email)
-            expect(page).to_not have_content(follower_2.email)
-          end
+    end
+    it "it does not display friends to Include if the user has no friends" do
+      user = User.create(password: "hello", email: "sample@email.com")
+      follower_1 = User.create(password: "hello1", email: "sample12@email.com")
+      follower_2 = User.create(password: "hello2", email: "sample45@email.com")
+      visit root_path
+      click_on "Log In!"
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      click_on "Log In"
+      VCR.use_cassette('new_party_2') do
+      visit new_party_path({ movie_id: 550 })
+        within "#party-info" do
+          expect(page).to_not have_content("Include")
+          expect(page).to_not have_content(follower_1.email)
+          expect(page).to_not have_content(follower_2.email)
+        end
       end
-      it "can create a new party by entering the required info" do
-        user = User.create(password: "hello", email: "sample@email.com")
-        follower_1 = User.create(password: "hello1", email: "sample12@email.com")
-        follower_2 = User.create(password: "hello2", email: "sample45@email.com")
-        Follow.create!(follower_id: follower_1.id, followee_id: user.id)
-        Follow.create!(follower_id: follower_2.id, followee_id: user.id)
-        visit root_path
-        click_on "Log In!"
-        fill_in :email, with: user.email
-        fill_in :password, with: user.password
-        click_on "Log In"
-        visit new_party_path
-          # within "#party-info" do
-          #   fill_in "party[duration]", with: 180
-          #   fill_in "party[date]", with:
-          # end
+    end
+    it "can create a new party by entering the required info" do
+      user = User.create(password: "hello", email: "sample@email.com")
+      follower_1 = User.create(password: "hello1", email: "sample12@email.com")
+      follower_2 = User.create(password: "hello2", email: "sample45@email.com")
+      Follow.create!(follower_id: follower_1.id, followee_id: user.id)
+      Follow.create!(follower_id: follower_2.id, followee_id: user.id)
+      visit root_path
+      click_on "Log In!"
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      click_on "Log In"
+      VCR.use_cassette('new_party_3') do
+      visit new_party_path({ movie_id: 550 })
+        within "#party-info" do
+          fill_in "party[duration]", with: 180
+          fill_in "party[date]", with: "04/01/2021"
+          fill_in "party[time]", with: "1:00 PM"
+          within "#follower-#{follower_1.id}" do
+            page.check("invited_#{follower_1.id}")
+          end
+          within "#follower-#{follower_2.id}" do
+            page.check("invited_#{follower_2.id}")
+          end
+          click_button("Create Party")
+        end
       end
     end
   end
