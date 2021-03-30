14b1a57c06a4dbad764b77578eb8e11b4af7f3e0
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-29 21:28:10 -0500

    Add feature test for creating a new party.

---
+++ spec/features/parties/new_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "New Party Page" do
+  describe "As an authenticated user" do
+    describe "When I visit the new viewing party page" do
+      it "I should see the name of the movie title rendered above a form with some fields" do
+        visit new_party_path
+          within "#party-info" do
+            expect(page).to have_css("#movie-title")
+          end
+      end
+    end
+  end
+end
