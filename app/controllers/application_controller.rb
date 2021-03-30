fde49e83562548e6704c62b74cd671a906908ab0
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-03-30 10:53:23 -0600

    Make a require login method for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+    def require_login
+      unless current_user
+        flash[:error] = "You must be logged in to view this page"
+        redirect_to root_path
+        end
+      end
+
+  helper_method :current_user, :require_login
 end
