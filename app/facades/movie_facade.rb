be146258d090f8881a81a2fc1411eda5772c5c4f
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-03-30 17:19:26 -0600

    add trending to facades

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -4,6 +4,8 @@ class MovieFacade
   def self.search_results(search, limit)
     if search == 'Top Rated'
       self.top_rated(limit)
+    elsif search == 'Trending Movies'
+      self.trending(10)
     elsif search && search != ''
       self.search(search, limit)
     end
@@ -13,6 +15,10 @@ class MovieFacade
     MovieService.top_rated(limit)
   end
 
+  def self.trending(limit)
+    MovieService.trending(limit)
+  end
+
   def self.search(keywords, limit)
     MovieService.search(keywords, limit)
   end
@@ -20,4 +26,5 @@ class MovieFacade
   def self.movie_info(api_movie_id)
     MovieService.movie_info(api_movie_id)
   end
+
 end
