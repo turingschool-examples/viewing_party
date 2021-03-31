2bf0e08b965dd2d3296ecd3aa0aa7813df0f0b8d
kylejschulz <kylschulz@gmail.com>
2021-03-31 18:04:04 -0400

    Delete database and add delivery_method Co-authored-by: Andrew Johnston <72912074+avjohnston@users.noreply.github.com> Co-authored-by: Megan Gonzales <62969459+MGonzales26@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Party.destroy_all
+Movie.destroy_all
+Invitee.destroy_all
+Follow.destroy_all
