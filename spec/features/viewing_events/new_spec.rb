245b2cb2a4f477535262b0521ccf734fc63b5ee7
Gvieve <gnuebel13@gmail.com>
2021-03-30 23:01:56 -0600

    Create feature test for new viewing party
    Co-authored-by: Harrison Blake <72946334+harrison-blake@users.noreply.github.com>
    Co-authored-by: Jacob Arellano <58375638+jakejakearell@users.noreply.github.com>

---
+++ spec/features/viewing_events/new_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe "As an authenticated user, when I visit the movies detail page" do
+  before :each do
+    @user_1 = User.create!(email: 'sassy@email.com', password: 'sassyperson1')
+    @user_2 = User.create!(email: 'grumpy@email.com', password: 'grumpyperson1')
+    @user_3 = User.create!(email: 'sleepy@email.com', password: 'sleepyperson1')
+    @user_4 = User.create!(email: 'dopey@email.com', password: 'dopeyperson1')
+    @friend_1 = Friend.create!(follower_id: @user_1.id, followed_id: @user_2.id)
+    @friend_2 = Friend.create!(follower_id: @user_1.id, followed_id: @user_3.id)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+
+    VCR.use_cassette('create_viewing_party') do
+      @movies = MoviesFacade.top40
+      @find_movie = @movies.third
+      visit movie_path(@find_movie.id)
+      @movie = MoviesFacade.movie_info(@find_movie.id)
+    end
+    click_button 'Create Viewing Party for Movie'
+  end
+
+  describe "and I click 'Create Viewing Party for Movie' button" do
+    it "I see a form with the movie title, duration, date, time" do
+      time = Time.now.strftime("%Y-%m-%d")
+
+      expect(page).to have_content("#{@find_movie.title}")
+      expect(page).to have_field(:duration, with: "#{@movie.runtime}")
+      expect(page).to have_field(:start_date, with: time)
+      expect(page).to have_field(:start_time)
+    end
+
+    it "I also see an invite section where I can invite my friends" do
+      friend_1 = @user_1.followed.first
+      friend_2 = @user_1.followed.last
+
+      expect(page).to have_unchecked_field("friends_#{friend_1.id}")
+      expect(page).to have_unchecked_field("friends_#{friend_2.id}")
+    end
+  end
+end
