7c6d37c8ee70fa31fc859f021b5db9564bf5df5d
Gvieve <gnuebel13@gmail.com>
2021-03-30 23:37:02 -0600

    Create new viewing party view and create action
    Co-authored-by: Harrison Blake <72946334+harrison-blake@users.noreply.github.com>
    Co-authored-by: Jacob Arellano <58375638+jakejakearell@users.noreply.github.com>

--- app/controllers/viewing_events_controller.rb
+++ app/controllers/viewing_events_controller.rb
@@ -1,7 +1,29 @@
 class ViewingEventsController < ApplicationController
-  def new; end
+  def new
+    @movie = Movie.find_by(movie_db_id: session[:movie_info]["movie_db_id"])
+    if @movie.nil?
+      @movie = Movie.create!(session[:movie_info])
+    else
+      @movie
+    end
+  end
 
-  def create_movie
-    require "pry"; binding.pry
+  def create
+    viewing_event = ViewingEvent.new(viewing_event_params)
+    if viewing_event.save && params[:friends]
+      params[:friends].each do |friend|
+        Viewer.new(user_id: friend, viewing_event_id: viewing_event.id)
+      end
+      redirect_to dashboard_index_path
+    else
+      flash.now[:errors] = viewing_event.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def viewing_event_params
+    params.permit(:duration, :start_date, :start_time, :user_id, :movie_id)
   end
 end
