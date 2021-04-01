6179eed2ef502cd7fd9f36a28b22c1c034ba0dcd
DougWelchons <doug.welchons@gmail.com>
2021-03-31 19:18:59 -0600

    Add refactor into cast facade

--- app/controllers/cast_controller.rb
+++ app/controllers/cast_controller.rb
@@ -1,7 +1,6 @@
 class CastController < ApplicationController
 
   def show
-    @person = MovieService.person_info(params[:id])
-    @results = MovieService.movies_by_cast_id(params[:id], 40)
+    @show = CastFacade.new(params[:id], 40)
   end
 end
