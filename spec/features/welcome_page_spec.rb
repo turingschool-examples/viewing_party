fb1443808656efb52b5b7a4091e3b575e98b8e76
jaharaclark <jaharamclark@gmail.com>
2021-05-11 11:55:30 -1000

    Pause for time now

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe 'Welcome Page', type: :feature do
       (Details: Implement basic auth in the application allowing a user to log in with an email and password. The password should be stored in the database using bcrypt)." do
       user = User.create(username: "ClaSte", password: "1234")
       visit root_path
-      expect(page).to have_content("Welome! We're Glad You're Here! At Viewing Party, we make it easy to watch a movie with friends from th")
+      expect(page).to have_content("Welome! We're Glad You're Here! At Viewing Party, we make it easy to watch a movie with friends from the world. Let's get started!")
       fill_in :username, with: user.username
       fill_in :password, with: user.password
 
       click_on "Log In"
 
       expect(current_path).to eq(root_path)
-      expect(page).to have_content("Welcome, #{user.username}!We're Glad You're Here! At Viewing Party, we make it easy to watch a movie with friends from th")
+      expect(page).to have_content("Welcome, #{user.username}!We're Glad You're Here! At Viewing Party, we make it easy to watch a movie with friends from the world. Let's get started!")
     end
   end
 end
