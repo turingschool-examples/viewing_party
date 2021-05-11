8c1fa3f53dd64de9fe14c4db8a9c1d791d55d243
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-11 15:22:14 -0600

    Create movies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_210909) do
+ActiveRecord::Schema.define(version: 2021_05_11_212001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "api_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
