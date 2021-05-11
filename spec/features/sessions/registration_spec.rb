c79e149ff077a665e1a856a64daba281e3c96345
Joey Haas <josephthaas@gmail.com>
2021-05-11 13:52:51 -0600

    Created the first test for the registration page to display after selecting from the root page

---
+++ spec/features/sessions/registration_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+    
+RSpec.describe 'show registration form' do
+
+  describe 'i can create a new user' do
+    it 'can create a new user without error' do
+        visit root_path
+
+        click_link "Register"
+
+        expect(current_path).to eq("/register")
+
+        fill_in :user_email, with: "joeyh@gmail.com"
+        fill_in :user_password, with: "Password"
+        fill_in :user_password_confirmation, with: "Password"
+
+
+        click_button "Register"
+        # This needs to be changed to equal to the user dashboard, based on the session ID
+        expect(current_path).to eq(root_path)
+        user = User.last
+        expect(user.email).to eq("joeyh@gmail.com")
+      end
+    end
+  end
