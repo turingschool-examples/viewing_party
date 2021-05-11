53f1825045dadbd78b28be455952b9ddfc286e06
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-11 15:10:57 -0600

    Add user_name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_202142) do
+ActiveRecord::Schema.define(version: 2021_05_11_210909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_11_202142) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
   end
 
 end
