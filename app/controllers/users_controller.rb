bbdf5028a5203a8dcef34a5d72cd9bff10e0f5a1
Joey Haas <josephthaas@gmail.com>
2021-05-11 15:34:13 -0600

    update rubocop violations for indentations across multiple files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = user_params
+    user[:user_name] = user[:user_name].downcase
+    new_user = User.create(user)
+
+    if new_user.save
+      flash[:info] = 'Registration Succesful'
+      redirect_to root_path
     end
-    def create
+  end
 
-        user = user_params
-        user[:user_name] = user[:user_name].downcase
-        new_user = User.create(user)
+  private
 
-        if new_user.save
-            flash[:info] = "Registration Succesful"
-            redirect_to root_path
-        end
-    end
-
-    private
-
-    def user_params
-        params.require(:user).permit(:user_name, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:user_name, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
-class User <ApplicationRecord 
-    has_secure_password
+class User < ApplicationRecord
+  has_secure_password
 
-    validates :user_name, presence: true, uniqueness: true
-    validates :password_digest, presence: true
-    validates :password, confirmation: { case_sensitive: true }
+  validates :user_name, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, confirmation: { case_sensitive: true }
 end
