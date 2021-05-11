3cfe13194f6d2edaaec18dd15e980e31c1b8cc8c
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-11 14:51:17 -0600

    Fix shoulda matcher syntax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User do
   describe 'validations' do
-    it {should validate_presence_of :email}
-    it {should validate_uniqueness_of :email}
-    it {should validate_presence_of :password_digest}
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should have_secure_password }
   end
 
   describe 'relationships' do
-    # User - has many friends(other users) through UserFriends, dependent parties, dependent UserFriends
-    it {should have_many :user_friends}
-    it {should have_many (:friends).through(:user_friends) }
+    it { should have_many(:user_friends) }
+    it { should have_many(:friends).through(:user_friends) }
+    it { should have_many(:parties) }
   end
 end
