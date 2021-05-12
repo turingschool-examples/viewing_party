e5b013d9c87b3b2f33da1599db686e5dc58e3d36
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-05-11 22:49:43 -0600

    add dependency tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User do
   describe 'relationships' do
-    it { should have_many :friends }
-    it { should have_many :parties }
-    it { should have_many :guests }
+    # it { should have_many :friends }
+    # it { should have_many :parties }
+    # it { should have_many :guests }
+    it { should have_many(:friends).dependent(:destroy) }
+    it { should have_many(:parties).dependent(:destroy) }
+    it { should have_many(:guests).dependent(:destroy) }
   end
 end
