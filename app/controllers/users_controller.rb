b13a99004e2baf2a0472868441fb9e2e4a019670
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-11 21:38:20 -0600

    Pass user params in create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new 
+    @user = User.new
+  end
+
+  def create
+    user = user_params
+    user_email = user[:email].downcase
+    new_user = User.create(user_params)
+    if new_user.save
+      session[:user_id] = new_user.id #session comes with rails
+      flash[:info] = "Welcome, #{new_user.email}!"
+      redirect_to dashboard_path(user_email: "#{new_user.email}")
+    else
+      flash[:error]
+    end #make sad path error as well
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
