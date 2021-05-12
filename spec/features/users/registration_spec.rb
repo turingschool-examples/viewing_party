015766d7297c0ed99e9ab6e0135dbb81f3c0f454
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-12 18:32:45 -0400

    Add test for unique email and password confirmation

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -21,11 +21,10 @@ RSpec.describe 'Registration Page' do
     end
   end
 
-  describe 'sad path' do
+  describe 'sad path and edge case' do
     it 'email field cannot be blank' do
 
       password = "test"
-
       fill_in "user[password]", with: password
       fill_in "user[password_confirmation]", with: password
       click_button "Register"
@@ -38,7 +37,6 @@ RSpec.describe 'Registration Page' do
     it 'password field cannot be blank' do
 
       email = "example@example.com"
-
       fill_in "user[email]", with: email
       click_button "Register"
 
@@ -52,12 +50,46 @@ RSpec.describe 'Registration Page' do
       fill_in "user[email]", with: "iloveMovies.com"
       fill_in "user[password]", with: "password"
       fill_in "user[password_confirmation]", with: "password"
-
       click_on "Register"
 
       invalid_email = "Email is invalid"
 
       expect(page).to have_content(invalid_email)
+    end
+
+    it 'cannot register with existing account email' do
+      email = "example@example.com"
+      password = "test"
+      fill_in "user[email]", with: email
+      fill_in "user[password]", with: password
+      fill_in "user[password_confirmation]", with: password
+      click_button "Register"
+
+      expect(current_path).to eq('/dashboard')
+
+      visit "/registration"
+
+      fill_in "user[email]", with: email
+      fill_in "user[password]", with: password
+      fill_in "user[password_confirmation]", with: password
+      click_button "Register"
+
+      account_exists = "Email has already been taken"
+
+      expect(page).to have_content(account_exists)
+    end
+
+    it 'password and password confirmation must match' do
+      email = "example@example.com"
+      password = "test"
+      fill_in "user[email]", with: email
+      fill_in "user[password]", with: password
+      fill_in "user[password_confirmation]", with: "wrong0password"
+      click_button "Register"
+      
+      confirmation_error = "Password confirmation doesn't match Password"
+
+      expect(page).to have_content(confirmation_error)
     end
   end
 end
