02fec9c4bdaa3f3c75905d9608746c3beac21d9e
Joey Haas <josephthaas@gmail.com>
2021-05-12 17:41:21 -0600

    ran and fixed rubocop violations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,7 +2,6 @@ class Friendship < ApplicationRecord
   validates :followee_id, presence: true
   validates :follower_id, presence: true
 
-
   belongs_to :follower, class_name: 'User'
   belongs_to :followee, class_name: 'User'
 end
