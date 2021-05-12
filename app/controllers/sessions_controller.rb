7da9c5bfc7b50c32cef1df03573909b6f0f7246e
joemecha <jwmecha@gmail.com>
2021-05-11 20:38:44 -0500

    Refactor welcome Page and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,19 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(user_name: params[:user_name])
+    require 'pry'; binding.pry
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:info] = "Welcome, #{user.user_name}!"
-      redirect_to root_path
+      redirect_to dashboard_path
     else
-      flash[:error] = 'Your email or password are incorrect'
       render :new
+      flash[:error] = 'Your email or password are incorrect'
     end
   end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_path
-  end
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to root_path
+  # end
 end
