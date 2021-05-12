80b40a067c86d318f5dff175ff154ae8fc195e2a
koshea9 <keegan.oshea9@gmail.com>
2021-05-11 19:34:30 -0600

    Fix formatting with rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :username, presence: true
-  validates_presence_of :password, require: true
+  validates :password, presence: { require: true }
   has_secure_password
 end
