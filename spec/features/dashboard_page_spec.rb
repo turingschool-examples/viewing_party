259f4c47170d854b131d6e328dd13a8e5a3f53d9
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-12 17:44:12 -0600

    Complete specs for dashboard page

--- spec/features/dashboard_page_spec.rb
+++ spec/features/dashboard_page_spec.rb
@@ -10,22 +10,32 @@ require 'rails_helper'
 
 RSpec.describe 'Dashboard', type: :feature do
   describe 'Dashboard Page' do
+    before(:each) do
+      @new_user = User.last
+    end
+
     it "I can see and click a button to discover movies" do
       visit dashboard_path
 
-      expect(page).to have_content("Discover Movies")
+      expect(page).to have_button("Discover Movies")
 
-      click_link "Discover Movies"
+      click_button "Discover Movies"
 
       expect(current_path).to eq(discover_path)
     end
 
     it 'I can see a friends section' do
+      visit dashboard_path
 
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Friends")
     end
 
     it 'I can see a viewing parties section' do
+      visit dashboard_path
 
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Viewing Parties")
     end
   end
 end
