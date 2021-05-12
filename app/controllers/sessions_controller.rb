733a41d3a14cf621c7f16117eaab68300afcab41
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-11 21:40:59 -0600

    Sve

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        flash[:info] = "Welcome #{user.email}!"
-        redirect_to root_path
+      session[:user_id] = user.id
+      flash[:info] = "Welcome #{user.email}!"
+      redirect_to root_path
     else
-        flash[:error] = "Your email or password are incorrect"
-        render :new
+      flash[:error] = "Your email or password are incorrect"
+      render :new
     end
   end
 
-  def destroy
-      session[:user_id] = nil
-      redirect_to root_path
-  end
+  # def destroy
+  #     session[:user_id] = nil
+  #     redirect_to root_path
+  # end
 end
