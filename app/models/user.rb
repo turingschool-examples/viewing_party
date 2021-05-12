c60ea9ad625361642bde7128f19d18d90c3cf3db
Harrison Blake <harrison.r.blake.gmail.com>
2021-05-12 16:15:47 -0600

    Add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :username, presence: true
   validates :password, presence: { require: true }
+  validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   has_secure_password
+
 end
