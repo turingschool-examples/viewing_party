27abae05b9ae0977a77101c6d02aa30a9ba50e74
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-05-11 23:18:11 -0600

    add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :friends, dependent: :destroy
   has_many :parties, foreign_key: :host_id, dependent: :destroy
   has_many :guests, dependent: :destroy
+
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe User do
     it { should have_many(:parties).dependent(:destroy) }
     it { should have_many(:guests).dependent(:destroy) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:password) }
+  end
 end
