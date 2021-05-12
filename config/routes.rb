c5dfb3f63cb2966a80fb31c45f58ab183525de1c
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-05-12 17:53:53 -0600

    add test for root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root to: "application#welcome"
+
+  get "/register", to: "users#new"
+  get "/login", to: "sessions#new"
+
+  resources :users, except: [:new]
+  resources :sessions, except: [:new]
+
 end
---
+++ spec/features/application/welcome_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+Rspec.describe 'welcome page' do
+  context "you go to main page" do
+
+    before :each do
+      visit root_path
+    end
+
+    it "has welcome message" do
+      expect(page).to have_content("Welcome to ViewingParty")
+    end
+
+    it "has application discription" do
+      expect(page).to have_content("the app for planing movie parties with your friends")
+    end
+
+    context "you arrive at page for first time" do
+      it "has link to register a new user" do
+
+        click_on "Register new user"
+        expect(current_path).to eq(register_path)
+
+        fill_in("Email"), with: "555@dooffus.edu"
+        fill_in("Password"), with: "password"
+        fill_in("Name"), with: "The Doof"
+        click_on("Register")
+
+        expect(current_path).to eq(root_path)
+      end
+
+      it "allows existing user to login" do
+        user = create(:user)
+
+        click_on("Login")
+        expect(current_path).to eq(login_path)
+
+        fill_in("Email"), with: "#{user.email}"
+        fill_in("Password"), with: "#{user.password}"
+
+        click_on("Login")
+        expect(current_path).to eq(root_path)
+      end
+    end
+
+    context "when you are allready logged in" do
+      before :each do
+        @user = create(:user)
+        visit root_path
+
+        click_on("Login")
+        fill_in("Email"), with: "#{user.email}"
+        fill_in("Password"), with: "#{user.password}"
+
+        click_on("Login")
+      end
+      it "has no login and no register link" do
+
+        expect(page).to_not have_link("Login")
+        expect(page).to_not have_link("Register new user")
+      end
+
+      it "has a log out link" do
+        expect(page).to have_link("Logout")
+      end
+
+    end
+
+  end
+end
