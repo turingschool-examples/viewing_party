4e1304cd54e946786587d4520a601eac41ee4292
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-12 20:28:21 -0400

    Make password validation one line

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-  validates :password, presence: true
-  validates :password, confirmation: true
+  validates :password, presence: true, confirmation: true
   has_secure_password
 end
