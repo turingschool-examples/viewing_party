f3e3125ef43507369cc70a3577d1106ea50a8798
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-12 19:52:07 -0600

    Add sad path test and remove extra spacing

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'welcome index page' do
     visit root_path
     expect(page).to have_content('Welcome!')
     expect(page).to have_content('This is the description')
-    expect(page).to have_link('Log in')
+    expect(page).to have_link('Log In')
     expect(page).to have_link('Register As A User')
   end
 end
