55a387fc2ed35dfc8992846ac56d49c5efb488e6
loganjacob76 <nagolisback@gmail.com>
2021-05-13 11:56:05 -0600

    Add email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User do
   end
 
   describe 'validations' do
+    it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:password_digest) }
