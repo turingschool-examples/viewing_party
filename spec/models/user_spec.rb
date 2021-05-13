8ca283242e1463518aa139cef0c2df06e7728d52
loganjacob76 <nagolisback@gmail.com>
2021-05-13 11:53:29 -0600

    Update models to remove redundant tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User do
   describe 'relationships' do
-    # it { should have_many :friends }
-    # it { should have_many :parties }
-    # it { should have_many :guests }
     it { should have_many(:friends).dependent(:destroy) }
     it { should have_many(:parties).dependent(:destroy) }
     it { should have_many(:guests).dependent(:destroy) }
