b29de16175a985c8071b3d179e039fd7d744ebc0
Joey Haas <josephthaas@gmail.com>
2021-05-13 23:51:59 -0600

    Added passing test to display top 40 movies and updates to view

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -7,7 +7,10 @@ class DiscoverController < ApplicationController
     if params[:search].present?
       @user = current_user
       @results = MovieInfo.new(params[:search]).search_results
+    elsif params[:top_40].present?
+      @user = current_user
+      @popular_movies = MovieInfo.new(params).popular_movies
+      binding.pry
     end
-    binding.pry
   end
 end
