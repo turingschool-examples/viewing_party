634267a9dc82836c9bbd5a273ab06020e3e7b062
koshea9 <keegan.oshea9@gmail.com>
2021-05-13 18:44:15 -0600

    Add service test for top movies

---
+++ spec/services/moviedb_service_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe MoviedbService do
+  before(:each) do
+    @service = MoviedbService.new
+  end
+
+  it "returns top 20 movies" do
+    expect(@service.get_popular_movies.count).to eq(20)
+  end
+end
--- spec/services/moviedb_service_test.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe Moviedb do
-  attr_reader :service
-
-  before(:each) do
-    @service = Moviedb.new
-  end
-end
