d550a51ab7a54bf9a5873906d57285f2a56ac19e
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-14 20:30:39 -0600

    Add top 40 movies iv to index method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
 class MoviesController < ApplicationController
   def index
+    @top_40_movies = MovieService.get_top_rated_movies
+    #refactor this into a facade later 
+  end
 
-  end 
+  def show
+    #Jahara info, we need to make a poro/facade to call something such as
+    #@movie = MoviesFacade.get_movie_details(params[:id])
+    #session[:movie] = { api_id: @movie[:id],
+                        # title: @movie[:original_title],
+                        # duration: @movie[:length]
+                        #but even before this, i think we need to make a poro
+                        #for the movie so we can just call things like
+                        #@movie.id for the above hash
+  end
 end
