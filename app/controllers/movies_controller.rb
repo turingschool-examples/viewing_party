b0737fab049b0e40b536805863cf4ee37692bbb0
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-05-15 10:48:35 -0500

    Make sad path for blank search field on discover page
    Co-authored-by: Diana Buffone <71909590+Diana20920@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,14 @@
 class MoviesController < ApplicationController
   def index
-    if params["movie_title"]
+    if params["movie_title"] == ""
+      flash[:error] = "Search field cannot be blank."
+      redirect_to discover_path
+    elsif params["movie_title"]
       movie_keyword = params["movie_title"]
       response = Faraday.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIE_KEY']}&language=en-US&page=1&include_adult=false") do |req|
         req.params['query'] = movie_keyword
       end
-      parsed = JSON.parse(response.body, symbolize_names: true)  
+      parsed = JSON.parse(response.body, symbolize_names: true)
       @search_results = parsed[:results].map do |result|
         Film.new(result)
       end.first(40)
