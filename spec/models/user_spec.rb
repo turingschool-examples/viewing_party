f150552b90d5ec74e3364b661f5d3b49ebb3fccc
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-16 13:49:29 -0400

    Create friendship table and relationship with user
    Co-authored-by: Gaelyn Cooper <57960885+gaelyn@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password) }
     it { should validate_confirmation_of(:password) }
   end
+
+  describe 'relationships' do
+    it { should have_many(:friendships) }
+    it { should have_many(:friends).through(:friendships) }
+  end
 end
