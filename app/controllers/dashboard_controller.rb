ecfc4e2cb6fdb37d24f21f15d85caf2633989fb5
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-16 16:39:51 -0600

    Add create method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,17 @@
 class DashboardController < ApplicationController
   def index
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:user_id])
+    session[:user_id] = @user.id
     @user_email = params[:user_email]
+  end
+
+  def create
+    if params.include?(:friend_search)
+      user = User.find(params[:user_id])
+      friend = params[:friend_search]
+      search = User.find_by(email: "#{friend}")
+      UserFriend.create!(user_id: user.id, friend_id: search.id)
+      redirect_to dashboard_index_path(user_id: "#{user.id}")
+    end
   end
 end
