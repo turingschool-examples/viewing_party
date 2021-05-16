04700d49f824e0990860d135a9d60d569b63fd15
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-16 14:06:03 -0400

    Add test for creating friendships feature
    Co-authored-by: Gaelyn Cooper <57960885+gaelyn@users.noreply.github.com>

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -29,6 +29,30 @@ RSpec.describe 'Dashboard Page' do
 
       expect(current_path).to eq('/discover')
     end
+
+    describe 'I can add friends' do
+      it 'by entering their email address and clicking add friend' do
+        new_friend = User.create!(email: 'world@email.com', password: "hello")
+
+        visit '/login'
+        email = "example@example.com"
+        password = "test"
+        user = User.create!(email: email, password: password)
+
+        fill_in :email, with: email
+        fill_in :password, with: password
+        click_button "Log In"
+
+        expect(page).to have_content("You currently have no friends")
+        exptect(page).to_not have_content(new_friend.email)
+
+        fill_in :friend_email, with: new_friend.email
+        click_button "Add Friend"
+
+        expect(page).to have_content(new_friend.email)
+        expect(page).to_not have_content("You currently have no friends")
+      end
+    end
   end
 
   describe 'sad path and edge case' do
