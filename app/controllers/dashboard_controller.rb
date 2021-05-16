3af019249a4618b5f1a5af03893f415f6f346296
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-16 17:33:36 -0600

    Add conditional for sad path create method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,17 +1,32 @@
 class DashboardController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    session[:user_id] = @user.id
-    @user_email = params[:user_email]
+    if params.include?(params[:user_id])
+      #currently user_id is only being passed after registration
+      @user = User.find_by(email: "#{params[:user_email]}")
+      session[:user_id] = @user.id
+      @user_email = @user.email
+    else
+      #i need to pass user_id in the login form somehow, but
+      #i do not have @user to get from
+      @user = User.find(params[:user_id])
+      session[:user_id] = @user.id
+      @user_email = params[:user_email]
+    end
   end
 
   def create
     if params.include?(:friend_search)
       user = User.find(params[:user_id])
       friend = params[:friend_search]
-      search = User.find_by(email: "#{friend}")
-      UserFriend.create!(user_id: user.id, friend_id: search.id)
-      redirect_to dashboard_index_path(user_id: "#{user.id}")
+      # binding.pry
+      if User.find_by(email: "#{friend}").nil?
+        flash[:error] = "I'm sorry your frined cannot be found"
+        redirect_to dashboard_index_path(user_id: "#{user.id}")
+      else
+        search = User.find_by(email: "#{friend}")
+        UserFriend.create!(user_id: user.id, friend_id: search.id)
+        redirect_to dashboard_index_path(user_id: "#{user.id}")
+      end
     end
   end
 end
