ecac2e539873d9f8ad0dc61d69cf02cbb10262d6
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-16 16:40:44 -0600

    Pass user object to dashboard controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     user = user_params
     user_email = user[:email].downcase
-    new_user = User.create(user_params)
+    new_user = User.create!(user_params)
     if !new_user.save
       flash.now[:error] = "Please make sure the passwords match"
       # redirect_to register_path
@@ -14,16 +14,13 @@ class UsersController < ApplicationController
     else new_user.save
       session[:user_id] = new_user.id
       flash[:info] = "Welcome, #{new_user.email}!"
-      redirect_to dashboard_path(user_email: "#{new_user.email}")
+      redirect_to dashboard_index_path(user_email: "#{new_user.email}", user_id: "#{new_user.id}")
     end
   end
 
-
   private
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
-
 end
-
-
