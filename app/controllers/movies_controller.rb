33bc06246a14cc1a384243c60122ae191c5f0a6f
loganjacob76 <nagolisback@gmail.com>
2021-05-16 20:10:54 -0600

    Add routes for discover/index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,11 @@
 class MoviesController < ApplicationController
+  def index
+    @title = params[:title].gsub(' ', '%20') if params[:title]
+  end
+
   def show
     @movie_id = params[:id]
   end
+
+  def discover; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   get "/register", to: "users#new"
   get "/login", to: "sessions#new"
+  get '/discover', to: 'movies#discover'
 
   resources :users, except: [:new]
   resources :sessions, except: [:new]
-  resources :movies
-
+  resources :movies, except: [:new]
 end
