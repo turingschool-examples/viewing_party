ef0606a52214870a47db175e0eb7b136cc5161a1
Kseniya K <kseniya.kudelich@gmail.com>
2021-05-16 19:44:38 -0600

    Update cassetes to reflect 2 pages of top 40 result

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -3,11 +3,11 @@ class MovieService
   def top_40
     token = ENV['movie_token']
 
-    response1 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{token}&page=1&language=en-US").body
-    response2 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{token}&page=2&language=en-US").body
+    response1 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{token}&page=1&language=en-US")
+    response2 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{token}&page=2&language=en-US")
 
-    result1 = JSON.parse(response1, symbolize_names: true)
-    result2 = JSON.parse(response2, symbolize_names: true)
+    result1 = JSON.parse(response1.body, symbolize_names: true)
+    result2 = JSON.parse(response2.body, symbolize_names: true)
 
     result1[:results] + result2[:results]
   end
