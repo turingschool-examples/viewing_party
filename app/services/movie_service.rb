cf01b4007013000e4da6e8e861c5e55c3bd41cb7
Kseniya K <kseniya.kudelich@gmail.com>
2021-05-16 19:16:49 -0600

    Fix top 40 results

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -3,9 +3,12 @@ class MovieService
   def top_40
     token = ENV['movie_token']
 
-    response = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{token}&language=en-US&page=1&page=2")
+    response1 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{token}&page=1&language=en-US").body
+    response2 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{token}&page=2&language=en-US").body
 
-    data = response.body
-    JSON.parse(data, symbolize_names: true)
+    result1 = JSON.parse(response1, symbolize_names: true)
+    result2 = JSON.parse(response2, symbolize_names: true)
+
+    result1[:results] + result2[:results]
   end
 end
