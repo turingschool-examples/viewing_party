b1fd1fcbadfe3b2697cff24056016a18cd3c6cf9
loganjacob76 <nagolisback@gmail.com>
2021-05-16 20:11:34 -0600

    Add api helper methods

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -1,7 +1,7 @@
 class TMDBService
-  def initialize(token, movie_id)
+  def initialize(token, movie_info)
     @token = token
-    @movie_id = movie_id
+    @movie_info = movie_info
   end
 
   def conn
@@ -9,17 +9,45 @@ class TMDBService
   end
 
   def get_movie_info
-    resp = conn.get("/3/movie/#{@movie_id}?api_key=#{@token}")
+    resp = conn.get("/3/movie/#{@movie_info}?api_key=#{@token}")
     JSON.parse(resp.body, symbolize_names: true)
   end
 
   def get_first_10_credits
-    resp = conn.get("/3/movie/#{@movie_id}/credits?api_key=#{@token}")
+    resp = conn.get("/3/movie/#{@movie_info}/credits?api_key=#{@token}")
     JSON.parse(resp.body, symbolize_names: true)[:cast][0..9]
   end
 
   def get_movie_reviews
-    resp = conn.get("/3/movie/#{@movie_id}/reviews?api_key=#{@token}")
+    resp = conn.get("/3/movie/#{@movie_info}/reviews?api_key=#{@token}")
     JSON.parse(resp.body, symbolize_names: true)
+  end
+
+  def top_movies
+    top_page1 + top_page2
+  end
+
+  def top_page1
+    resp = conn.get("/3/movie/top_rated?api_key=#{@token}")
+    JSON.parse(resp.body, symbolize_names: true)[:results]
+  end
+
+  def top_page2
+    resp = conn.get("/3/movie/top_rated?api_key=#{@token}&page=2")
+    JSON.parse(resp.body, symbolize_names: true)[:results]
+  end
+
+  def movie_search
+    search_page1 + search_page2
+  end
+
+  def search_page1
+    resp = conn.get("/3/search/movie?api_key=#{@token}&query=#{@movie_info}")
+    JSON.parse(resp.body, symbolize_names: true)[:results]
+  end
+
+  def search_page2
+    resp = conn.get("/3/search/movie?api_key=#{@token}&query=#{@movie_info}&page=2")
+    JSON.parse(resp.body, symbolize_names: true)[:results]
   end
 end
