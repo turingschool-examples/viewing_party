74620094c10ad5eb353b74df9d2586a32f707a5d
loganjacob76 <nagolisback@gmail.com>
2021-05-17 17:32:37 -0600

    Update gsub method to applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,8 @@ class ApplicationController < ActionController::Base
   def error_message(errors)
     errors.full_messages.join(', ')
   end
+
+  def to_uri(param)
+    param.gsub(' ', '%20')
+  end
 end
