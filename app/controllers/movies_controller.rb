d421c121af16f63f360148d994ad286431abd11f
loganjacob76 <nagolisback@gmail.com>
2021-05-17 17:33:21 -0600

    Update files to match service refactor

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,12 @@
 class MoviesController < ApplicationController
   def index
-    @title = params[:title].gsub(' ', '%20') if params[:title]
+    @title = to_uri(params[:title]) if params[:title] && params[:title].present?
+
+    @service = if @title
+                 FilmService.movie_search(@title)
+               else
+                 FilmService.top_movies
+               end
   end
 
   def show
