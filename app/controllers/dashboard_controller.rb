d5cfa533bd58ca39db3a415512b8804e7a7a426a
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-18 10:52:59 -0600

    Add create method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,20 @@
-
 class DashboardController < ApplicationController
   def index
-    @user = User.find_by(params[:id])
-    @user_email = params[:user_email]
+    @user = current_user
+  end
+
+  def create
+    if params.include?(:friend_search)
+      user = current_user
+      friend = params[:friend_search]
+      if User.find_by(email: "#{friend}").nil?
+        flash[:error] = "#{friend} is not currently active with Viewing Party."
+        redirect_to dashboard_index_path
+      else
+        search = User.find_by(email: "#{friend}")
+        UserFriend.create(user_id: user.id, friend_id: search.id) if search.id != current_user.id
+        redirect_to dashboard_index_path
+      end
+    end
   end
 end
