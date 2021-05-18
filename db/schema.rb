546780d62a118136a658ce70c9c6ccc98228b151
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-05-17 19:06:01 -0500

    Change start_time to string in parties table
    Co-authored-by: Diana Buffone <71909590+Diana20920@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_205855) do
+ActiveRecord::Schema.define(version: 2021_05_18_000254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_05_16_205855) do
   create_table "parties", force: :cascade do |t|
     t.integer "duration"
     t.date "date"
-    t.time "start_time"
+    t.string "start_time"
     t.string "movie_title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
