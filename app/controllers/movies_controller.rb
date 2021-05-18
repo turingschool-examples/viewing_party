543bda5dfdcc02dec8d73758078e0700dcc54b20
loganjacob76 <nagolisback@gmail.com>
2021-05-17 23:42:16 -0600

    Update rails_helper for coverage

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,6 @@ class MoviesController < ApplicationController
 
   def show
     @movie_id = params[:id]
-
   end
 
   def discover; end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,8 +34,11 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.before :each do |test|
-    user = create(:user) unless test.metadata[:logged_out]
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    @user = create(:user)
+    
+    allow_any_instance_of(ApplicationController)
+    .to receive(:current_user)
+    .and_return(@user) unless test.metadata[:logged_out]
   end
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
