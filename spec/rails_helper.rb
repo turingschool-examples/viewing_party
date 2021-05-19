9a25dfa04512bba861ed7695924d8911827595f4
jaharaclark <jaharamclark@gmail.com>
2021-05-18 16:05:08 -1000

    Complete Webmock additions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,4 +66,10 @@ RSpec.configure do |config|
     config.include(Shoulda::Matchers::ActiveRecord, type: :model)
   end
 
+  VCR.configure do |config|
+    config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+    config.hook_into :webmock
+    config.filter_sensitive_data('<MOVIE_TOKEN>') { ENV['MOVIE_TOKEN'] }
+  end
+
 end
