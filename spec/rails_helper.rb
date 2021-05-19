b7e7f54c47e8d8032b18d2a4b367cc579c1106af
Joey Haas <josephthaas@gmail.com>
2021-05-19 09:31:49 -0600

    Updated tests to allow for use of search with large response

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -73,8 +73,9 @@ Shoulda::Matchers.configure do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.filter_sensitive_data('<movie_api_key>') { ENV['movie_api_key'] }
   config.configure_rspec_metadata!
+  config.default_cassette_options = { re_record_interval: 7.days }
 end
