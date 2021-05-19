59930e1810d559d9bad4872da0b048c9acd34e0d
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-18 19:00:09 -0600

    Make index, new, and create methods

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,4 +2,6 @@ class Party < ApplicationRecord
   has_many :party_viewers, dependent: :destroy
   belongs_to :host, class_name: 'User'
   belongs_to :movie
+
+  # validates :party_viewers, presence: true 
 end
