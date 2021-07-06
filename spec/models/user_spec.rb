d5e8be7f92d7006c25474972b3e8da89d3a6a161
twarbelow <taija.warbelow@gmail.com>
2021-07-05 18:34:21 -0700

    update to use has_secure_password in user model and spec, spec passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-describe 'User' do
+describe User do
   describe 'validations' do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:email)}
-    it {should validate_presence_of(:password_digest)}
+    it {should have_secure_password}
   end
 end
