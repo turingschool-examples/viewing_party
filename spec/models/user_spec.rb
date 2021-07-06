824704bc13bae27ed584f7635caf0ca250684121
twarbelow <taija.warbelow@gmail.com>
2021-07-05 23:01:52 -0700

    add parties relationship expectation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,6 @@ describe User do
   describe 'relationships' do
     it {should have_many(:friendships)}
     it {should have_many(:friends).through(:friendships)}
+    it {should have_many(:parties)}
   end
 end
