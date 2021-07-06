325fa39c6d8aaf65138f643edb28b0887e0b3ecc
twarbelow <taija.warbelow@gmail.com>
2021-07-05 22:43:48 -0700

    revise columns

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 describe Party do
   describe 'validations' do
     it {should validate_presence_of(:host)}
-    it {should validate_presence_of(:date)}
-    it {should validate_presence_of(:time)}
-    it {should validate_presence_of(:duration)}
+    it {should validate_presence_of(:start)}
+    it {should validate_presence_of(:end)}
     it {should validate_presence_of(:movie_id)}
     it {should validate_presence_of(:movie_title)}
   end
