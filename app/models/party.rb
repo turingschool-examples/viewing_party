0f7a5b7c22d4d0a3455818fc8b79e2585d234fa0
twarbelow <taija.warbelow@gmail.com>
2021-07-06 14:53:39 -0700

    add iverse_of and dependent: :destroy options

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,4 @@
 class Party < ApplicationRecord
-
   validates :host, presence: true
   validates :movie_id, presence: true
   validates :start, presence: true
@@ -7,7 +6,6 @@ class Party < ApplicationRecord
   validates :movie_title, presence: true
 
   belongs_to :host, class_name: 'User'
-  has_many :invitations
+  has_many :invitations, dependent: :destroy
   has_many :invitees, through: :invitations
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
-  has_many :parties, foreign_key: :host_id, dependent: :destroy
-  has_many :invitations, foreign_key: :invitee_id, dependent: :destroy
+  has_many :parties, foreign_key: :host_id, dependent: :destroy, inverse_of: :user
+  has_many :invitations, foreign_key: :invitee_id, dependent: :destroy, inverse_of: :user
   has_many :parties, through: :invitations
 end
