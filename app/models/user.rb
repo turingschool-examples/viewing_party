85d9dae73ffe4bc06b9635ae1c1fcc8dc63e9f26
twarbelow <taija.warbelow@gmail.com>
2021-07-05 23:03:20 -0700

    add has_many relationship for parties to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
+  has_many :parties, foreign_key: :host_id, dependent: :destroy
 end
