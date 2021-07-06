67db5325333b357533412d45dcbd9c7ae6c6caaf
twarbelow <taija.warbelow@gmail.com>
2021-07-05 22:51:11 -0700

    correct belongs_to syntax

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,10 +2,9 @@ class Party < ApplicationRecord
 
   validates :host, presence: true
   validates :movie_id, presence: true
-  validates :date, presence: true
-  validates :time, presence: true
+  validates :start, presence: true
+  validates :end, presence: true
   validates :movie_title, presence: true
-  validates :duration, presence: true
 
-  belongs_to :user, foreign_key: 'host'
+  belongs_to :host, class_name: 'User'
 end
