8da2503b42bdbfb39f629117f7b96ff24903e7e6
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-07-06 16:27:03 -0600

    fix migration of atendee

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
     it { should have_many(:frienders).class_name('User') }
 
     it { should have_many(:attendees).dependent(:destroy) }
-    it { should have_many(:viewing_parties) }
+    it { should have_many(:movie_parties) }
     # it { should have_many(:viewing_parties).dependent(:destroy)}
   end
 
