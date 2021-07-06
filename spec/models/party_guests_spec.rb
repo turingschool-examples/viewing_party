bf023655c3a41d0e69e71ec77da2a4d653d45bdb
twarbelow <taija.warbelow@gmail.com>
2021-07-06 08:00:57 -0700

    fix typo

--- spec/models/party_guests_spec.rb
+++ spec/models/party_guests_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe PartyGuest do
-  descripte 'relationships' do
+  describe 'relationships' do
     it {should belong_to(:party)}
     it {should belong_to(:user)}
   end
