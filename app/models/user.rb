0f34303a33810027720268f2c1f9be47c23c63dc
kevn7n <kevn_nguyen@yahoo.com>
2021-07-06 14:58:50 -0600

    Update party.rb, user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
 
-  has_many :friendships
+  has_many :parties, dependent: :destroy, foreign_key: :host_id
+  has_many :friendships, dependent: :destroy
   has_many :friends, class_name: :User, through: :friendships
 
   has_secure_password
