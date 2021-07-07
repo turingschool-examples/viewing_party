d16d9ced62416fbacf44e32d6f370ca4588245df
Suzie K <suziekim.dev@gmail.com>
2021-07-07 13:13:10 -0400

    Update registration form page

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -2,19 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "User registration form" do
   it "creates new user" do
+    # commented out because I don't have a welcome page
     # visit root_path 
 
     # click_on "Register Here!"
     # expect(current_path).to eq('/registration')
     visit registration_path 
-    
+   
     email = "hi@here.com"
     password = "hello"
     password_confirmation = "hello"
-
-    fill_in :user_email, with: email 
-    fill_in :user_password, with: password 
-    fill_in :user_password_confirmation, with: password_confirmation
+ 
+    fill_in 'user[email]', with: email 
+    fill_in 'user[password]', with: password 
+    fill_in 'user[password_confirmation]', with: password_confirmation
 
     click_on "Register User"
 
