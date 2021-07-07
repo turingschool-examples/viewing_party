c07ddb5225227cb16cd0ea26f9a08ec7e641da19
Lawrence Whalen <ldwhalen@gmail.com>
2021-07-07 17:51:20 -0600

    Test and create loggin in and out

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post '/users', to: 'users#create'
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users, only: :show
 
 end
