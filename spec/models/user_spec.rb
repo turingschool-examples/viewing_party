dbbef03f55027d2a94b1260ebc4de63b113d5128
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-07-07 16:27:27 -0600

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-
-  describe 'relationships' do
-    it { should have_many(:friends).dependent(:destroy) }
-    it { should have_many(:friendees).class_name('User') }
-    it { should have_many(:frienders).class_name('User') }
-
-    it { should have_many(:attendees).dependent(:destroy) }
-    it { should have_many(:viewing_parties) }
-    # it { should have_many(:viewing_parties).dependent(:destroy)}
-  end
-
-  # describe 'validations' do
-  #   it { should validate_presence_of(:email) }
-  #   it { should validate_uniqueness_of(:email) }
-  #   it { should validate_presence_of(:password_digest) }
-  # end
 
 end
