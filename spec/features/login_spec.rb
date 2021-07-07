5adf884104c62fdd966105380016d0b327a56280
kevn7n <kevn_nguyen@yahoo.com>
2021-07-07 01:37:06 -0600

    Center welcome-login page

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,50 +1,47 @@
 require 'rails_helper'
 
-describe 'logging in' do 
-    it 'as a registered user, I can log in with correct credentials' do 
-        user = User.create(username: 'meg@ex.com', password: 'ilovedogs')
-        
-        visit root_path
+describe 'logging in' do
+  it 'as a registered user, I can log in with correct credentials' do
+    user = User.create(email: 'pat@xyz.com', password: 'viewparty')
 
-        click_link "I already have an account"
+    visit root_path
+    save_and_open_page
+    expect(current_path).to eq("/login")
 
-        expect(current_path).to eq("/login")
-        
-        fill_in :username, with: "meg@ex.com"
-        fill_in :password, with: "ilovedogs"
-        
-        click_button "Log in"
-        
-        expect(current_path).to eq(root_path)
-        expect(page).to have_content("Welcome, meg@ex.com!")
-        expect(page).to_not have_link("I already have an account")
-        expect(page).to_not have_link("Register as a User")
-        expect(page).to have_link("Log out")
-        
-        click_link("Log out")
-        
-        expect(current_path).to eq(root_path)
-        expect(page).to_not have_link("Log out")
-        expect(page).to have_link("I already have an account")
-        expect(page).to have_link("Register as a User")
-    end 
+    fill_in :email, with: "pat@xyz.com"
+    fill_in :password, with: "viewparty"
 
-    it 'as a registered user, I cant log in with incorrect credentials' do 
-        user = User.create(username: 'meg@ex.com', password: 'ilovedogs')
-        
-        visit root_path
+    click_button "Log in"
 
-        click_link "I already have an account"
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Welcome, pat@xyz.com!")
+    expect(page).to_not have_link("I already have an account")
+    expect(page).to_not have_link("Register as a User")
+    expect(page).to have_link("Log out")
 
-        expect(current_path).to eq(login_path)
-        
-        fill_in :username, with: "meg@ex.com"
-        fill_in :password, with: "ilovecats"
-        
-        click_button "Log in"
-        
-        expect(current_path).to eq(login_path)
-        expect(page).to have_content("Your username or password are incorrect")
+    click_link("Log out")
 
-    end 
-end 
+    expect(current_path).to eq(root_path)
+    expect(page).to_not have_link("Log out")
+    expect(page).to have_link("I already have an account")
+    expect(page).to have_link("Register as a User")
+  end
+
+  it 'as a registered user, I cant log in with incorrect credentials' do
+    user = User.create(email: 'pat@xyz.com', password: 'viewparty')
+
+    visit root_path
+
+    click_link "I already have an account"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in :email, with: "pat@xyz.com"
+    fill_in :password, with: "ilovecats"
+
+    click_button "Log in"
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Your email or password are incorrect")
+  end
+end
