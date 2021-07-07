2f584ddd9514c24dba04c89c0e0c6779e98b360b
kevn7n <kevn_nguyen@yahoo.com>
2021-07-07 02:04:04 -0600

    Update login_spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "sessions#new"
-  get '/login', to:'sessions#new'
+  root "sessions#welcome"
+  get '/login', to:'sessions#welcome'
   post '/login', to:'sessions#create'
   # get '/logout', to: 'sessions#logout_user'
   delete '/logout', to: 'sessions#destroy'
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,42 +2,39 @@ require 'rails_helper'
 
 describe 'logging in' do
   it 'as a registered user, I can log in with correct credentials' do
-    user = User.create(email: 'pat@xyz.com', password: 'viewparty')
+    user = User.create(email: 'test123@xyz.com', password: 'viewparty')
 
-    visit root_path
-    save_and_open_page
+    visit login_path
+    # save_and_open_page
     expect(current_path).to eq("/login")
+    expect(find('form')).to have_content('Email')
+    expect(find('form')).to have_content('Password')
 
-    fill_in :email, with: "pat@xyz.com"
+
+    fill_in :email, with: "test123@xyz.com"
     fill_in :password, with: "viewparty"
 
     click_button "Log in"
 
-    expect(current_path).to eq(root_path)
-    expect(page).to have_content("Welcome, pat@xyz.com!")
-    expect(page).to_not have_link("I already have an account")
-    expect(page).to_not have_link("Register as a User")
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Welcome, test123@xyz.com!")
+    expect(page).to_not have_link("New to Viewing Party? Register Here")
     expect(page).to have_link("Log out")
 
     click_link("Log out")
 
-    expect(current_path).to eq(root_path)
+    expect(current_path).to eq(login_path)
     expect(page).to_not have_link("Log out")
-    expect(page).to have_link("I already have an account")
-    expect(page).to have_link("Register as a User")
+    expect(page).to have_link("New to Viewing Party? Register Here")
   end
 
-  it 'as a registered user, I cant log in with incorrect credentials' do
-    user = User.create(email: 'pat@xyz.com', password: 'viewparty')
+  it 'as a registered user, I can not log in with incorrect credentials' do
+    user = User.create(email: 'test123@xyz.com', password: 'viewparty')
 
-    visit root_path
+    visit login_path
 
-    click_link "I already have an account"
-
-    expect(current_path).to eq(login_path)
-
-    fill_in :email, with: "pat@xyz.com"
-    fill_in :password, with: "ilovecats"
+    fill_in :email, with: "test123@xyz.com"
+    fill_in :password, with: "noparty"
 
     click_button "Log in"
 
