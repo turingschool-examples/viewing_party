b9d9d0ac43bc457d76cb1594f8e9d4a82a0eea8d
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-08 08:30:19 -0600

    Create validation tests

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -8,26 +8,24 @@ RSpec.describe "Registration Page" do
       click_link 'Link to Registration'
 
       email = 'andrew@smith.com'
-      password = 'smith'
-      confirm_password = 'smith'
+      password = 'andrewsmith'
+      confirm_password = 'andrewsmith'
 
       fill_in 'user[email]', with: email
       fill_in 'user[password]', with: password
       fill_in 'user[password_confirmation]', with: confirm_password
-
+ 
       click_on 'Register'
 
       expect(page).to have_content("Welcome, #{email}!")
     end
 
-    it 'sad path' do
-      visit root_path
-
-      click_link 'Link to Registration'
+    it 'password sad path' do
+      visit '/register'
 
       email = 'andrew@smith.com'
-      password = 'smith'
-      confirm_password = 'smiths'
+      password = 'andrewsmith'
+      confirm_password = 'andrewsmiths'
 
       fill_in 'user[email]', with: email
       fill_in 'user[password]', with: password
@@ -35,7 +33,23 @@ RSpec.describe "Registration Page" do
 
       click_on 'Register'
 
-      expect(page).to have_content("Password confirmation doesn't match Password for #{email}! Please try again!")
+      expect(page).to have_content("Password confirmation doesn't match Password! Please try again!")
+    end
+
+    it 'email sad path' do
+      visit '/register'
+
+      email = 'andrew#smith.com'
+      password = 'andrewsmith'
+      confirm_password = 'andrewsmith'
+
+      fill_in 'user[email]', with: email
+      fill_in 'user[password]', with: password
+      fill_in 'user[password_confirmation]', with: confirm_password
+
+      click_on 'Register'
+
+      expect(page).to have_content('Email is invalid! Please try again!')
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validations" do
+    it {should validate_uniqueness_of :email}
+    it {should validate_presence_of :email}
+    it {should validate_presence_of :password}
+    it {should validate_length_of(:password).is_at_least(8).is_at_most(30)}
+    it {should have_secure_password}
   end
 
   describe "relationships" do
