6b6605528d6ac6d10b54cc25669b6a6092b640a8
Suzie K <suziekim.dev@gmail.com>
2021-07-08 10:41:11 -0400

    Update describe block syntax

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -9,10 +9,21 @@ RSpec.describe MovieService do
     expect(@m).to be_a(MovieService)
   end
 
-  it 'can return list of top rated movies' do
-    response = @m.top_rated 
-    
-    expect(response).to have_key("results")
-    expect(response["results"]).to be_an(Array)
+  describe 'instance methods' do 
+    describe '#top_rated' do 
+      it 'can return list of top rated movies' do
+        response = @m.top_rated 
+        
+        expect(response).to have_key(:results)
+        expect(response).to have_key(:page)
+        expect(response[:results]).to be_an(Array)
+        expect(response[:results].first[:title]).to eq("Dilwale Dulhania Le Jayenge")
+        expect(response[:results].last[:title]).to eq("Cruella")
+      end
+    end
+
+    describe '#find_by_title' do
+      it 'can find movie '
+    end
   end
 end
