f22da1d0e05c287f63b47467642d573f1e55b16f
Suzie K <suziekim.dev@gmail.com>
2021-07-08 10:40:59 -0400

    Green test top_rated

---
+++ app/services/movie_service.rb
@@ -0,0 +1,26 @@
+require 'faraday'
+require 'pry'
+require 'json'
+
+class MovieService
+
+  def top_rated
+    request_api("/3/movie/top_rated")
+  end
+  
+  def request_api(path)
+    resp = conn('https://api.themoviedb.org/').get(path) do |faraday|
+      faraday.params['api_key'] = ENV["themoviedb_key"]
+    end
+    parse_json(resp)
+  end
+  
+  private 
+    def parse_json(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def conn(url)
+      Faraday.new(url)
+    end
+end
