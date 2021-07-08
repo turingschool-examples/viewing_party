f0a10bcc2825e2e483dacc4dca0a3b1b4c159209
Suzie K <suziekim.dev@gmail.com>
2021-07-08 10:49:41 -0400

    Green test #find_by_title

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -7,6 +7,10 @@ class MovieService
   def top_rated
     request_api("/3/movie/top_rated")
   end
+
+  def find_by_title(title)
+    request_api("3/search/movie?query=#{title}")
+  end
   
   def request_api(path)
     resp = conn('https://api.themoviedb.org/').get(path) do |faraday|
