07fee504986ae287085ca96a28b78dc895c63c88
JoannaCoPo <jococopo@gmail.com>
2021-07-08 14:40:53 -0600

    Change redirect route to root path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     found_user = User.find_by(email: params[:email])
     if found_user && found_user.authenticate(params[:password])
       session[:user_id] = found_user.id
       flash[:success] = "Welcome back, #{found_user.email}!"
-      redirect_to dashboard_path
+      redirect_to root_path
     else
       flash[:error] = "Your username or password are incorrect. Please try again."
       redirect_to login_path
