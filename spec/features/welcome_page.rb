c1939d6fe6d9b96cab4b184515be88b6d6c1f437
JoannaCoPo <jococopo@gmail.com>
2021-07-07 21:47:36 -0600

    Clean up test file

--- spec/features/welcome_page.rb
+++ spec/features/welcome_page.rb
@@ -14,47 +14,21 @@ RSpec.describe "Welcome Page" do
       expect(page).to have_content(description)
     end
 
-    it 'it displays a log in form with button for registered users' do
+    it 'it displays login but that redirects to login page' do
       user = User.create!(email: 'empanada_luvr@email.com', password: 'hocuspocus')
-      within("registered-users") do
-        expect(page).to have_content("I already have an account, log me in!")
-        expect(page).to have_button('Log In')
 
-        fill_in :email, with: user.email
-        fill_in :password, with: user.password
+      expect(page).to have_content("I already have an account.")
+      expect(page).to have_button('Log me in!')
 
-        click_button('Log In')
-        expect(current_path).to eq(dashboard_path)
-        #double check which route we want
-      end
+      click_button('Log me in!')
+      expect(current_path).to eq('/login')
     end
 
-# save_and_open_page
-    xit 'it displays a registration link with desctiption for new users' do
-      within("message") do
+    it 'it displays a registration link with desctiption for new users' do
+
         expect(page).to have_content("I want this service, sign me up!")
         click_button('Register')
-        expect(current_path).to eq(register_path)
+        expect(current_path).to eq(users_path)
       end
     end
   end
-end
-
-# When a user visits the root path they should be on the welcome page which includes:
-#
-#  Welcome message
-#  Brief description of the application
-#  Button to Log in
-#  Link to Registration
-# Details: Implement basic auth in the application allowing a user to log in with an email and password.
-# The password should be stored in the database using bcrypt.
-
-
-# <% if current_user %>
-#     <%= button_to "Log out", '/logout', method: :delete %>
-# <% else %>
-#     <p>I want this service, sign me up!</p>
-#     <%= button_to "Register", "/register" %>
-#     <p>I already have an account, log me in!</p>
-#     <%= button_to "Login", "/login" %>
-# <% end %>
