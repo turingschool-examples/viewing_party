5bed779f1f79728d184cbba303b52cda639660d3
Suzie K <suziekim.dev@gmail.com>
2021-07-08 21:24:43 -0400

    Remove pry

--- spec/poros/movie_details_spec.rb
+++ spec/poros/movie_details_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe MovieDetails do
       cast = service.find_cast(337404)
       reviews = service.find_reviews(337404)
       cruella = MovieDetails.new(movie, cast, reviews)
-      require 'pry'; binding.pry
+   
       expect(cruella).to be_a MovieDetails
     end
   end
