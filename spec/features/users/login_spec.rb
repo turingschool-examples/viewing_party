42ad682da4a20ed7e2f4dc6584f8378747dc484b
JoannaCoPo <jococopo@gmail.com>
2021-07-08 21:50:19 -0600

    Test correct login credentials

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -8,17 +8,17 @@ RSpec.describe "Welcome Page" do
     end
 
     it 'it allows registered users to log in with correct credentials' do
-      click_button 'Log me in!'
+      click_link 'Log me in!'
       fill_in :email, with: 'empanada_luvr@email.com'
       fill_in :password, with: 'hocuspocus'
-      click_button('Log me in!')
+      click_button('Log In')
 
       expect(current_path).to eq(root_path)
-      expect(page).to have_content('Welcome back,empanada_luvr@email.com')
+      expect(page).to have_content('Welcome back, empanada_luvr@email.com')
       expect(page).to_not have_content("I already have an account")
       expect(page).to_not have_link("Log me in!")
       expect(page).to_not have_link("Register")
-      expect(page).to have_link("Log out")
+      expect(page).to have_button("Log out")
     end
   end
 end
