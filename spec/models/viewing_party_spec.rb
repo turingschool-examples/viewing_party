199201dd86e79a165295723ce1bb0eb8ae04f254
JoannaCoPo <jococopo@gmail.com>
2021-07-10 13:14:09 -0600

    Create model test for ViewingParty

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ViewingParty, type: :model do
   describe 'relationships' do
-    it { should belong_to(:party_host) }
+    it { should belong_to(:party_host)}
+    it { should have_many(:attendees)}
   end
 end
