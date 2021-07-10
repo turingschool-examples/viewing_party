50cb05f1fd0dc736f4fc10aee0f92f632e6b9aa8
Suzie K <suziekim.dev@gmail.com>
2021-07-10 12:10:24 -0400

    Add webmock and vcr gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,12 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
