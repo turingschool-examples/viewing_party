6739b20c470538b2628e551bea3ef81b5cf5e14d
Suzie K <suziekim.dev@gmail.com>
2021-07-11 19:05:16 -0400

    Green test should have top 40 results

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -8,16 +8,17 @@ RSpec.describe "Movies index page" do
     fill_in :password, with: "viewparty"
     click_button "Sign In"
     visit movies_path
+
   end
   
   it 'should list the top 40 results from my search' do
-    # response_body = File.read('spec/fixtures/top_rated.json')
-    save_and_open_page
-    # stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated").
-    #     to_return(status: 200, body: response_body, headers: {})
+    response_body = File.read('spec/fixtures/top_rated.json')
 
+    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated").
+        to_return(status: 200, body: response_body, headers: {})
+    
     expect(page).to have_content("Dilwale Dulhania Le Jayenge")
-    expect(page).to have_content("City of God")
+    expect(page).to have_content("The Shawshank Redemption")
     expect(current_path).to eq(movies_path)
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ VCR.configure do |c|
   c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.allow_http_connections_when_no_cassette = true
 end
 # Add additional requires below this line. Rails is not loaded until this point!
 
