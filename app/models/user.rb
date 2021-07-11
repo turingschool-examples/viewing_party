0261082e658f9ae21efd50c52e3f9ab6e1d21e6e
JoannaCoPo <jococopo@gmail.com>
2021-07-11 09:34:19 -0600

    Active record for user invites

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,11 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
-  has_many :user_friendships, dependent: :destroy
-  has_many :friends, through: :user_friendships
 
   has_secure_password
+
+  def invites
+    Attendee.where(user_id: self.id)
+            .map(&:party)
+  end
 end
