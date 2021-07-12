527045592ddd3b6a594c150fa8bcb9ec449269d8
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-07-12 07:55:23 -0600

    updated testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
-describe User, type: :model do
+RSpec.describe User, type: :model do
+  describe 'relationships' do
+    it { should have_many(:friendships)}
+    it { should have_many(:friends).through(:friendships)}
+  end
+
   describe "validations" do
     it {should validate_presence_of(:username)}
     it {should validate_uniqueness_of(:username)}
