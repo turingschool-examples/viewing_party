92e445de712e282ea06912e7e9b2bb4d79bef552
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-07-12 08:40:24 -0600

    update user show spec

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,48 +1,82 @@
 require 'rails_helper'
 
 RSpec.describe 'dashboard' do
-  describe 'dashboard' do
-    it 'as a registered user, I can log in with correct credentials' do
-      user = User.create(username: 'person@ex.com', password: 'ilovecatsanddogs')
+
+  before :each do
+      @user = User.create(username: 'person@ex.com', password: 'ilovecatsanddogs')
+      @user_1 = User.create(username: 'person@ex1.com', password: 'ilovecatsanddogs2')
+      @user_2 = User.create(username: 'person@ex2.com', password: 'ilovecatsanddogs3')
 
       visit root_path
 
       click_on "Log in here"
 
       expect(current_path).to eq("/login")
+  end
+  describe 'dashboard' do
+    it 'has a welcome with username' do
 
       fill_in :username, with: "person@ex.com"
       fill_in :password, with: "ilovecatsanddogs"
 
       click_on "Log in"
 
+      expect(current_path).to eq("/dashboard")
       expect(page).to have_content("Welcome, person@ex.com!")
-      expect(page).to have_content("Friend List")
       expect(page).to have_button("Discover Movies")
     end
 
-    it 'can add friends' do
-      user = User.create(username: 'person@ex.com', password: 'ilovecatsanddogs')
-      user_1 = User.create(username: 'person@ex1.com', password: 'ilovecatsanddogs2')
-      user_2 = User.create(username: 'person@ex2.com', password: 'ilovecatsanddogs3')
+    it 'has a friend list with no friends' do
+      fill_in :username, with: "person@ex.com"
+      fill_in :password, with: "ilovecatsanddogs"
 
-      visit root_path
+      click_on "Log in"
+      expect(page).to have_content("Friend List")
+      expect(page).to have_content("You currently have no friends.")
+    end
 
-      click_on "Log in here"
-
-      expect(current_path).to eq("/login")
+    it 'has a friend list with friends' do
+      Friendship.create(user: @user, friend: @user_1)
 
       fill_in :username, with: "person@ex.com"
       fill_in :password, with: "ilovecatsanddogs"
 
       click_on "Log in"
-      fill_in :search_by_username, with: 'person@ex1.com'
+      expect(page).to have_content("Friend List")
+      within("#friends-list") do
+        expect(page).to have_content(@user_1.username)
+      end
+    end
+
+    it 'can add friends' do
+      fill_in :username, with: "person@ex.com"
+      fill_in :password, with: "ilovecatsanddogs"
+
+      click_on "Log in"
+
+      fill_in :search_by_username, with: 'person@ex2.com'
       click_on "Search"
-      click_on "Add #{user_1.username}"
+      click_on "Add #{@user_2.username}"
 
       within("#friends-list") do
-        expect(page).to have_content(user_1.username)
+        expect(page).to have_content(@user_2.username)
       end
+    end
+
+    it 'has a discover movies button' do
+      fill_in :username, with: "person@ex.com"
+      fill_in :password, with: "ilovecatsanddogs"
+
+      click_on "Log in"
+      expect(page).to have_button("Discover Movies")
+    end
+
+    it 'has a viewing parties section' do
+      fill_in :username, with: "person@ex.com"
+      fill_in :password, with: "ilovecatsanddogs"
+
+      click_on "Log in"
+      expect(page).to have_content("Viewing Parties")
     end
   end
 end
