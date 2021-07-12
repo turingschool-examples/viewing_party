a8338e902d2cda9fa80a96521c6b1cd23fcaa494
Suzie K <suziekim.dev@gmail.com>
2021-07-11 20:04:29 -0400

    Green tests for Movies index page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,10 @@
 class MoviesController < ApplicationController
   def index
     movie_service = MovieService.new
-    if params[:q].nil? 
+    if params[:query].nil? 
       @movies = movie_service.top_rated[:results]
-    elsif params[:q].present? 
-      @movies = movie_service.find_by_title(params[:q])[:results]
+    elsif params[:query].present? 
+      @movies = movie_service.find_by_title(params[:query])[:results]
     else
       @movies = []
     end
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -8,21 +8,52 @@ RSpec.describe "Movies index page" do
     fill_in :password, with: "viewparty"
     click_button "Sign In"
     visit movies_path
-
   end
+
+  describe 'Movies Index Page' do 
+    it 'has a button to find top rated movies' do
+      
+      expect(page).to have_button('Find Top Rated Movies')
+    end
+
+    it 'has form to search for movies by title' do
   
-  it 'should list the top 40 results from my search' do
-    response_body = File.read('spec/fixtures/top_rated.json')
+      expect(page).to have_field('query')
+      expect(page).to have_button('Find Top Rated Movies')
+    end
 
-    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated").
-        to_return(status: 200, body: response_body, headers: {})
-    
-    expect(page).to have_content("Dilwale Dulhania Le Jayenge")
-    expect(page).to have_content("The Shawshank Redemption")
-    expect(current_path).to eq(movies_path)
+    context "if user clicks 'Find Top Rated Movies' button" do 
+      it 'should return list of top rated movies and its vote average' do
+        click_button 'Find Top Rated Movies'
+        
+        response_body = File.read('spec/fixtures/top_rated.json')
+        stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated").
+            to_return(status: 200, body: response_body, headers: {})
+      
+        expect(page).to have_content("Dilwale Dulhania Le Jayenge")
+        expect(page).to have_content("8.7")
+        expect(page).to have_content("The Shawshank Redemption")
+        expect(page).to have_content("8.5")
+        expect(current_path).to eq(movies_path)
+      end
+    end
+
+    context 'if user searches for movie title with search bar' do
+      it 'should list movies from search results by title and vote average' do 
+        fill_in("query", with: "Cruella")
+        click_button("Find Movies")
+
+        response_body = File.read('spec/fixtures/find_movie_by_title.json')
+        stub_request(:get, "https://api.themoviedb.org/3/search/movie?query=Cruella").
+            to_return(status: 200, body: response_body, headers: {})
+
+            expect(page).to_not have_content("Dilwale Dulhania Le Jayenge")
+            expect(page).to_not have_content("The Shawshank Redemption")
+            expect(page).to have_content("Cruella")
+            expect(page).to have_content("Cruella 2")
+            expect(page).to have_content("A Christmas Cruella")
+            expect(page).to have_content("Mega Mindy - Della Cruella")
+      end
+    end
   end
-
-  it 'should list movies from search results by title and vote average' 
-
-  it "should have a Find Top Rate Movies button and form" 
 end
