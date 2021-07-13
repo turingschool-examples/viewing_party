3232cb7e4b5993fe962cfdb85359d894f88a768d
JoannaCoPo <jococopo@gmail.com>
2021-07-13 11:01:07 -0600

    Set up Faraday connection and JSON parsing

--- app/services/API_Services.rb
+++ app/services/API_Services.rb
@@ -1,3 +1,19 @@
-class APIServices
+class APIServices << self
+  #movie search
+  def get_search_movie(movie_search)
+    response = conn.get("3/search/movie?query=#{movie_search}")
+    parse_json(response)
+  end
 
+  private
+
+  def conn
+    Faraday.new(url: 'https://api.themoviedb.org/3') do |faraday|
+      faraday.params["X-API-KEY"] = ENV['THE_MOVIES_DB_API_KEY']
+    end
+  end
+
+  def parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
