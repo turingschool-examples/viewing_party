757b9022b3abc5504a02945ca93cee7b31849892
JoannaCoPo <jococopo@gmail.com>
2021-07-13 13:40:55 -0600

    Prepare stubs for api testing

--- spec/services/API_services_spec.rb
+++ spec/services/API_services_spec.rb
@@ -1,18 +1,25 @@
 require 'rails_helper'
 
 describe APIServices do
-  describe "class methods" do
-    describe "#get_search_movie" do
-      it 'returns primary information about a movie' do
-        response_body = File.read('spec/fixtures/shawshank_search.json')
+  describe "#get_search_movie" do
+    it 'can search movie' do
+      search_movie = "Shawshank"
+      # response_body = File.open("#{Rails.root}/spec/fixtures/shawshank_search.json")
+      response_body = File.read('spec/fixtures/shawshank_search.json')
+      # make_request(:get, "3/search/movie?api-key=#{ENV['MOVIES_DB_API_KEY']}&query=shawshank&page=1", response_body)
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api-key&query=shawshank").
+         with(
+           headers: {
 
-        stub_request(:get, "https://api.themoviedb.org/3/movie/278?api_key=0ac03930ac7ad73e4782acd4540af5a7&language=en-US")
-        # to_return(status:200, body: response_body, headeres: {})
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v1.5.0'
+           }).
+         to_return(status: 200, body: "", headers: {})
 
-        response = APIServices.get_search_movie
+      response_1 = APIServices.get_search_movie(search_movie)
 
-        expect(response).to be_a Hash
-      end
+      expect(response_1).to be_a(Array)
     end
   end
 end
