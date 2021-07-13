bf228547050af665b3d704875535025c93e987ab
kevn7n <kevn_nguyen@yahoo.com>
2021-07-13 00:24:01 -0600

    Add friend section test in dashboard/show spec test

--- app/controllers/authenticated/dashboard_controller.rb
+++ app/controllers/authenticated/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Authenticated::DashboardController < Authenticated::BaseController
   def show
     @user = current_user
+    # @friend_list
   end
   
 end
--- spec/features/authenticated/dashboard_spec.rb
+++ spec/features/authenticated/dashboard_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'dashboard page' do
 
   describe 'dashboard page with login' do
     before(:each)do
-      @user = User.create(email: 'test123@xyz.com', password: 'viewparty')
+      @user_1 = User.create(email: 'test123@xyz.com', password: 'viewparty')
       visit welcome_path
       fill_in :email, with: "test123@xyz.com"
       fill_in :password, with: "viewparty"
@@ -17,12 +17,38 @@ RSpec.describe 'dashboard page' do
       visit dashboard_path
     end
 
-    it 'has button to Discover Movies, headers' do
+    it 'has button to Discover Movies page' do
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content("Welcome test123!")
       expect(page).to have_button("Discover Movies")
+      click_button "Discover Movies"
+      expect(current_path).to eq(discover_path)
+    end
+
+    it 'has Friends section with Add Field button' do
       expect(page).to have_content("Friends")
-      expect(page).to have_content("Viewing Parties")
+      expect(page).to have_field('friend')
+      expect(page).to have_button('Add Friend')
+    end
+
+    it 'shows no friends error message' do
+      expect(page).to have_content("Friends")
+      expect(page).to have_field('friend')
+      expect(page).to have_button('Add Friend')
+      click_button "Add Friend"
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("No Friends Currently")
+    end
+
+    it 'shows friends in Friends section' do
+      @user_2 = User.create(email: 'lola_rabbit@aol.com', password: 'lola')
+      @user_3 = User.create(email: 'bugs_bunny@gmail.com', password: 'bugs')
+      @user_4 = User.create(email: 'daffy_duck@yahoo.com', password: 'daffy')
+
+      fill_in :email, with: "lola_rabbit@aol.com"
+      click_button "Add Friend"
+      expect(current_path).to eq(dashboard_path)
+      expect(current_path).to have_content(l"lola_rabbit@aol.com")
     end
 
     it 'has Logout link to Welcome Page' do
