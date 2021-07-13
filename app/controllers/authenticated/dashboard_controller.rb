e70de4ee264067f8d126b89d628e62a9aba90264
kevn7n <kevn_nguyen@yahoo.com>
2021-07-13 00:46:10 -0600

    Start friend list in dashboard

--- app/controllers/authenticated/dashboard_controller.rb
+++ app/controllers/authenticated/dashboard_controller.rb
@@ -1,7 +1,11 @@
 class Authenticated::DashboardController < Authenticated::BaseController
   def show
     @user = current_user
-    # @friend_list
+    @friend_list = []
+    if params[:email].present?
+      params[:email] >> @friend_list
+    end
+    render :show 
   end
-  
+
 end
