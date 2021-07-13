e9c39365eb1e690566ae25b111b15f13239bb311
Suzie K <suziekim.dev@gmail.com>
2021-07-12 21:11:53 -0400

    Remove commented lines

--- spec/features/authenticated/movies/show_spec.rb
+++ spec/features/authenticated/movies/show_spec.rb
@@ -1,21 +1,3 @@
-# As an authenticated user,
-# When I visit the movie's detail page,
-# I should see
-
-#  Button to create a viewing party
-# Details This button should take the authenticated user to the new event page
-
-# And I should see the following information about the movie:
-
-#  Movie Title
-#  Vote Average of the movie
-#  Runtime in hours & minutes
-#  Genere(s) associated to movie
-#  Summary description
-#  List the first 10 cast members (characters&actress/actors)
-#  Count of total reviews
-#  Each review's author and information
-
 require 'rails_helper'
 
 RSpec.describe "Movies show page" do
