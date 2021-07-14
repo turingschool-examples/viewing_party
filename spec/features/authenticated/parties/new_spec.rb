caddb8050725846bdf8aaf74986aa8b5c856e2bb
Suzie K <suziekim.dev@gmail.com>
2021-07-13 21:43:28 -0400

    Red test add friends to party form

--- spec/features/authenticated/parties/new_spec.rb
+++ spec/features/authenticated/parties/new_spec.rb
@@ -19,6 +19,7 @@ RSpec.describe "Parties New form" do
       fill_in 'duration', with: 200
       click_on "Create Party"
       
+      expect(page).to_not have_content("Invite:")
       expect(page).to have_content("You've created a Viewing Party!")
       expect(current_path).to eq(dashboard_path)
     end
@@ -41,6 +42,37 @@ RSpec.describe "Parties New form" do
       
       expect(page).to have_content("You must enter a valid start time, date and duration.")
       expect(current_path).to eq(parties_new_path)
+    end
+  end
+
+  context "if user has friends" do
+    before(:each) do
+      @friend_1 = create(:mock_user)
+      @friend_2 = create(:mock_user)
+      @friend_3 = create(:mock_user)
+      @user.friends.push(@friend_1, @friend_2, @friend_3)
+      visit "/movies/337404"
+      click_on "Create Viewing Party"
+    end
+
+    it 'shows friends you can invite on the form' do
+     
+      expect(page).to have_content("Invite:")
+      expect(page).to have_content(@friend_1.email)
+      expect(page).to have_content(@friend_2.email)
+      expect(page).to have_content(@friend_3.email)
+    end
+
+    it "creates party with invited friends" do
+      
+      fill_in 'date', with: '12/12/21'
+      fill_in 'start_time', with: '5:00 PM'
+      fill_in 'duration', with: 200
+      check "friend[#{@friend_2.id}]"
+      click_on "Create Party"
+
+      expect(current_path).to eq(dashboard_path)
+      # expect(page).to have_content("Cruella")
     end
   end
 end
