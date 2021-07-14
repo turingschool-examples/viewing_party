f7bc154347bd4dfd6e124cc44833a3cc83459d5e
Caleb Wittman <calebjwittman1991@gmail.com>
2021-07-14 07:02:11 -0600

    Add some more functionality to new viewing party, currently incomplete

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -1,14 +1,20 @@
 class PartyController < ApplicationController
   def new
-    binding.pry
     @user = User.find(session[:user_id])
     @movie = Movie.new(params[:movie_id])
   end
 
   def create
-    @user = user.find(session[:user_id])
+    @user = User.find(session[:user_id])
     @movie = Movie.new(params[:movie_id])
-    #runtime
-    #movie name
+    date = "#{params['date(2i)']} #{params['date(3i)']}, #{params['date(1i)']}"
+    binding.pry
+    if !@user.nil? && params[:duration].to_i >= @movie.runtime_in_minutes
+      Party.create!(party_host_id: @user.id, movie_title: params[:title], movie_id: params[:movie_id], date: date, duration: params[:duration], start_time: params[:start_time])
+      redirect_to '/dashboard'
+    else
+      flash[:error] = 'One or more of your fields is incorrect'
+      render :new
+    end
   end
 end
