23301ca1453078db57fa1d8f6e42fe12e2b54ba7
JoannaCoPo <jococopo@gmail.com>
2021-07-13 21:41:01 -0600

    Fix syntax issue

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -1,18 +1,18 @@
 class MoviesFacade
-  # def self.find_movie_title(title)
-  # end
-
   def self.search_by_movie_title(movie_title)
     json = APIServices.movie_title_query(movie_title)
-    json[:results.map do |result_data]
+
+    json[:results].map do |result_data|
       Film.new(result_data)
     end.first(40)
   end
 
-  # def self.create_movie_objects(movie_data_via_api)
-  #   movie_data_via_api.map do |movie_data|
-  #     Film.new(movie_data) #POROS
-  # end
+  def self.top_rated
+    json = APIServices.top_rated_query
+    json.map do |result_data|
+      Film.new(result_data)
+    end
+  end
 end
 
 #from services > facade > poros > controller
