fc53e0756cbc1c105e0bd8dee817d0a13fb3a354
Suzie K <suziekim.dev@gmail.com>
2021-07-13 21:19:11 -0400

    Red sad path tests

--- spec/features/authenticated/parties/new_spec.rb
+++ spec/features/authenticated/parties/new_spec.rb
@@ -16,9 +16,31 @@ RSpec.describe "Parties New form" do
     
       fill_in 'date', with: '12/12/21'
       fill_in 'start_time', with: '5:00 PM'
+      fill_in 'duration', with: 200
+      click_on "Create Party"
+      
+      expect(page).to have_content("You've created a Viewing Party!")
+      expect(current_path).to eq(dashboard_path)
+    end
+
+    it "can't create party with a duration less than the movie runtime" do 
+    
+      fill_in 'date', with: '12/12/21'
+      fill_in 'start_time', with: '5:00 PM'
+      fill_in 'duration', with: 55
       click_on "Create Party"
 
-      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Party duration cannot be less than movie run time.")
+      expect(current_path).to eq(parties_new_path)
+    end
+    
+    it "can't create a party with empty fields" do
+      
+      fill_in 'duration', with: 190
+      click_on "Create Party"
+      
+      expect(page).to have_content("You must enter a valid start time, date and duration.")
+      expect(current_path).to eq(parties_new_path)
     end
   end
 end
