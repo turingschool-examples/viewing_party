2cf8c01d5c6c862a68b369576ebdb148120b9ffa
JoannaCoPo <jococopo@gmail.com>
2021-07-13 21:29:20 -0600

    Fix instance variable

--- app/poros/film.rb
+++ app/poros/film.rb
@@ -1,13 +1,13 @@
 class Film
   attr_reader :id,
               :title,
-              :vote_average,
+              :rating,
               :popularity,
               :overview
   def initialize(result_data)
     @id = result_data[:id]
     @title = result_data[:title] #check to see if we'll need to capitalize, etc.
-    @vote_average = result_data[:vote_average]
+    @rating = result_data[:vote_average]
     @popularity = result_data[:popularity]
     @overview = result_data[:overview]
   end
