40dcd3d0e01004a52d523db96f29a04fc84754e9
JoannaCoPo <jococopo@gmail.com>
2021-07-13 21:42:07 -0600

    Fix order syntax in top_rated

--- app/services/API_Services.rb
+++ app/services/API_Services.rb
@@ -1,28 +1,17 @@
 class APIServices
+  def self.top_rated_query
+    response_pg_1 = conn.get("3/movie/top_rated?api_key=#{ENV['MOVIES_DB_API_KEY']}&page=1")
+    response_pg_2 = conn.get("3/movie/top_rated?api_key=#{ENV['MOVIES_DB_API_KEY']}&page=2")
 
-  # def get_top_rated_movies
-  #   response_pg_1 = conn.get("3/movie/top_rated")
-  #   parse_json(response)
-  #
-  #   response_pg_2 = conn.get("3/movie/top_rated&page=2")
-  #   parse_json(response)
-  # end
-  #
+    pg_1_parsed = parse_json(response_pg_1)
+    pg_2_parsed = parse_json(response_pg_2)
+
+    pg_1_parsed[:results] + pg_2_parsed[:results]
+  end
 
   def self.movie_title_query(movie_title)
-    response = conn.get("3/search/movie?query=#{search_movie}")
+    response = conn.get("3/search/movie?query=#{movie_title}")
     parse_json(response)
-  end
-
-
-  def self.conn
-    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
-      faraday.params['api_key'] = ENV['MOVIES_DB_API_KEY']
-    end
-  end
-
-  def self.parse_json(response)
-    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.title_vote_summary_genres_runtime(movie_id)
@@ -38,5 +27,17 @@ class APIServices
   def self.reviewer_data(movie_id)
     response = Faraday.get("https://api.themoviedb.org/3/movie/#{movie_id}/reviews?api_key=" + ENV['api_key'])
     json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.conn
+    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.params['api_key'] = ENV['MOVIES_DB_API_KEY']
+    end
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
