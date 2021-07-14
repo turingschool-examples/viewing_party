4951f84477a843b832f2a83f3a0394df85cfd61a
kevn7n <kevn_nguyen@yahoo.com>
2021-07-14 17:18:49 -0600

    Fix preset friends test

--- spec/features/authenticated/dashboard_spec.rb
+++ spec/features/authenticated/dashboard_spec.rb
@@ -50,18 +50,22 @@ RSpec.describe 'dashboard page' do
       expect(page).to_not have_content("lola_rabbit@aol.com")
     end
 
-    # it 'shows Sad Path, friend not added' do
-    #   @user_2 = User.create(email: 'lola_rabbit@aol.com', password: 'lola')
-    #
-    #   expect(current_path).to eq(dashboard_path)
-    #   expect(page).to have_field('email')
-    #   expect(page).to have_button('Add Friend')
-    #   fill_in :email, with: "lola_rabbit@aol.com"
-    #   click_button "Add Friend"
-    #   expect(current_path).to eq(dashboard_path)
-    #   expect(page).to have_content("Unable to Add Friend")
-    #   expect(page).to_not have_content("lola_rabbit@aol.com")
-    # end
+    it 'shows Sad Path, friend not added' do
+      @user_2 = User.create(email: 'lola_rabbit@aol.com', password: 'lola')
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_field('email')
+      expect(page).to have_button('Add Friend')
+      fill_in :email, with: "lola_rabbit@aol.com"
+      click_button "Add Friend"
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Unable to Add Friend")
+      expect(page).to have_content("lola_rabbit@aol.com")
+      fill_in :email, with: "lola_rabbit@aol.com"
+      click_button "Add Friend"
+      expect(current_path).to eq(dashboard_path)
+
+    end
 
     it 'adds user to and shows in Friends list' do
       @user_2 = User.create(email: 'lola_rabbit@aol.com', password: 'lola')
@@ -76,25 +80,30 @@ RSpec.describe 'dashboard page' do
       expect(page).to have_content("lola_rabbit@aol.com")
     end
 
-    # it 'shows friends in Friends section' do
-    #   @user_2 = User.create!(email: 'lola_rabbit@aol.com', password: 'lola')
-    #   @user_3 = User.create!(email: 'bugs_bunny@gmail.com', password: 'bugs')
-    #   @user_4 = User.create!(email: 'daffy_duck@yahoo.com', password: 'daffy')
-    #   Friendship.create(user: @user_1, friend: @user_2)
-    #   Friendship.create(user: @user_1, friend: @user_3)
-    #
-    #   expect(current_path).to eq(dashboard_path)
-    #   # save_and_open_page
-    #   expect(page).to have_content("lola_rabbit@aol.com")
-    #   expect(page).to have_content("bugs_bunny@gmail.com")
-    #   expect(page).to_not have_content("daffy_duck@yahoo.com")
-    # end
+    it 'shows friends in Friends section' do
+      click_link("Log out")
+      User.destroy_all
+      @user_1 = User.create!(email: 'test123@xyz.com', password: 'viewparty')
+      @user_2 = User.create!(email: 'lola_rabbit@aol.com', password: 'lola')
+      @user_3 = User.create!(email: 'bugs_bunny@gmail.com', password: 'bugs')
+      @user_4 = User.create!(email: 'daffy_duck@yahoo.com', password: 'daffy')
+      @user_1.friends.push(@user_2, @user_3)
+
+      fill_in :email, with: "test123@xyz.com"
+      fill_in :password, with: "viewparty"
+      click_button "Sign In"
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("#{@user_2.email}")
+      expect(page).to have_content("lola_rabbit@aol.com")
+      expect(page).to have_content("bugs_bunny@gmail.com")
+      expect(page).to_not have_content("daffy_duck@yahoo.com")
+    end
 
     it 'has Logout link to Welcome Page' do
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_link("Log out")
       click_link("Log out")
-
       expect(current_path).to eq(welcome_path)
       expect(page).to_not have_link("Log out")
       expect(page).to have_content("Welcome to Viewing Party")
