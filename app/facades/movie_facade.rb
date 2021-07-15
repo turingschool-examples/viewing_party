077ba5d2ebc66705f67e07ce9a9383dd604cbb06
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-07-14 21:51:14 -0600

    move stuff from service to faccade, and adust tests some

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,17 +1,49 @@
 class MovieFacade
   def self.top_movie_results
-    json = ImdbService.top_movies
+    json = self.top_movies
     json.map do |movie|
       Movie.new(movie)
     end
   end
 
+  def self.top_movies(movie_numbers = 40)
+    results_array = []
+    page_number = 1
+    until results_array.length >= movie_numbers
+      response = ImdbService.top_movies_page(page_number)
+      results = response[:results]
+      results.each do |movie|
+        results_array << movie
+      end
+      page_number +=1
+    end
+    results_array.first(movie_numbers)
+  end
+
   def self.top_movie_search_results(search)
-    json = ImdbService.top_movies_search(search)
+    json = self.top_movies_search(search)
     return 'No Movies Found' if json == []
     json.map do |movie|
       Movie.new(movie)
     end
+  end
+
+  def self.top_movies_search(search = nil, movie_numbers = 40)
+    search_terms = search.gsub(' ', '+')
+    results_array = []
+    page_number = 1
+    until results_array.length >= movie_numbers
+      response = ImdbService.top_movies_search_page(search_terms, page_number)
+      results = response[:results]
+      if results == []
+        return []
+      end
+      results.each do |movie|
+        results_array << movie
+      end
+      page_number +=1
+    end
+    results_array.first(movie_numbers)
   end
 
   def self.movie_data_results(id)
--- app/services/imdb_service.rb
+++ app/services/imdb_service.rb
@@ -1,58 +1,15 @@
 class ImdbService
 
-  def self.top_movies(movie_numbers = 40)
-    results_array = []
-    page_number = 1
-    until results_array.length >= movie_numbers
-      response = top_movies_page(page_number)
-      results = response[:results]
-      results.each do |movie|
-        results_array << movie
-      end
-      page_number +=1
-    end
-    results_array.first(movie_numbers)
-  end
-
   def self.top_movies_page(page)
     response = con.get("/3/discover/movie?sort_by=popularity.desc&page=#{page}")
-    parsed_response = JSON.parse(response.body, symbolize_names: true)
-  end
-
-  # def self.top_movies_search(search = nil)
-  #   search_terms = search.gsub(' ', '+')
-  #   results_array = []
-  #   index = 1
-  #   until results_array.length >= 2
-  #     results_array.push(con.get("/3/search/movie?query=#{search_terms}&page=#{index}").body)
-  #     index += 1
-  #   end
-  #   results_array.flatten.map do |page_results|
-  #     JSON.parse(page_results, symbolize_names: true)[:results]
-  #   end.flatten
-  # end
-
-  def self.top_movies_search(search = nil, movie_numbers = 40)
-    search_terms = search.gsub(' ', '+')
-    results_array = []
-    page_number = 1
-    until results_array.length >= movie_numbers
-      response = top_movies_search_page(search_terms, page_number)
-      results = response[:results]
-      if results == []
-        return []
-      end
-      results.each do |movie|
-        results_array << movie
-      end
-      page_number +=1
-    end
-    results_array.first(movie_numbers)
+    body = response.body
+    JSON.parse(body, symbolize_names: true)
   end
 
   def self.top_movies_search_page(search, page)
     response = con.get("/3/search/movie?query=#{search}&page=#{page}")
-    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    body = response.body
+    JSON.parse(body, symbolize_names: true)
   end
 
   def self.movie_data(movie_id)
