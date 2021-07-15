a0c293116565b6a8c80be8fa7ca655cac71aa1e0
Suzie K <suziekim.dev@gmail.com>
2021-07-14 22:24:18 -0400

    Remove commented out tests

--- spec/features/authenticated/dashboard_spec.rb
+++ spec/features/authenticated/dashboard_spec.rb
@@ -50,19 +50,6 @@ RSpec.describe 'dashboard page' do
       expect(page).to_not have_content("lola_rabbit@aol.com")
     end
 
-    # it 'shows Sad Path, friend not added' do
-    #   @user_2 = User.create(email: 'lola_rabbit@aol.com', password: 'lola')
-    #
-    #   expect(current_path).to eq(dashboard_path)
-    #   expect(page).to have_field('email')
-    #   expect(page).to have_button('Add Friend')
-    #   fill_in :email, with: "lola_rabbit@aol.com"
-    #   click_button "Add Friend"
-    #   expect(current_path).to eq(dashboard_path)
-    #   expect(page).to have_content("Unable to Add Friend")
-    #   expect(page).to_not have_content("lola_rabbit@aol.com")
-    # end
-
     it 'adds user to and shows in Friends list' do
       @user_2 = User.create(email: 'lola_rabbit@aol.com', password: 'lola')
 
@@ -75,20 +62,6 @@ RSpec.describe 'dashboard page' do
       expect(page).to have_content("#{@user_2.email[/[^@]+/]} Added as Friend")
       expect(page).to have_content("lola_rabbit@aol.com")
     end
-
-    # it 'shows friends in Friends section' do
-    #   @user_2 = User.create!(email: 'lola_rabbit@aol.com', password: 'lola')
-    #   @user_3 = User.create!(email: 'bugs_bunny@gmail.com', password: 'bugs')
-    #   @user_4 = User.create!(email: 'daffy_duck@yahoo.com', password: 'daffy')
-    #   Friendship.create(user: @user_1, friend: @user_2)
-    #   Friendship.create(user: @user_1, friend: @user_3)
-    #   require 'pry'; binding.pry
-    #   expect(current_path).to eq(dashboard_path)
-    #   # save_and_open_page
-    #   expect(page).to have_content("lola_rabbit@aol.com")
-    #   expect(page).to have_content("bugs_bunny@gmail.com")
-    #   expect(page).to_not have_content("daffy_duck@yahoo.com")
-    # end
 
     it 'has Logout link to Welcome Page' do
       expect(current_path).to eq(dashboard_path)
