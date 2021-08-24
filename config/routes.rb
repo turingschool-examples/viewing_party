dd6fe81f96927c47c7798ec2baec06e7b1f0c2b9
Matt Toensing <matthew.toensing@gmail.com>
2021-08-23 19:51:29 -0700

    Add routes for user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+
+  controller :users do
+    post '/dashboard', action: :login
+    post '/login', action: :login
+  end
 end
