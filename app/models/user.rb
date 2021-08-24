b914648d0b22334ac5902c9c027b7841049245b5
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-24 16:19:03 -0400

    Update model to include validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :friends, through: :friendships
   has_many :attendees, through: :events
+
+  validates :username, presence: true
+  validates :email, presence: true
+  has_secure_password
 end
