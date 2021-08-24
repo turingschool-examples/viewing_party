018ad0da0e977de20634ebefd62849793a1c5177
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-24 15:46:33 -0400

    Update migration for event having a host user id

--- db/migrate/20210824142152_create_events.rb
+++ db/migrate/20210824142152_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :title
-      t.integer :host
+      t.references :user, foreign_key: true
       t.integer :duration
       t.date :day
       t.time :start_time
