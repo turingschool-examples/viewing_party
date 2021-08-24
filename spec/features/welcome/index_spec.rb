5eb30c0a8d507453a843912e330ed5a01b05a97a
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-24 15:48:18 -0700

    Add tests for login and registration forms

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'The welcome landing page' do
   before(:each) do
+    username = "StevenSpielberg"
+    password = "Test"
+
     visit root_path
   end
 
@@ -9,7 +12,22 @@ RSpec.describe 'The welcome landing page' do
     expect(current_path).to eq('/')
   end
 
-  it 'has a form to login'
+  it 'has a form to login' do
+    expect(page).to have_content("Username")
+    expect(page).to have_content("Password")
 
-  it 'has a link to register if you arent already'
+    expect(page).to have_button("Log In")
+  end
+
+  it 'has a link to register if you arent already' do
+    expect(page).to have_link("New to Virtual Watch Party? Register Here")
+    click_link "New to Virtual Watch Party? Register Here"
+
+    expect(current_path).to eq(registration_path)
+
+    expect(page).to have_content("Username")
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password")
+    expect(page).to have_content("Password Confirmation")
+  end
 end
