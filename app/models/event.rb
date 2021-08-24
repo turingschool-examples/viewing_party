23a4f0927e47b576ebad31e503b8434ea1dce8a2
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-24 16:33:41 -0400

    Create validations on event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :attendees
   has_many :users, through: :attendees
+
+  validates_presence_of :title, :user_id, :duration, :day, :start_time
+  validates_numericality_of :duration  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates :email, presence: true
+
   has_secure_password
 end
