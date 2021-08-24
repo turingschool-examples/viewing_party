9a3de895c24663b772bf0354f32225a3e4924bff
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-24 15:51:28 -0400

    Update user model association tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User do
   describe 'associations' do
     it { should have_many(:events) }
     it { should have_many(:attendees).through(:events) }
+    it { should have_many(:friends) }
   end
   describe 'validations' do
 
