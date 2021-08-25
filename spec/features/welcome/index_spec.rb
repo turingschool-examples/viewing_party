086a279280ede8835230fb18c6df01060ee78b8f
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-24 17:44:40 -0700

    Add sad path tests

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'The welcome landing page' do
   before(:each) do
-    username = "StevenSpielberg"
-    password = "Test"
-
     visit root_path
   end
 
@@ -30,4 +27,75 @@ RSpec.describe 'The welcome landing page' do
     expect(page).to have_content("Password")
     expect(page).to have_content("Password Confirmation")
   end
+
+   it "can create a new user" do
+     click_link "New to Virtual Watch Party? Register Here"
+
+     fill_in "user[username]", with: "StevenSpielberg"
+     fill_in "user[email]", with: "stevenspielberg@email.com"
+     fill_in "user[password]", with: "Test"
+     fill_in "user[password_confirmation]", with: "Test"
+
+     click_button "Create User"
+
+     # require "pry"; binding.pry
+     expect(current_path).to eq(dashboard_path(User.last.id))
+     expect(page).to have_content("Welcome, StevenSpielberg!")
+   end
+
+   it "won't create a new user without username" do
+     click_link "New to Virtual Watch Party? Register Here"
+
+     fill_in "user[username]", with: ""
+     fill_in "user[email]", with: "stevenspielberg@email.com"
+     fill_in "user[password]", with: "Test"
+     fill_in "user[password_confirmation]", with: "Test"
+
+     click_button "Create User"
+
+     expect(current_path).to eq(registration_path)
+     expect(page).to have_content("Please fill out all fields")
+   end
+
+   it "won't create a new user without user email" do
+     click_link "New to Virtual Watch Party? Register Here"
+
+     fill_in "user[username]", with: "StevenSpielberg"
+     fill_in "user[email]", with: ""
+     fill_in "user[password]", with: "Test"
+     fill_in "user[password_confirmation]", with: "Test"
+
+     click_button "Create User"
+
+     expect(current_path).to eq(registration_path)
+     expect(page).to have_content("Please fill out all fields")
+   end
+
+   it "won't create a new user without password" do
+     click_link "New to Virtual Watch Party? Register Here"
+
+     fill_in "user[username]", with: "StevenSpielberg"
+     fill_in "user[email]", with: "stevenspielberg@email.com"
+     fill_in "user[password]", with: ""
+     fill_in "user[password_confirmation]", with: "Test"
+
+     click_button "Create User"
+
+     expect(current_path).to eq(registration_path)
+     expect(page).to have_content("Please fill out all fields")
+   end
+
+   xit "won't create a new user without password confirmation" do
+     click_link "New to Virtual Watch Party? Register Here"
+
+     fill_in "user[username]", with: "StevenSpielberg"
+     fill_in "user[email]", with: "stevenspielberg@email.com"
+     fill_in "user[password]", with: "Test"
+     fill_in "user[password_confirmation]", with: "Te"
+
+     click_button "Create User"
+
+     expect(current_path).to eq(registration_path)
+     expect(page).to have_content("Please fill out all fields")
+   end
 end
