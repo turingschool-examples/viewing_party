4b982563d6eacf2f936ef7671e36372c38f103c8
Alex Klick <aklick12@gmail.com>
2021-08-24 18:46:59 -0600

    password confimation field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,20 @@ class UsersController < ApplicationController
 
   def create
     user = user_params
-    user[:email] = user[:email].downcase
-    new_user = User.create(user)
-    flash[:success] = "Welcome, #{new_user.email}!"
-    redirect_to root_path
+    if user[:password_confirmation] == user[:password]
+      user[:email] = user[:email].downcase
+      new_user = User.create(user)
+      flash[:success] = "Welcome, #{new_user.email}!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Password and confirmation must match!"
+      #if have time, refactor to save information
+      redirect_to new_user_path
+    end
   end
   
   private
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- spec/features/authentication/registration_spec.rb
+++ spec/features/authentication/registration_spec.rb
@@ -10,12 +10,30 @@ RSpec.describe "User registration form" do
     
     username = "funbucket13"
     password = "test"
+    password_confirmation = "test"
 
     fill_in :email, with: username
     fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
 
     click_on "Register"
 
     expect(page).to have_content("Welcome, #{username}!")
+  end
+
+  it "doesn't create new user if password and confirmation don't match" do
+    visit new_user_path
+    
+    username = "funbucket13"
+    password = "test"
+    password_confirmation = "tes"
+
+    fill_in :email, with: username
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+
+    click_on "Register"
+    expect(page).to have_content("Password and confirmation must match!")
+    expect(current_path).to eq(new_user_path)
   end
 end
