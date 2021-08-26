c4b62f67a9e05636c53cd7b198416a880136a945
Matt Toensing <matthew.toensing@gmail.com>
2021-08-25 18:46:32 -0700

    Functioning sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params)
     if user.save
+      session[:user_id] = user.id
       flash[:success] = "Welcome #{user.email}!"
       redirect_to dashboard_path
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates :password, presence: true
   validates :password, confirmation: { case_sensitive: true }
   has_many :friendships
