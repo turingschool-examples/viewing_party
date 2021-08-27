9e337d67fd7a89504e0dd5631022bd6ab8c8b695
Alex Klick <aklick12@gmail.com>
2021-08-26 18:46:37 -0600

    controller refactoring

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
 class SessionsController < ApplicationController
   def create
     found_user = User.find_by(email: params[:email])
-    binding.pry
     if found_user
       if found_user.authenticate(params[:password])
         session[:user_id] = found_user.id
         flash[:success] = "Welcome, #{found_user.email}!"
         redirect_to '/dashboard'
+      else
+        flash[:alert] = "No matching account!"
+        redirect_to root_path
       end
+    else
+      flash[:alert] = "No matching account!"
+      redirect_to root_path
     end
-    flash[:alert] = "No matching account!"
-    redirect_to root_path
   end
 
   def destroy
