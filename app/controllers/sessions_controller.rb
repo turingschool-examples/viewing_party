927e707d8e0124d29bf2b0fc94ea038bf976bd21
Alex Klick <aklick12@gmail.com>
2021-08-26 18:48:48 -0600

    refactor just needed local: true

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,10 @@
 class SessionsController < ApplicationController
   def create
     found_user = User.find_by(email: params[:email])
-    if found_user
-      if found_user.authenticate(params[:password])
-        session[:user_id] = found_user.id
-        flash[:success] = "Welcome, #{found_user.email}!"
-        redirect_to '/dashboard'
-      else
-        flash[:alert] = "No matching account!"
-        redirect_to root_path
-      end
+    if found_user.authenticate(params[:password])
+      session[:user_id] = found_user.id
+      flash[:success] = "Welcome, #{found_user.email}!"
+      redirect_to '/dashboard'
     else
       flash[:alert] = "No matching account!"
       redirect_to root_path
