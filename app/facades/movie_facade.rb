30af623530292105f92d8572b993824ad3f5a45c
Matt Toensing <matthew.toensing@gmail.com>
2021-08-28 14:09:56 -0700

    Change moviefacade to be all class methods with class variable

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,26 +1,26 @@
 class MovieFacade
   attr_reader :service
 
-  def initialize
-    @service = MovieService.new
-  end
 
-  def cast_information(id)
-    cast = @service.get_cast(id)
+    @@service = MovieService.new
+
+
+  def self.cast_information(id)
+    cast = @@service.get_cast(id)
     cast.map do |hash|
       {name: hash[:name], character: hash[:character]}
     end
   end
 
-  def movie_review_info(id)
-    reviews = @service.get_review(id)
+  def self.movie_review_info(id)
+    reviews = @@service.get_review(id)
     reviews.map do |hash|
       {author: hash[:author], review: hash[:content]}
     end
   end
 
-  def movie_information(id)
-    json = @service.get_movie(id)
+  def self.movie_information(id)
+    json = @@service.get_movie(id)
     {
       original_title: json[:original_title],
       overview: json[:overview],
@@ -34,7 +34,7 @@ class MovieFacade
     }
   end
 
-  def create_movie(id)
+  def self.create_movie(id)
     Movie.new(movie_information(id))
   end
 end
