a18df2090e875e92067447045daac18b963bb046
Alex Klick <aklick12@gmail.com>
2021-08-29 16:46:13 -0600

    movies api top 40

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,11 @@
 class MoviesController < ApplicationController
   def index
+    if params['result'] == 'top_40'
+      @movies = MovieServices.top_forty
+      binding.pry
+    else
+      
+    end
   end
 
   def show
---
+++ app/javascript/components/TopRatedBtn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const TopRatedBtn = (props) => {
+  return (
+    <a href="/movies/?result=top_40" className="btn btn-secondary">
+      Top Rate Movies
+    </a>
+  );
+};
+export default TopRatedBtn;
