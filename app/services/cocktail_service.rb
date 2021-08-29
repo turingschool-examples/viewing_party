a9931d1a0b479c8c946ef1e2be55573cbac40df2
Matt Toensing <matthew.toensing@gmail.com>
2021-08-28 18:42:38 -0700

    Add webmock to tests, as well as additional tests and methods to service

--- app/services/cocktail_service.rb
+++ app/services/cocktail_service.rb
@@ -1,14 +1,26 @@
 class CocktailService < ApiService
   def get_random_cocktail
-    response = get_data('www.thecocktaildb.com/api/json/v1/1/random.php').get
+    response = get_data('https://www.thecocktaildb.com/api/json/v1/1/random.php').get
 
     get_json(response)[:drinks][0]
   end
 
   def search_cocktail(query)
-    response = get_data("www.thecocktaildb.com/api/json/v1/1/search.php?s=#{query}").get
+    response = get_data("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{query}").get
 
     get_json(response)[:drinks]
   end
+
+
+  def get_mocktail
+    response = get_data("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic").get
+
+    get_json(response)[:drinks]
+  end
+
+  def get_drink(id)
+    response = get_data("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{id}").get
+
+    get_json(response)[:drinks][0]
+  end
 end
-# www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
