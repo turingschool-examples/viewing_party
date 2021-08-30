76f6aca511bfcee19d17ab1556f430d54d1ac6ad
Alex Klick <aklick12@gmail.com>
2021-08-29 18:33:17 -0600

    movies index has poster and links to themovieDB internal id

--- app/services/movie_services.rb
+++ app/services/movie_services.rb
@@ -5,9 +5,9 @@ class MovieServices < ApiService
   end
 
   def top_forty
-    endpoint1 = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["movies_api_key"]}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_count.gte=500&with_watch_monetization_types=flatrate"
+    endpoint1 = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["movies_api_key"]}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=500&with_watch_monetization_types=flatrate"
     page1 = get_data(endpoint1)
-    endpoint2 =endpoint1 = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["movies_api_key"]}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&vote_count.gte=500&with_watch_monetization_types=flatrate"
+    endpoint2 = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["movies_api_key"]}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=2&vote_count.gte=500&with_watch_monetization_types=flatrate"
     page2 = get_data(endpoint2)
     result = page1[:results].concat(page2[:results])
   end
