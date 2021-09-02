48b9ec5531a99ead9388beb6515a9a5f50fcc4ea
Matt Toensing <matthew.toensing@gmail.com>
2021-09-01 20:00:11 -0700

    Make changes to folder structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user1 = User.create!(email: 'test@test.com', password: 'test')
+# user1 = User.create!(email: 'test@test.com', password: 'test')
 user2 = User.create!(email: 'test2@test.com', password: 'test')
 user3 = User.create!(email: 'test3@test.com', password: 'test')
 Friendship.create!(user: user1, friend: user2)
