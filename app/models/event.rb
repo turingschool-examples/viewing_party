8df12b74ce9a2d073c6761e712a4b5261e92cc7a
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-09-29 10:47:44 -0700

    Add model relationships between friendship, event, and attendee

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ApplicationRecord
-  has_many :attendees
-  belongs_to :user 
+  has_many :attendees, dependent: :destroy
+  belongs_to :user
+  has_many :users, through: :attendees 
+  validates :movie_title, presence: true
+  validates :movie_id, presence: true
+  validates :event_date, presence: true
+  validates :event_time, presence: true
+  validates :runtime, presence: true
+
 end
