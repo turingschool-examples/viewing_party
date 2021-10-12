140cc089c9a027a6ea5036a80375699c2a9783be
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-10-12 16:06:14 -0700

    Fix typo

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,8 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def show
   end
   def new
   end
   def create
-  end
-  private
-  def _params
-    params.permit(:)
   end
 end
