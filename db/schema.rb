9a96be90e2fbce721bc4cba54747e07d59a3bac9
iPowers <isika.powers@gmail.com>
2021-10-12 13:19:48 -0600

    Change column name in users table from password to password_digest.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_020805) do
+ActiveRecord::Schema.define(version: 2021_10_12_191628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_10_12_020805) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
