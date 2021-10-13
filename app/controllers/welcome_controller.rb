cd56531e4f0326d0da98f5777f2470f0bb5a70f5
iPowers <isika.powers@gmail.com>
2021-10-12 18:59:16 -0600

    Add index and login methods to welcome controller.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,18 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.first_name}!"
+
+      redirect_to root_path
+    else
+      flash[:error] = "Error: Please make sure your credentials are correct."
+
+      redirect_to root_path
+    end
+  end
+end
