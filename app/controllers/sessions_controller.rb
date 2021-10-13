97e6de8033e74209684977d455708eacf978e033
iPowers <isika.powers@gmail.com>
2021-10-13 15:59:57 -0600

    Create sessions controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.first_name}!"
+
+      redirect_to dashboard_path
+    else
+      flash[:error] = 'Error: Please make sure your credentials are correct.'
+
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.clear
+
+    redirect_to root_path
+  end
+end
