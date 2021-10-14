31546cc05109c5be5c5edb973250a0349cdfdf54
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-13 20:08:07 -0600

    Add friendships relationships to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :attendees, dependent: :destroy
   has_many :parties, through: :attendees
   has_many :friendships
+  has_many :friends, through: :friendships
 
   has_secure_password
 
