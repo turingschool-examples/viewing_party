b70361dac092b0fd22a9cdf6481ea7eee2e8a9df
iPowers <isika.powers@gmail.com>
2021-10-13 20:05:04 -0600

    Add vcr config to rails_helper.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,3 +71,14 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = false
+  config.cassette_library_dir = File.expand_path('cassettes', __dir__)
+  config.hook_into :webmock
+  config.ignore_request { ENV['DISABLE_VCR'] }
+  config.ignore_localhost = true
+  config.default_cassette_options = {
+    record: :new_episodes
+  }
+end
