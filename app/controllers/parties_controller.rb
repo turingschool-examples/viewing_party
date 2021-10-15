36e46f6ae81bc1ed9ace4e2b8affaa039c661571
iPowers <isika.powers@gmail.com>
2021-10-15 13:11:55 -0600

    Creeate parties controller and new page.

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,6 +2,9 @@ class PartiesController < ApplicationController
   def new
     @user = current_user
 
-    @movie = MovieFacade.movie_info_by_id(:movie_id)
+    @movie = MovieFacade.movie_info_by_id(params[:movie_id])
+  end
+
+  def create
   end
 end
