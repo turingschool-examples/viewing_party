b0e540eea1290a13d1d531a7ff51b67ebe224a73
Erin Quinn <equinn125@gmail.com>
2021-10-15 08:25:39 -0600

    complete viewing party section

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe 'The User dashboard' do
     @user = User.create(email: 'test@test.com', password: 'password123', password_confirmation: 'password123')
     @party = @user.events.create(title: 'Harry Potter and The Chamber of Secrets', date: "2021-10-10", time: '8:00:00', runtime: 90, user_id: @user.id)
     @user_1 = User.create(email: 'test1@test.com', password: 'password1234', password_confirmation: 'password1234')
+    @party_1 =  Event.create(title: 'Harry Potter and The Goblet of Fire', date: "2021-10-11", time: '9:00:00', runtime: 120, user_id: @user_1.id)
     @friendship = Friendship.create(follower_id:@user.id, followee_id:@user_1.id)
-    @attendees =  Attendee.create(user_id:@user_1.id, event_id:@party.id)
+    @attendee =  Attendee.create(user_id:@user_1.id, event_id:@party.id)
+    @attendee_1 = Attendee.create(user_id:@user.id, event_id:@party_1.id)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     visit dashboard_path
 
@@ -24,10 +26,21 @@ RSpec.describe 'The User dashboard' do
 
   it 'has a viewing party section thst shows parties I have created' do
     #note once new viewing party page has been created this will need to be updated
-    expect(page).to have_content(@party.title)
-    expect(page).to have_content(@party.date)
-    expect(page).to have_content(@party.time)
-    expect(page).to have_content(@attendees.user.email)
-    expect(page).to have_content('Hosting')
+    within "#hostin-parties" do
+      expect(page).to have_content(@party.title)
+      expect(page).to have_content(@party.date)
+      expect(page).to have_content(@party.time)
+      expect(page).to have_content(@attendee.user.email)
+      expect(page).to have_content('Hosting')
+    end
+  end
+
+  it 'has a viewing party section for parties I have been invited to' do
+    within "#invited-parties" do
+      expect(page).to have_content(@party_1.date)
+      expect(page).to have_content(@party_1.time)
+      expect(page).to have_content(@attendee_1.user.email)
+      expect(page).to have_content("#{@user_1.email} is hosting")
+    end
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,14 +12,24 @@ RSpec.describe Event , type: :model do
     it { should validate_presence_of(:runtime) }
   end
   before :each do
-
   end
   describe 'class methods' do
     describe '.' do
     end
   end
   describe 'instance methods' do
-    describe '#' do
+    describe '#host?' do
+      # it 'can tell who is hosting the event' do
+      #   @user = User.create(email: 'test@test.com', password: 'password123', password_confirmation: 'password123')
+      #   @party = @user.events.create(title: 'Harry Potter and The Chamber of Secrets', date: "2021-10-10", time: '8:00:00', runtime: 90, user_id: @user.id)
+      #   @user_1 = User.create(email: 'test1@test.com', password: 'password1234', password_confirmation: 'password1234')
+      #   @party_1 =  Event.create(title: 'Harry Potter and The Goblet of Fire', date: "2021-10-11", time: '9:00:00', runtime: 120, user_id: @user_1.id)
+      #   @friendship = Friendship.create(follower_id:@user.id, followee_id:@user_1.id)
+      #   @attendee =  Attendee.create(user_id:@user_1.id, event_id:@party.id)
+      #   @attendee_1 = Attendee.create(user_id:@user.id, event_id:@party_1.id)
+      #   expect(@party.host?(@user.id)).to eq('hosting')
+      #   expect(@party_1.host?(@user.id)).to eq('hosting')
+      # end
     end
   end
 end
