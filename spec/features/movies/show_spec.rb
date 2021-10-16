ecc4a4a0e3fc92e42f720e650de4ccc505cf0d33
iPowers <isika.powers@gmail.com>
2021-10-16 15:00:45 -0600

    Add tests to movies pages.

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -4,31 +4,42 @@ RSpec.describe 'Movies Show Page' do
   before :each do
     @user = create(:mock_user)
 
-    # allow_any_instance_of(MovieService).to receive(:request_api(path)).and_return('Cruella')
-    # allow_any_instance_of(MovieInfo).to receive(:cast).and_return('Emma Stone')
-    # allow_any_instance_of(MovieInfo).to receive(:reviews).and_return('Good')
-
-    # service = MovieService.new
-    # movie = service.request_api("/3/movie/#{movie_id}")
-    # cast = service.request_api("/3/movie/#{movie_id}/credits")
-    # reviews = service.request_api("/3/movie/#{movie_id}/reviews")
-
-    # @movie = MovieInfo.new(movie, cast, reviews)
-    # @movie = VCR.use_cassette("movie_info_by_id") do
-    #   MovieFacade.movie_info_by_id(337404)
-    # end
-
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-    visit movie_path(@movie)
+    # service = MovieService.new
+    # @movie = VCR.use_cassette('movie_info_by_id') do
+    #   MovieFacade.movie_info_by_id(337404)
+    # end
+    stub_movie_info_by_id
+
+    visit movie_path(337404)
+  end
+
+  describe 'create viewing party button' do
+    it 'displays create viewing party button' do
+
+      expect(page).to have_button('Create Viewing Party')
+
+      click_on 'Create Viewing Party'
+
+      expect(current_path).to eq(new_party_path)
+    end
   end
 
   describe "movies attributes" do
-    xit "displays that movie's attributes" do
-      expect(page).to have_content(@movie.title)
-      expect(page).to have_content(@movie.runtime)
-      expect(page).to have_content(@movie.cast)
-      expect(page).to have_content(@movie.duration)
+    it "displays that movie's attributes" do
+      expect(page).to have_content('Title')
+      expect(page).to have_content('Runtime')
+      expect(page).to have_content('Cast')
+      expect(page).to have_content('Summary')
+      expect(page).to have_content('Review(s)')
+      expect(page).to have_content('Author')
+    end
+
+    it "displays that movie's attributes" do
+      expect(page).to have_content('Cruella')
+      expect(page).to have_content(134)
+      expect(page).to have_content('Emma Stone as Estella')
     end
   end
 end
