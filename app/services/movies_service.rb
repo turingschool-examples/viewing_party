7bc7ad45db364cb2151f53145c9d088a55dff52b
Erin Quinn <equinn125@gmail.com>
2021-10-15 21:18:42 -0600

    create initial api framework

---
+++ app/services/movies_service.rb
@@ -0,0 +1,13 @@
+class MovieService
+
+  def get_data(endpoint)
+    response = Faraday.get(endpoint)
+    data = response.body
+    JSON.parse(data, symbolize_names:  true)
+  end
+
+  def top_40
+  first = get_data("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['API_KEY']}&language=en-US&page=1")
+  second = get_data("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['API_KEY']}&language=en-US&page=2")
+  end
+end
