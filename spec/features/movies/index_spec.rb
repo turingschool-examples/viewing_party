6165426a235b2c15bb75dd0f537e14227fcfde7b
Erin Quinn <equinn125@gmail.com>
2021-10-16 18:38:21 -0600

    add vcr and api testing

---
+++ spec/features/movies/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'movie index page' do
+
+  scenario 'user clicks on Discover Top 40 Movies ', :vcr do
+    visit discover_path
+    click_on 'Discover Top 40 Movies'
+    expect(current_path).to eq(movies_path)
+    expect(page).to have_content('The Godfather')
+
+  end
+end
