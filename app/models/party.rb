47fd16c7286c33c3a32a1b7ea1ed39415015c16b
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-18 17:30:59 -0600

    Add host and invited_users instance method

--- app/models/party.rb
+++ app/models/party.rb
@@ -6,4 +6,16 @@ class Party < ApplicationRecord
 
   has_many :attendees, dependent: :destroy
   has_many :users, through: :attendees
+
+  def host
+    host_id = attendees.where('status = ?', 0).first.user_id
+    User.find(host_id)
+  end
+
+  def invited_users
+    invited = attendees.where('status = ?', 1)
+    invited.map do |atnde|
+      User.find(atnde.user_id)
+    end
+  end
 end
