e19570674bf75eb640bc6e0171aa6947d2e8dac1
Erin Quinn <equinn125@gmail.com>
2021-10-17 21:18:58 -0600

    adds vote average

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'movie index page' do
     click_on 'Discover Top 40 Movies'
     expect(current_path).to eq(movies_path)
     expect(page).to have_content('Pulp Fiction')
+    expect(page).to have_content('8.5')
   end
 
   scenario 'user searches by title of a movie', :vcr do
@@ -14,5 +15,6 @@ RSpec.describe 'movie index page' do
     fill_in :search, with: 'Pulp Fiction'
     click_on 'Search by Movie Title'
     expect(page).to have_content('Pulp Fiction')
+    expect(page).to have_content('8.5')
   end
 end
