71c047b0db4171e035b9168e69cdf7cfdc2fff15
iPowers <isika.powers@gmail.com>
2021-10-18 13:54:36 -0600

    Add tests for poros.

---
+++ spec/poros/movie_info_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Movie Info Poros' do
+  describe '::instantiation' do
+    it 'exists and has attributes' do
+      service = MovieService.new
+
+      cruella = VCR.use_cassette("movie_info_by_id") do
+        MovieFacade.movie_info_by_id(337404)
+      end
+
+      expect(cruella).to be_a MovieInfo
+      expect(cruella.title).to eq("Cruella")
+      expect(cruella.vote_average).to eq(8.3)
+      expect(cruella.runtime).to eq(134)
+      expect(cruella.genres.size).to eq(2)
+      expect(cruella.overview.size).to eq(633)
+      expect(cruella.reviews.size).to eq(6)
+      expect(cruella.id).to eq(337404)
+    end
+  end
+end
