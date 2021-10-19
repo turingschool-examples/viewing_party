08261fafc62d478e9723ed0c37acbffe6fa49019
iPowers <isika.powers@gmail.com>
2021-10-18 22:34:34 -0600

    Create gravatar_url method for chatrooms.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@ class User < ApplicationRecord
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+  "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
