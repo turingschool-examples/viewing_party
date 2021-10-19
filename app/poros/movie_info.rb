444db224bcb7873f6a96446d8a8a8a01e313eeaa
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-19 14:43:32 -0600

    Allow initialize without cast and reviews

--- app/poros/movie_info.rb
+++ app/poros/movie_info.rb
@@ -8,7 +8,7 @@ class MovieInfo
               :reviews,
               :id
 
-  def initialize(movie, cast, reviews)
+  def initialize(movie, cast = no_cast, reviews = no_reviews)
     @title = movie[:title]
     @vote_average = movie[:vote_average]
     @runtime = movie[:runtime]
@@ -17,5 +17,13 @@ class MovieInfo
     @cast = cast[:cast]
     @reviews = reviews[:results]
     @id = movie[:id]
+  end
+
+  def no_cast
+    {cast: nil}
+  end
+
+  def no_reviews
+    {results: nil}
   end
 end
