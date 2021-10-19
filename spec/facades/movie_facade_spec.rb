140d072d53dd4d353ebe136a2f01513e5ca83c2b
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-19 15:07:40 -0600

    Add tests for MovieFacade methods return values and movie_info_array helper method

---
+++ spec/facades/movie_facade_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe MovieFacade do
+  it '#forty_top_rated_movies' do
+    top_forty = MovieFacade.forty_top_rated_movies
+    expect(top_forty).to be_an(Array)
+    expect(top_forty[0]).to be_a(MovieInfo)
+  end
+
+  it '#search_movie_title' do
+    search_results = MovieFacade.search_movie_title('baby')
+    expect(search_results).to be_an(Array)
+    expect(search_results[0]).to be_a(MovieInfo)
+  end
+
+  it '#movie_info_by_id' do
+    movie = MovieFacade.movie_info_by_id(52)
+    expect(movie).to be_a(MovieInfo)
+  end
+
+  it '#upcoming' do
+    upcoming_movies = MovieFacade.upcoming
+    expect(upcoming_movies).to be_an(Array)
+    expect(upcoming_movies[0]).to be_a(MovieInfo)
+  end
+
+  it '#now_playing' do
+    now_playing_movies = MovieFacade.now_playing
+    expect(now_playing_movies).to be_an(Array)
+    expect(now_playing_movies[0]).to be_a(MovieInfo)
+  end
+
+  it '#create_movie_info_array', :vcr do
+    service = MovieService.new
+    movies = service.request_api('/3/movie/upcoming')[:results]
+    movie_info_array = MovieFacade.create_movie_info_array(movies)
+    expect(movie_info_array).to be_an(Array)
+    expect(movie_info_array[0]).to be_a(MovieInfo)
+  end
+end
