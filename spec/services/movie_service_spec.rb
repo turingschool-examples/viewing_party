d1714dad8704ae16a520d7888e67d0244b0cf3c7
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-10-18 22:36:52 -0700

    Movie service top 40 testing

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -5,7 +5,21 @@ describe MoviesService do
     VCR.use_cassette('top_40_call') do
       response = MoviesService.top_40
 
+
       expect(response).to be_an Array
+      expect(response[0]).to be_a Hash
+
+      expect(response[0]).to have_key :original_title
+      expect(response[0][:original_title]).to be_a String
+
+      expect(response[0]).to have_key :title
+      expect(response[0][:title]).to be_a String
+
+      expect(response[0]).to have_key :release_date
+      expect(response[0][:release_date]).to be_a String
+
+      expect(response[0]).to have_key :vote_average
+      expect(response[0][:vote_average]).to be_a Float
     end
   end
 end
