063bec6fcf4d7471513b6dba8f26c6553103ccb9
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-19 15:08:14 -0600

    Add class methods upcoming, now_playing, and create_movie_info_array

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -4,17 +4,13 @@ class MovieFacade
     page1 = '/3/movie/top_rated?page=1'
     page2 = '/3/movie/top_rated?page=2'
     movies_json = service.request_api(page1)[:results] + service.request_api(page2)[:results]
-    movies_json.map do |movie_info|
-      MovieInfo.new(movie_info)
-    end
+    create_movie_info_array(movies_json)
   end
 
   def self.search_movie_title(title)
     service = MovieService.new
     search_results_json = service.request_api("/3/search/movie?query=#{title}")[:results]
-    search_results_json.map do |movie_info|
-      MovieInfo.new(movie_info)
-    end
+    create_movie_info_array(search_results_json)
   end
 
   def self.movie_info_by_id(movie_id)
@@ -24,5 +20,23 @@ class MovieFacade
     reviews = service.request_api("/3/movie/#{movie_id}/reviews")
 
     MovieInfo.new(movie, cast, reviews)
+  end
+
+  def self.upcoming
+    service = MovieService.new
+    upcoming_movies = service.request_api('/3/movie/upcoming')[:results]
+    create_movie_info_array(upcoming_movies)
+  end
+
+  def self.now_playing
+    service = MovieService.new
+    now_playing_movies = service.request_api('/3/movie/now_playing')[:results]
+    create_movie_info_array(now_playing_movies)
+  end
+
+  def self.create_movie_info_array(json_results)
+    json_results.map do |movie_info|
+      MovieInfo.new(movie_info)
+    end
   end
 end
