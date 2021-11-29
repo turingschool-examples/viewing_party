7bd35b9f275d2186e22e9904a2502e6463f4f29c
iPowers <isika.powers@gmail.com>
2021-11-29 15:03:57 -0700

    Set configurations for action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,24 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :domain => 'mail.google.com',
+  :port => 587,
+  :user_name => ENV['GOOGLE_USERNAME'],
+  :password => ENV['GOOGLE_PASSWORD'],
+  :authentication => "plain",
+  :enable_starttls_auto => true
+}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
